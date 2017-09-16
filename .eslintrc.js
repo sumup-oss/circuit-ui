6eb9807e93d60714ce8425f34025280c6ce51fe4
Monica Lent <lent.monica@gmail.com>
2017-09-16 16:14:14 +0200

    Make npm start run storybook, add eslintrc.

---
+++ .eslintrc.js
@@ -0,0 +1,57 @@
+module.exports = {
+  extends: [
+    'airbnb-base',
+    'plugin:react/recommended',
+    'prettier',
+    'prettier/react'
+  ],
+  plugins: [
+    'react',
+    'prettier'
+  ],
+  rules: {
+    'no-use-before-define': ['error', { functions: false }],
+    'no-underscore-dangle': 'off',
+    'no-param-reassign': 'off',
+    'max-len': ['error', {
+      code: 80,
+      tabWidth: 2,
+      ignoreComments: true,
+      ignoreUrls: true
+    }],
+    'prettier/prettier': ["error", {
+      singleQuote: true
+    }],
+    'react/prop-types': [1, { ignore: ['i18n'] }],
+    'import/prefer-default-export': 0
+  },
+  globals: {
+    describe: true,
+    beforeEach: true,
+    angular: true,
+    inject: true,
+    it: true,
+    test: true,
+    expect: true,
+    afterEach: true
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    allowImportExportEverywhere: true,
+    ecmaFeatures: {
+      ecmaVersion: 2017,
+      impliedStrict: true,
+      jsx: true
+    }
+  },
+  env: {
+    browser: true
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: 'webpack.config.js'
+      }
+    }
+  }
+};
