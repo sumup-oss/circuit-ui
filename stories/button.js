b479a452303fdd1b573b78d243de618398403ed9
Felix Jung <felix.jung@sumup.com>
2017-10-12 18:01:47 +0200

    Directly return Button from story.

--- stories/button.js
+++ stories/button.js
@@ -4,15 +4,11 @@ import { action } from '@storybook/addon-actions';
 import { Button, LoadingButton } from '..';
 
 storiesOf('Buttons', module)
-  .add('Button', () => {
-    const story = (
-      <Button className="btn btn--highlight" onClick={action('clicked')}>
-        Hello Button
-      </Button>
-    );
-
-    return story;
-  })
+  .add('Button', () => (
+    <Button className="btn btn--highlight" onClick={action('clicked')}>
+      Hello Button
+    </Button>
+  ))
   .add('LoadingButton', () => (
     <LoadingButton className="btn btn--highlight" onClick={action('clicked')}>
       I am Loading!
