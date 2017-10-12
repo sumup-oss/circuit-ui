eb2d33291aec200e6d36d1844c8fc500525def92
Monica Lent <lent.monica@gmail.com>
2017-10-12 15:33:17 +0200

    Fix jest testing and add some tests for the Button component.

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   plugins: ['stylelint-scss'],
   extends: ['stylelint-config-standard', 'stylelint-config-sass-guidelines'],
-  ignoreFiles: ['app/**/brand-styles/**'],
+  ignoreFiles: ['src/brand-styles/**'],
   rules: {
     'selector-pseudo-class-no-unknown': [
       true,
