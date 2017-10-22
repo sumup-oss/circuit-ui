f632ec1169a1c51ee9b49fdd4780bd170804ff39
Monica Lent <lent.monica@gmail.com>
2017-10-22 14:55:55 +0200

    Initial copies of validated input, validated select, validations and label.

---
+++ src/Forms/ValidatedInput/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { flow } from 'lodash/fp';
+import styles from './index.scss';
+import Input from '../Input';
+import Validations from '../Validations';
+import ValidationsAndLabel from '../ValidationsAndLabel';
+import withForm from '../withForm';
+import withStyles from '../../../util/withStyles';
+
+function ValidatedInput(
+  { field, children, label, name, id, type = 'text', ...otherProps },
+  { form: { onFieldChange, data } }
+) {
+  const props = { field, label };
+  const Parent = label === undefined ? Validations : ValidationsAndLabel;
+
+  return (
+    <Parent {...props}>
+      <div className="input-wrapper">
+        <Input
+          type={type}
+          name={name}
+          id={id}
+          className="input"
+          onChange={onFieldChange(field)}
+          value={data.values[field]}
+          errors={data.errors[field]}
+          meta={data.meta[field]}
+          {...otherProps}
+        />
+        {children}
+      </div>
+    </Parent>
+  );
+}
+
+ValidatedInput.propTypes = {
+  field: PropTypes.string,
+  children: PropTypes.node,
+  label: PropTypes.string,
+  name: PropTypes.string,
+  id: PropTypes.string,
+  type: PropTypes.string
+};
+
+export default flow(withStyles(styles), withForm)(ValidatedInput);
