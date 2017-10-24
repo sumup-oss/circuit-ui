a11abee311e687640a84dd28e28e7d28167c6dbc
Monica Lent <lent.monica@gmail.com>
2017-10-24 10:18:31 +0200

    Create input and select forms stories.

--- stories/forms.js
+++ stories/forms.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { storiesOf } from '@storybook/react';
+import { withState } from 'recompose';
 import { action } from '@storybook/addon-actions';
 import {
   ValidatedInput,
@@ -9,47 +10,74 @@ import {
   createFormState
 } from '..';
 
-class MyForm extends Component {
-  constructor(props) {
-    super(props);
-    const formState = createFormState(['name', 'fruit'], {
-      validations: {
-        name: { required: true },
-        fruit: { required: true }
-      },
-      messages: {
-        name: { required: 'Your name is required' },
-        fruit: { required: 'Your favorite fruit is required' }
-      }
-    });
-
-    this.state = formState;
-    this.onChange = this.onChange.bind(this);
+const INITIAL_STATE = createFormState(['name', 'fruit', 'color'], {
+  values: {
+    fruit: 'apples'
+  },
+  validations: {
+    name: { required: true },
+    fruit: { required: true },
+    color: { required: false }
+  },
+  messages: {
+    name: { required: 'Your name is required' },
+    fruit: { required: 'Your favorite fruit is required' }
   }
+});
 
-  onChange(form) {
-    this.setState(prevState => onChangeForm(prevState, form));
-  }
+const withFormState = withState('data', 'onUpdate', INITIAL_STATE);
 
-  render() {
-    return (
-      <div>
-        <Form
-          data={this.state}
-          onChange={this.onChange}
-          onSubmit={action('onSubmit')}
+storiesOf('Forms', module)
+  .add('ValidatedInput', () => {
+    const MyInputForm = withFormState(({ data, onUpdate }) => (
+      <Form
+        data={data}
+        onChange={newData =>
+          onUpdate(prevData => onChangeForm(prevData, newData))}
+        onSubmit={action('onSubmit')}
+      >
+        <ValidatedInput field="name" label="My name" />
+        <ValidatedInput
+          field="fruit"
+          label="My favorite fruit"
+          disabled={true}
+        />
+        <ValidatedInput field="color" label="My favorite color" />
+      </Form>
+    ));
+    return <MyInputForm />;
+  })
+  .add('ValidatedSelect', () => {
+    const MySelectForm = withFormState(({ data, onUpdate }) => (
+      <Form
+        data={data}
+        onChange={newData =>
+          onUpdate(prevData => onChangeForm(prevData, newData))}
+        onSubmit={action('onSubmit')}
+      >
+        <ValidatedSelect field="name" label="Your name">
+          <option value="">Pick your name</option>
+          <option value="Heinz">Heinz</option>
+          <option value="Heinrich">Heinrich</option>
+          <option value="Helga">Helga</option>
+        </ValidatedSelect>
+        <ValidatedSelect
+          field="fruit"
+          label="Your favorite fruit"
+          disabled={true}
         >
-          <ValidatedInput field="name" label="My name" />
-          <ValidatedSelect field="fruit" label="Your favorite fruit">
-            <option value="">Pick a fruit</option>
-            <option value="apples">Apples</option>
-            <option value="bananas">Bananas</option>
-            <option value="fruit loops">Fruit loops</option>
-          </ValidatedSelect>
-        </Form>
-      </div>
-    );
-  }
-}
-
-storiesOf('Forms', module).add('Input', () => <MyForm />);
+          <option value="">Pick a fruit</option>
+          <option value="apples">Apples</option>
+          <option value="bananas">Bananas</option>
+          <option value="fruit loops">Fruit loops</option>
+        </ValidatedSelect>
+        <ValidatedSelect field="color" label="Your favorite color">
+          <option value="">Pick a color</option>
+          <option value="blue">Blue</option>
+          <option value="green">Green</option>
+          <option value="yellow">Yellow</option>
+        </ValidatedSelect>
+      </Form>
+    ));
+    return <MySelectForm />;
+  });
