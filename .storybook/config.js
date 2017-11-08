da951bb41a925d4108168260b16b746af6fd68d0
Ilya Noskov <ilya.noskov@sumup.com>
2017-11-08 16:49:02 +0200

    flash alerts are done. UI != behavior, right?

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,7 +4,12 @@ const req = require.context('../stories', true, /\.js$/);
 
 function loadStories() {
   require('../stories');
+<<<<<<< HEAD
   req.keys().forEach((filename) => req(filename))
+=======
+  require('../stories/button');
+  require('../stories/flash_alert');
+>>>>>>> flash alerts are done. UI != behavior, right?
 }
 
 configure(loadStories, module);
