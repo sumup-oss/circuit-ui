571f8857ced37956d0348dffcb2d1b1128fe0b53
Ilya Noskov <ilya.noskov@sumup.com>
2017-11-13 17:56:25 +0200

    adding className support for notifications component. Making text required for the notifications. Adding tests and snapshots

--- src/Notification/index.js
+++ src/Notification/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import withStyles from '../../util/withStyles';
 import styles from './index.scss';
 
-const Notification = ({ type, data }) => (
-  <div className={`alert alert--${type}`}>{data.msg}</div>
-);
+const Notification = ({ type, msg, children, className }) => {
+  if (!(msg || children)) return null;
+  const content = children || msg;
+  const classes = classNames(
+    {
+      alert: true
+    },
+    `alert--${type}`,
+    className
+  );
+  return <div className={classes}>{content}</div>;
+};
 
 Notification.propTypes = {
   type: PropTypes.string,
