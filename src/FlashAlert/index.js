932e714f64cabfa796356c59a8b505b5fc7d1f45
Ilya Noskov <ilya.noskov@sumup.com>
2017-11-13 18:14:28 +0200

    adding className support for flash-alerts. + tests

--- src/FlashAlert/index.js
+++ src/FlashAlert/index.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import withStyles from '../../util/withStyles';
 import styles from './index.scss';
 
-const FlashAlert = ({ type, data }) => (
-  <div className={`flash-alert flash-alert--${type}`}>
-    <span>{data.msg}</span>
-  </div>
-);
+const FlashAlert = ({ type, msg, className, children }) => {
+  if (!(type && (children || msg))) return null;
+  const content = children || msg;
+  const classes = classNames(
+    { 'flash-alert': true },
+    `flash-alert--${type}`,
+    className
+  );
+  return (
+    <div className={classes}>
+      <span>{content}</span>
+    </div>
+  );
+};
 
 FlashAlert.propTypes = {
   type: PropTypes.string,
-  data: PropTypes.object,
-  children: PropTypes.any
+  msg: PropTypes.string,
+  children: PropTypes.any,
+  className: PropTypes.string
 };
 
 export default withStyles(styles)(FlashAlert);
--- stories/flash_alert.js
+++ stories/flash_alert.js
@@ -4,8 +4,6 @@ import { storiesOf } from '@storybook/react';
 import { FlashAlert } from '..';
 
 storiesOf('Flash Alerts', module)
-  .add('Error', () => <FlashAlert type="error" data={{ msg: 'ERROR' }} />)
-  .add('Success', () => <FlashAlert type="success" data={{ msg: 'SUCCESS' }} />)
-  .add('Warning', () => (
-    <FlashAlert type="warning" data={{ msg: 'WARNING' }} />
-  ));
+  .add('Error', () => <FlashAlert type="error" msg="ERROR" />)
+  .add('Success', () => <FlashAlert type="success" msg="SUCCESS" />)
+  .add('Warning', () => <FlashAlert type="warning" msg="WARNING" />);
