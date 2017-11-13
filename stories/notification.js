64054c5a5e7d53ec921a8cfea78750f477544153
Ilya Noskov <ilya.noskov@sumup.com>
2017-11-13 18:16:55 +0200

    fixing story view for notifications

--- stories/notification.js
+++ stories/notification.js
@@ -3,11 +3,7 @@ import { storiesOf } from '@storybook/react';
 import { Notification } from '..';
 
 storiesOf('Notifications', module)
-  .add('Error', () => <Notification type="error" data={{ msg: 'error' }} />)
-  .add('Warning', () => (
-    <Notification type="warning" data={{ msg: 'warning' }} />
-  ))
-  .add('Info', () => <Notification type="info" data={{ msg: 'info' }} />)
-  .add('Success', () => (
-    <Notification type="success" data={{ msg: 'success' }} />
-  ));
+  .add('Error', () => <Notification type="error" msg="error" />)
+  .add('Warning', () => <Notification type="warning" msg="warning" />)
+  .add('Info', () => <Notification type="info" msg="info" />)
+  .add('Success', () => <Notification type="success" msg="success" />);
