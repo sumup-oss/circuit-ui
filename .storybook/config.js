bed84dd618193911f4beab2bb7d0be97342d12c2
Ilya Noskov <ilya.noskov@sumup.com>
2017-11-13 11:23:08 +0200

    fixing stylelint errors

--- .storybook/config.js
+++ .storybook/config.js
@@ -3,6 +3,7 @@ import { configure } from '@storybook/react';
 function loadStories() {
   require('../stories');
   require('../stories/button');
+  require('../stories/notification');
 }
 
 configure(loadStories, module);
---
+++ stories/notification.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Notification } from '..';
+
+storiesOf('Notifications', module)
+  .add('Error', () => <Notification type="error" data={{ msg: 'error' }} />)
+  .add('Warning', () => (
+    <Notification type="warning" data={{ msg: 'warning' }} />
+  ))
+  .add('Info', () => <Notification type="info" data={{ msg: 'info' }} />)
+  .add('Success', () => (
+    <Notification type="success" data={{ msg: 'success' }} />
+  )
+);
