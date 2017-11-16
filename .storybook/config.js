a47d5d71c8b9e0bd07e1c5b81403c9ceb1c614b1
Ilya Noskov <ilya.noskov@sumup.com>
2017-11-16 18:48:22 +0200

    adding hide functionality

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,15 +4,7 @@ const req = require.context('../stories', true, /\.js$/);
 
 function loadStories() {
   require('../stories');
-<<<<<<< HEAD
   req.keys().forEach((filename) => req(filename))
-=======
-  require('../stories/button');
-  require('../stories/flash-alert');
-<<<<<<< HEAD
->>>>>>> kebab case change
-=======
->>>>>>> 5643129f550e16a344ef76094ae0ed261ecebee0
 }
 
 configure(loadStories, module);
--- src/FlashAlert/index.js
+++ src/FlashAlert/index.js
@@ -1,29 +1,52 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import withStyles from '../../util/withStyles';
 import styles from './index.scss';
 
-const FlashAlert = ({ type, msg, className, children }) => {
-  if (!(type && (children || msg))) return null;
-  const content = children || msg;
-  const classes = classNames(
-    { 'flash-alert': true },
-    `flash-alert--${type}`,
-    className
-  );
-  return (
-    <div className={classes}>
-      <span>{content}</span>
-    </div>
-  );
-};
+class FlashAlert extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shown: true
+    };
+  }
+
+  render() {
+    const { type, msg, className, children } = this.props;
+    const dismissLabel = this.props.dismissLabel || 'Hide';
+    const checkProps = type && (children || msg);
+
+    if (!checkProps || !this.state.shown) return null;
+
+    const content = children || msg;
+    const classes = classNames(
+      { 'flash-alert': true },
+      `flash-alert--${type}`,
+      className
+    );
+    return (
+      <div className={classes}>
+        <span>{content}</span>
+        <span
+          style={{ marginLeft: '40px', fontWeight: '600', cursor: 'pointer' }}
+          onClick={() => {
+            this.setState({ shown: false });
+          }}
+        >
+          {dismissLabel}
+        </span>
+      </div>
+    );
+  }
+}
 
 FlashAlert.propTypes = {
   type: PropTypes.string,
   msg: PropTypes.string,
   children: PropTypes.any,
-  className: PropTypes.string
+  className: PropTypes.string,
+  dismissLabel: PropTypes.string
 };
 
 export default withStyles(styles)(FlashAlert);
