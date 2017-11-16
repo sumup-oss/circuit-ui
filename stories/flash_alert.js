96c106998787bffc91f8220749fe3341f9f20db1
Ilya Noskov <ilya.noskov@sumup.com>
2017-11-16 15:42:21 +0200

    rebasing

--- stories/flash_alert.js
+++
@@ -1,9 +0,0 @@
-/* eslint-disable */
-import React from 'react';
-import { storiesOf } from '@storybook/react';
-import { FlashAlert } from '..';
-
-storiesOf('Flash Alerts', module)
-  .add('Error', () => <FlashAlert type="error" msg="ERROR" />)
-  .add('Success', () => <FlashAlert type="success" msg="SUCCESS" />)
-  .add('Warning', () => <FlashAlert type="warning" msg="WARNING" />);
