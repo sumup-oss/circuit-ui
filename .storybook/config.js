731108b1f60a54fc87f56d48bc15d39a14edd02c
Ilya Noskov <ilya.noskov@sumup.com>
2017-11-16 15:28:07 +0200

    kebab case change

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,12 +4,7 @@ const req = require.context('../stories', true, /\.js$/);
 
 function loadStories() {
   require('../stories');
-<<<<<<< HEAD
   req.keys().forEach((filename) => req(filename))
-=======
-  require('../stories/button');
-  require('../stories/flash_alert');
->>>>>>> flash alerts are done. UI != behavior, right?
 }
 
 configure(loadStories, module);
