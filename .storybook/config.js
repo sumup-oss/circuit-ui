e870ea2c61a6234e9cdd6360e221f53138cdbbac
Ilya Noskov <ilya.noskov@sumup.com>
2017-11-16 15:45:06 +0200

    rebasing

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,7 +4,12 @@ const req = require.context('../stories', true, /\.js$/);
 
 function loadStories() {
   require('../stories');
+<<<<<<< HEAD
   req.keys().forEach((filename) => req(filename))
+=======
+  require('../stories/button');
+  require('../stories/logo');
+>>>>>>> adding logo, needs svg loader
 }
 
 configure(loadStories, module);
