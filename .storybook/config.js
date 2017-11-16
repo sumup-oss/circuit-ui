f237063aaa31690946dc5b752ac02d4056009788
Ilya Noskov <ilya.noskov@sumup.com>
2017-11-16 15:42:21 +0200

    rebasing

--- .storybook/config.js
+++ .storybook/config.js
@@ -4,7 +4,12 @@ const req = require.context('../stories', true, /\.js$/);
 
 function loadStories() {
   require('../stories');
+<<<<<<< HEAD
   req.keys().forEach((filename) => req(filename))
+=======
+  require('../stories/button');
+  require('../stories/flash-alert');
+>>>>>>> kebab case change
 }
 
 configure(loadStories, module);
