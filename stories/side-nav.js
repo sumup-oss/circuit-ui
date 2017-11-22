f242a01a267ad5513e75ec1ad138704a15d75023
Ilya Noskov <ilya.noskov@sumup.com>
2017-11-22 13:09:14 +0200

    removing yarn.lock

--- stories/side-nav.js
+++ stories/side-nav.js
@@ -2,7 +2,4 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { SideNav } from '..';
 
-storiesOf('SideNav', module)
-  .add('Side Menu', () => (
-      <SideNav />
-  ));
+storiesOf('SideNav', module).add('Side Menu', () => <SideNav />);
