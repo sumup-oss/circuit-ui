0016fcff975556d626852c3e363e0810e40bdc3b
Ilya Noskov <ilya.noskov@sumup.com>
2017-11-23 16:59:13 +0200

    offsetting show logic to parent component

--- src/FlashAlert/index.js
+++ src/FlashAlert/index.js
@@ -1,52 +1,46 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import withStyles from '../../util/withStyles';
 import styles from './index.scss';
 
-class FlashAlert extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      shown: true
-    };
-  }
+const FlashAlert = props => {
+  const { type, msg, className, children, onDismiss } = props;
+  let {show} = props;
+  const dismissLabel = props.dismissLabel || 'Hide';
+  if (show === undefined) show = true;
+  const checkProps = show && type && (children || msg);
 
-  render() {
-    const { type, msg, className, children } = this.props;
-    const dismissLabel = this.props.dismissLabel || 'Hide';
-    const checkProps = type && (children || msg);
+  if (!checkProps) return null;
 
-    if (!checkProps || !this.state.shown) return null;
+  const content = children || msg;
+  const classes = classNames(
+    { 'flash-alert': true },
+    `flash-alert--${type}`,
+    className
+  );
 
-    const content = children || msg;
-    const classes = classNames(
-      { 'flash-alert': true },
-      `flash-alert--${type}`,
-      className
-    );
-    return (
-      <div className={classes}>
-        <span>{content}</span>
-        <span
-          style={{ marginLeft: '40px', fontWeight: '600', cursor: 'pointer' }}
-          onClick={() => {
-            this.setState({ shown: false });
-          }}
-        >
-          {dismissLabel}
-        </span>
-      </div>
-    );
-  }
-}
+  console.log(props);
+  return (
+    <div className={classes}>
+      <span>{content}</span>
+      <span
+        style={{ marginLeft: '40px', fontWeight: '600', cursor: 'pointer' }}
+        onClick={onDismiss}
+      >
+        {dismissLabel}
+      </span>
+    </div>
+  );
+};
 
 FlashAlert.propTypes = {
   type: PropTypes.string,
   msg: PropTypes.string,
   children: PropTypes.any,
   className: PropTypes.string,
-  dismissLabel: PropTypes.string
+  dismissLabel: PropTypes.string,
+  onDismiss: PropTypes.func
 };
 
 export default withStyles(styles)(FlashAlert);
--- stories/flash-alert.js
+++ stories/flash-alert.js
@@ -3,7 +3,34 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { FlashAlert } from '..';
 
+class Flash extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { show: true };
+  }
+
+  onDismiss = e => {
+    e.preventDefault();
+    this.setState({ show: false });
+  };
+
+  render() {
+    return (
+      <div>
+        {React.cloneElement(this.props.alert, {
+          onDismiss: this.onDismiss,
+          show: this.state.show
+        })}
+      </div>
+    );
+  }
+}
+
 storiesOf('Flash Alerts', module)
-  .add('Error', () => <FlashAlert type="error" msg="ERROR" />)
-  .add('Success', () => <FlashAlert type="success" msg="SUCCESS" />)
-  .add('Warning', () => <FlashAlert type="warning" msg="WARNING" />);
+  .add('Error', () => <Flash alert={<FlashAlert type="error" msg="ERROR" />} />)
+  .add('Success', () => (
+    <Flash alert={<FlashAlert type="success" msg="SUCCESS" />} />
+  ))
+  .add('Warning', () => (
+    <Flash alert={<FlashAlert type="warning" msg="WARNING" />} />
+  ));
