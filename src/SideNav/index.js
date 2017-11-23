661e750d6967bcea7531ebdae19e326480e116ad
Ilya Noskov <ilya.noskov@sumup.com>
2017-11-23 15:18:44 +0200

    side-nav is done. minor css fixes and tests needed

--- src/SideNav/index.js
+++ src/SideNav/index.js
@@ -1,60 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component, Element } from 'react';
 import withStyles from '../../util/withStyles';
 import styles from './index.scss';
 import { NavOverviewIcon } from '../../src/Icons';
 
-class SideNav extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      expanded: false
-    };
-  }
-
-  selectOption(option) {
-    this.setState({ option });
-  }
-
-  render() {
-    const status = this.state.expanded ? 'expanded' : 'collapsed';
-    return (
-      <div>
-        <div
-          className={`layout-sidenav layout-sidenav--${status}`}
-          onClick={() => {
-            this.setState({ expanded: !this.state.expanded });
-          }}
-        >
-          <div>
-            <ul className={`sidenav-list sidenav-list--${status}`}>
-              <MenuItem isActive={true} expanded={this.state.expanded} />
-              <MenuItem isActive={false} expanded={this.state.expanded} />
-              <MenuItem isActive={false} expanded={this.state.expanded} />
-            </ul>
-          </div>
+const SideNavComponent = props => {
+  const status = props.expanded ? 'expanded' : 'collapsed';
+  const childrenWithProps = React.Children.map(props.children, child =>
+    React.cloneElement(child, { expanded: props.expanded })
+  );
+  
+  return (
+    <div>
+      <div className={`layout-sidenav layout-sidenav--${status}`}>
+        <div>
+          <ul className={`sidenav-list sidenav-list--${status}`}>
+            {childrenWithProps}
+          </ul>
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
-const MenuItem = props => {
+const MenuItemComponent = props => {
   const itemLinkStatus = props.isActive ? 'sidenav-item-link--active' : '';
   const itemTextStatus = props.expanded ? 'expanded' : 'collapsed';
+
   return (
     <li className="sidenav-item ">
       <a className={`sidenav-item-link ${itemLinkStatus}`}>
-        <span className="sidenav-icon--container">
-          <NavOverviewIcon width={20} height={20} />
-        </span>
+        <span className="sidenav-icon--container">{props.icon}</span>
         <span
           className={`sidenav-item-text sidenav-item-text--${itemTextStatus}`}
         >
-          Overview
+          {props.children}
         </span>
       </a>
     </li>
   );
 };
 
-export default withStyles(styles)(SideNav);
+const SideNav = withStyles(styles)(SideNavComponent);
+const MenuItem = withStyles(styles)(MenuItemComponent);
+
+export { SideNav, MenuItem };
--- stories/side-nav.js
+++ stories/side-nav.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { SideNav } from '..';
+import { SideNav, MenuItem } from '..';
+import { NavOverviewIcon } from '..';
 
-storiesOf('SideNav', module).add('Side Menu', () => <SideNav />);
+storiesOf('SideNav', module).add('Side Menu', () => (
+  <SideNav expanded={true}>
+    <MenuItem isActive={false} icon={<NavOverviewIcon />}>
+      Item 1
+    </MenuItem>
+    <MenuItem isActive={true} icon={<NavOverviewIcon />}>
+      Item 2
+    </MenuItem>
+    <MenuItem isActive={false} icon={<NavOverviewIcon />}>
+      Item 3
+    </MenuItem>
+  </SideNav>
+));
