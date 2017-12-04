0ab7e06fd48b981b9a13a3ecf04dabb85e090c68
Felix Jung <felix.jung@sumup.com>
2017-12-04 18:20:35 +0100

    fixup! Add colors and update postpended ccy symbol style

--- src/Price/service.js
+++ src/Price/service.js
@@ -3,12 +3,7 @@ import { includes } from 'lodash/fp';
 export const getColorClass = color => {
   const safeColor = color.toLowerCase();
   const DEFAULT_COLOR = 'brand';
-  const VALID_OPTIONS = [
-    'highlight',
-    'warning',
-    'error',
-    DEFAULT_COLOR
-  ];
+  const VALID_OPTIONS = ['highlight', 'warning', 'error', DEFAULT_COLOR];
   const colorModifier = includes(safeColor, VALID_OPTIONS)
     ? safeColor
     : DEFAULT_COLOR;
