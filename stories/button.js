b7fc8cd55bf64fb8088af70cc21c4332e21dc29c
Felix Jung <felix.jung@sumup.com>
2018-01-02 15:38:36 +0100

    Add jest storybook addon

--- stories/button.js
+++ stories/button.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+
+import withTests from '../util/withTests';
 import { Button, LoadingButton } from '..';
 
 storiesOf('Buttons', module)
+  .addDecorator(withTests('Button/spec.js', 'LoadingButton/spec.js'))
   .add('Button', () => (
     <Button className="btn btn--highlight" onClick={action('clicked')}>
       Hello Button
