f0c39c7494b0b690f55dbc87b5b1e8d6c6e6c0c3
Felix Jung <felix.jung@sumup.com>
2018-01-17 12:02:58 +0100

    Add snapshot serializer for emotion

--- jest.setup.js
+++ jest.setup.js
@@ -1,5 +1,7 @@
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import * as emotion from 'emotion';
+import { createSerializer } from 'jest-emotion';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -9,3 +11,13 @@ global.mount = mount;
 
 // This is defined by webpack in storybook builds using the DefinePlugin plugin.
 global.STORYBOOK = false;
+
+// Add a snapshot serializer that removes random hashes
+// from emotion class names.
+expect.addSnapshotSerializer(
+  createSerializer(emotion, {
+    classNameReplacer(className, index) {
+      return `circuit-${index}`;
+    }
+  })
+);
