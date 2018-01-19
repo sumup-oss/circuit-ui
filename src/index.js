e0799e35ad5bdd975406b2278a8c0cbe4a01150d
Felix Jung <felix.jung@sumup.com>
2018-01-19 15:41:37 +0100

    Remove dead exports

--- src/index.js
+++ src/index.js
@@ -5,6 +5,3 @@ import * as currency from './util/currency';
 export Price from './components/Price';
 
 export { themes, numbers, currency };
-
-export { default as withStyles } from './util/withStyles';
-export { default as StyleProvider } from './util/StyleProvider';
