89c6060820c0c1d48b24420dca8b1349bc216443
Felix Jung <felix.jung@sumup.com>
2018-01-19 16:32:27 +0100

    Fix exports in library index

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import * as themes from './themes';
 import * as numbers from './util/numbers';
 import * as currency from './util/currency';
 
-export Price from './components/Price';
-export Heading from './components/Heading';
-export SubHeading from './components/SubHeading';
+export { default as Price } from './components/Price';
+export { default as Heading } from './components/Heading';
+export { default as SubHeading } from './components/SubHeading';
 
 export { themes, numbers, currency };
