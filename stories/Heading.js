b2f84fa45f7bd05bc02814d2addd9393a853e883
Felix Jung <felix.jung@sumup.com>
2018-01-19 18:08:08 +0100

    Use array instead of Fragment

--- stories/Heading.js
+++ stories/Heading.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { flow } from 'lodash/fp';
 import { storiesOf } from '@storybook/react';
 import { withNotes } from '@storybook/addon-notes';
@@ -12,29 +12,27 @@ storiesOf('Headings', module)
   .addDecorator(withTests('Heading'))
   .add(
     'Heading',
-    flow(withNotes(README), withInfo())(() => (
-      <Fragment>
-        <Heading key="h1zetta" element="h1" size="zetta">
-          This is a zetta heading with an h1 element
-        </Heading>
-        <Heading key="h2exa" element="h2" size="exa">
-          This is an exa heading with an h2 element
-        </Heading>
-        <Heading key="h3peta" element="h3" size="peta">
-          This is a peta heading with an h3 element
-        </Heading>
-        <Heading key="h4tera" element="h4" size="tera">
-          This is a tera heading with an h4 element
-        </Heading>
-        <Heading key="h5giga" element="h5" size="giga">
-          This is a giga heading with an h5 element
-        </Heading>
-        <Heading key="h6mega" element="h6" size="mega">
-          This is a mega heading with an h6 element
-        </Heading>
-        <Heading key="h6kilo" element="h6" size="kilo">
-          This is a kilo heading with an h6 element
-        </Heading>
-      </Fragment>
-    ))
+    flow(withNotes(README), withInfo())(() => [
+      <Heading key="h1zetta" element="h1" size="zetta">
+        This is a zetta heading with an h1 element
+      </Heading>,
+      <Heading key="h2exa" element="h2" size="exa">
+        This is an exa heading with an h2 element
+      </Heading>,
+      <Heading key="h3peta" element="h3" size="peta">
+        This is a peta heading with an h3 element
+      </Heading>,
+      <Heading key="h4tera" element="h4" size="tera">
+        This is a tera heading with an h4 element
+      </Heading>,
+      <Heading key="h5giga" element="h5" size="giga">
+        This is a giga heading with an h5 element
+      </Heading>,
+      <Heading key="h6mega" element="h6" size="mega">
+        This is a mega heading with an h6 element
+      </Heading>,
+      <Heading key="h6kilo" element="h6" size="kilo">
+        This is a kilo heading with an h6 element
+      </Heading>
+    ])
   );
