98d502b77cb1b11ab17dd4c1f966e4d55f27a738
Felix Jung <felix.jung@sumup.com>
2018-01-22 13:32:31 +0100

    Expose jest-emotion getStyles globally

--- jest.setup.js
+++ jest.setup.js
@@ -2,13 +2,14 @@ import 'jest-enzyme';
 import Enzyme, { shallow, render, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import * as emotion from 'emotion';
-import { createSerializer } from 'jest-emotion';
+import { createSerializer, getStyles } from 'jest-emotion';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 global.shallow = shallow;
 global.render = render;
 global.mount = mount;
+global.getStyles = getStyles;
 
 // This is defined by webpack in storybook builds using the DefinePlugin plugin.
 global.STORYBOOK = false;
