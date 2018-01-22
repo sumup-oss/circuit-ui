225dde8e53639ad9866ba6ccf990db9463106151
Felix Jung <felix.jung@sumup.com>
2018-01-22 11:35:19 +0100

    Split up BodyText story into modifier stories

--- stories/BodyText.js
+++ stories/BodyText.js
@@ -1,26 +1,33 @@
 import React from 'react';
-import { flow } from 'lodash/fp';
 import { storiesOf } from '@storybook/react';
-import { withNotes } from '@storybook/addon-notes';
 import { withInfo } from '@storybook/addon-info';
 
 import withTests from '../src/util/withTests';
 import { PlainBodyText as BodyText } from '../src/components/BodyText';
-import README from '../src/components/BodyText/README.md';
 
 storiesOf('BodyText', module)
   .addDecorator(withTests('BodyText'))
   .add(
-    'BodyTexts',
-    flow(withNotes(README), withInfo())(() => [
-      <BodyText key="pkilo" element="p" size="giga">
+    'Giga BodyText with p',
+    withInfo()(() => (
+      <BodyText element="p" size="giga">
         This is a giga BodyText with a p element
-      </BodyText>,
-      <BodyText key="articlemega" element="article" size="mega">
+      </BodyText>
+    ))
+  )
+  .add(
+    'Mega BodyText with article',
+    withInfo()(() => (
+      <BodyText element="article" size="mega">
         This is an mega BodyText with an article element
-      </BodyText>,
-      <BodyText key="divkilo" element="div" size="kilo">
+      </BodyText>
+    ))
+  )
+  .add(
+    'Kilo BodyText with div',
+    withInfo()(() => (
+      <BodyText element="div" size="kilo">
         This is a kilo BodyText with a div element
       </BodyText>
-    ])
+    ))
   );
