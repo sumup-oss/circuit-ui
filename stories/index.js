b803a01cdbf6453ec965b240e34b2384bc1d4db2
Felix Jung <felix.jung@sumup.com>
2018-01-22 11:44:25 +0100

    Remove React imports

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,3 @@
-import React, { Children } from 'react';
 import { addDecorator } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { withSmartKnobs } from 'storybook-addon-smart-knobs';
