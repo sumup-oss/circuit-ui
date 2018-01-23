e308a918a190fe2d2a50e7736659d457e4d57e88
Felix Jung <felix.jung@sumup.com>
2018-01-23 14:28:09 +0100

    Add analyticsId description

--- src/util/shared-prop-types.js
+++ src/util/shared-prop-types.js
@@ -1,6 +1,14 @@
 import PropTypes from 'prop-types';
 
+// TODO: figure out if we can still get these props in react-docgen
+//       when they are imported and merged into a component's
+//       propTypes.
+
 export const interactivePropTypes = {
+  /**
+   * An ID passed to the <input> element via a data attribute. This
+   * is used as an identifier for analytics tracking and e2e testing.
+   */
   analyticsId: PropTypes
 };
 
