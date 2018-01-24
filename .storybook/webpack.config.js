c58bf4ae7c1fe955b69bd210f0d6573ba2cdd835
Felix Jung <felix.jung@sumup.com>
2018-01-24 14:37:53 +0100

    Clean up dependencies further

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,9 +1,6 @@
 const path = require('path');
-const bourbonPath = require('bourbon').includePaths;
-const neatPath = require('bourbon-neat').includePaths;
 const webpack = require('webpack');
 
-const normalizePath = path.join(__dirname, '../node_modules/normalize.css');
 const merge = require('webpack-merge');
 
 module.exports = function(storybookBaseConfig, configType) {
@@ -34,30 +31,12 @@ module.exports = function(storybookBaseConfig, configType) {
               }
             }
           ]
-        },
-        {
-          test: /\.scss$/,
-          include: path.resolve(__dirname, '../'),
-          use: [
-            { loader: 'isomorphic-style-loader' },
-            { loader: 'css-loader', options: { sourceMap: true } },
-            { loader: 'postcss-loader', options: { sourceMap: true } },
-            {
-              loader: 'sass-loader',
-              options: {
-                sourceMap: true,
-                includePaths: [normalizePath]
-                  .concat(neatPath)
-                  .concat(bourbonPath)
-              }
-            }
-          ]
         }
       ]
     },
     plugins: [
       new webpack.DefinePlugin({
-        STORYBOOK: JSON.stringify(true),
+        STORYBOOK: JSON.stringify(true)
       })
     ]
   };
