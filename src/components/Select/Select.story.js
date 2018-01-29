5da0622ecdccc10e910406bd3ddb6a2c6f96a155
Felix Jung <felix.jung@sumup.com>
2018-01-29 18:15:49 +0100

    Add Select component

---
+++ src/components/Select/Select.story.js
@@ -0,0 +1,50 @@
+import React, { Fragment } from 'react';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+import { action } from '@storybook/addon-actions';
+
+import withTests from '../../util/withTests';
+import Select from './';
+
+const options = [
+  {
+    label: 'Option 1',
+    value: 1
+  },
+  {
+    label: 'Option 2',
+    value: 2
+  },
+  {
+    label: 'Option 3',
+    value: 3
+  }
+];
+
+storiesOf('Select', module)
+  // .addDecorator(withTests('Select'))
+  .add(
+    'Default Select',
+    withInfo()(() => (
+      <Fragment>
+        <Select
+          key="no-selection"
+          options={options}
+          onChange={action('Option selected')}
+        />
+        <Select
+          key="second-selected"
+          options={options}
+          value={2}
+          onChange={action('Option selected')}
+        />
+        <Select
+          key="second-selected"
+          options={options}
+          value={2}
+          onChange={action('Option selected')}
+          disabled
+        />
+      </Fragment>
+    ))
+  );
