fb22f3ea0673e54aac51619b562ae085f184cbe2
Felix Jung <felixjung@users.noreply.github.com>
2018-01-29 18:18:24 +0100

    Add missing exports

--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import * as themes from './themes';
 import * as numbers from './util/numbers';
 import * as currency from './util/currency';
 
-export { default as Price } from './components/Price';
+// Typography
 export { default as Heading } from './components/Heading';
 export { default as SubHeading } from './components/SubHeading';
 export { default as Text } from './components/Text';
+
+// Forms
+export { default as FormLabel } from './components/FormLabel';
+export { default as Input } from './components/Input';
+export { default as SearchInput } from './components/SearchInput';
 export { default as PasswordInput } from './components/PasswordInput';
+export { default as TextArea } from './components/TextArea';
+
+// Buttons
+export { default as Button } from './components/Button';
+export { default as SvgButton } from './components/SvgButton';
+
+// Misc
+export { default as Price } from './components/Price';
 
 export { themes, numbers, currency };
