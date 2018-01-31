a9e2aea3262a717bc64be0aab7ed55277fccbf2e
Felix Jung <felix.jung@sumup.com>
2018-01-31 10:04:37 +0100

    Add ButtonGroup story

--- src/components/ButtonGroup/ButtonGroup.story.js
+++ src/components/ButtonGroup/ButtonGroup.story.js
@@ -3,8 +3,17 @@ import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 
 import withTests from '../../util/withTests';
-import ButtonGroup from './ButtonGroup';
+import ButtonGroup from './';
+import Button from '../Button';
 
 storiesOf('ButtonGroup', module)
   .addDecorator(withTests('ButtonGroup'))
-  .add('Default ButtonGroup', withInfo()(() => <ButtonGroup />));
+  .add(
+    'Default ButtonGroup',
+    withInfo()(() => (
+      <ButtonGroup>
+        <Button secondary>Cancel</Button>
+        <Button>Confirm</Button>
+      </ButtonGroup>
+    ))
+  );
