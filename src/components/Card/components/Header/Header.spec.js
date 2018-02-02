4d7318e3541781f741f47c78af52cb44aff33756
Felix Jung <felixjung@users.noreply.github.com>
2018-02-02 10:38:00 +0100

    Add more Card unit tests (#79)
    * Add style test for Card component
    
    * Add more unit tests for CardHeader

--- src/components/Card/components/Header/Header.spec.js
+++ src/components/Card/components/Header/Header.spec.js
@@ -3,8 +3,29 @@ import React from 'react';
 import { CardHeader } from '../..';
 
 describe('CardHeader', () => {
+  /**
+   * Style tests.
+   */
+
   it('should have the correct styles', () => {
     const actual = create(<CardHeader />);
     expect(actual).toMatchSnapshot();
+  });
+
+  /**
+   * Logic tests.
+   */
+
+  it('should render a close button when an onClose prop is passed', () => {
+    const wrapper = shallow(<CardHeader onClose={() => {}} />).dive();
+    const actual = wrapper.find('CloseButton');
+    expect(actual).toHaveLength(1);
+  });
+
+  it('should call the onClose prop when the close button is clicked', () => {
+    const onClose = jest.fn();
+    const wrapper = shallow(<CardHeader onClose={onClose} />).dive();
+    wrapper.find('CloseButton').simulate('click');
+    expect(onClose).toHaveBeenCalled();
   });
 });
