653cc04a018a3bc674b87995336df9641b7473e0
Felix Jung <felix@jung.fm>
2018-02-05 17:06:50 +0100

    Complete meta data extraction for all components

--- build/stylesheet/extract-static.js
+++ build/stylesheet/extract-static.js
@@ -6,6 +6,8 @@ import readFile from './lib/read-file';
 
 const componentGlob = 'src/components/**/+([A-Z]+([a-z])).js';
 
+const blacklist = [/Price(:?Service)?/];
+
 const replaceValues = values => {
   const map = {
     'SIZES.KILO': 'kilo',
@@ -20,23 +22,21 @@ const replaceValues = values => {
   return values.map(val => map[val] || val);
 };
 
-const getPropType = ({ type: { name: type, value } }) => {
-  const values = value && value.map(({ value: val }) => val);
+const getPropMeta = ({ type: { name: type, value } }) => {
+  const isMappable = value && !!value.map;
+  const values = isMappable && value.map(({ value: val }) => val);
   return { type, values };
 };
 
 const filterProps = props =>
   Object.entries(props).reduce((filteredProps, [prop, data]) => {
     const TYPES_TO_ITERATE = ['bool', 'enum'];
-    const { type, values: allowedValues } = getPropType(data);
+    const { type, values } = getPropMeta(data);
     if (!TYPES_TO_ITERATE.includes(type)) {
       return filteredProps;
     }
-    const values = (allowedValues && replaceValues(allowedValues)) || [
-      true,
-      false
-    ];
-    filteredProps[prop] = values;
+    const cleanValues = (values && replaceValues(values)) || [true, false];
+    filteredProps[prop] = cleanValues;
     return filteredProps;
   }, {});
 
@@ -52,8 +52,11 @@ const parseComponent = async path => {
 };
 
 async function main() {
-  const files = await listFiles(componentGlob);
-  const componentData = await Promise.all(files.map(parseComponent));
+  const allFiles = await listFiles(componentGlob);
+  const filteredFiles = allFiles.filter(
+    path => !blacklist.some(regex => regex.test(path))
+  );
+  const componentData = await Promise.all(filteredFiles.map(parseComponent));
   console.log(componentData);
 }
 
