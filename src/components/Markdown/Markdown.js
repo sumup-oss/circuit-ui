6cbbb21a5c6ff32b1e48e4ca5a15bdd2ce1adf70
Connor Bär <connor-baer@users.noreply.github.com>
2018-02-06 09:51:17 +0100

    Add Markdown component (#91)
    * Add Markdown component
    
    * Add unit tests
    
    * Add missing unit tests

---
+++ src/components/Markdown/Markdown.js
@@ -0,0 +1,59 @@
+import PropTypes from 'prop-types';
+import { compiler } from 'markdown-to-jsx';
+
+/**
+ * Markdown component to parse and render GFM to JSX.
+ */
+const Markdown = ({
+  children,
+  overrides,
+  forceBlock,
+  forceInline,
+  transformer
+}) => {
+  const options = { overrides, forceBlock, forceInline };
+  const transformedMarkdown = transformer(children);
+  const html = compiler(transformedMarkdown, options);
+  return html;
+};
+
+Markdown.propTypes = {
+  /**
+   * The GitHub flavoured markdown content to be parsed and rendered.
+   */
+  children: PropTypes.string,
+  /**
+   * A function to transform/clean the markdown string before it is parsed.
+   * Receives the markdown string as first argument and should return a string.
+   */
+  transformer: PropTypes.func,
+  /**
+   * Override an HTML tag with a React component, e.g. `a` -> `<Link />`
+   * The React component is passed all HTML attributes as props.
+   * See https://github.com/probablyup/markdown-to-jsx for documentation.
+   */
+  overrides: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+  /**
+   * Treat all markdown strings as "block" elements.
+   * See https://github.com/probablyup/markdown-to-jsx for documentation.
+   */
+  forceBlock: PropTypes.bool,
+  /**
+   * Treat all markdown strings as "inline" elements.
+   * See https://github.com/probablyup/markdown-to-jsx for documentation.
+   */
+  forceInline: PropTypes.bool
+};
+
+Markdown.defaultProps = {
+  children: '',
+  transformer: md => md,
+  overrides: {},
+  forceBlock: false,
+  forceInline: false
+};
+
+/**
+ * @component
+ */
+export default Markdown;
