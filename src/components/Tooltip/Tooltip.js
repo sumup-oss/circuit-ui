f1994e46f45854307515ebcd382bd6265cc98301
Cristian Oliveira <contato@cristianoliveira.com.br>
2018-02-07 16:55:21 +0100

    Add Tooltip component
    * Add Tooltip component
    
    * Refactoring in order to add label for each tooltip align
    
    * Refactor change align constants
    
    * Add tests for icon content

---
+++ src/components/Tooltip/Tooltip.js
@@ -0,0 +1,133 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { css } from 'react-emotion';
+
+import { textKilo } from '../../styles/style-helpers';
+
+const ALIGN_CENTER = 'center';
+const ALIGN_RIGHT = 'right';
+const ALIGN_LEFT = 'left';
+
+const baseStyles = css`
+  label: tooltip;
+  position: relative;
+  display: inline-block;
+
+  &:hover {
+    > span {
+      visibility: visible;
+      opacity: 1;
+    }
+  }
+`;
+
+const arrowAlignsStyles = ({ align, theme }) =>
+  ({
+    center: css`
+      label: tooltip__text-arrow--center;
+      &::after {
+        left: 50%;
+      }
+    `,
+    right: css`
+      label: tooltip__text-arrow--right;
+      &::after {
+        left: ${theme.spacings.mega};
+      }
+    `,
+    left: css`
+      label: tooltip__text-arrow--left;
+      &::after {
+        right: ${theme.spacings.mega};
+      }
+    `
+  }[align]);
+
+const tooltipTextAlignStyles = ({ align }) =>
+  ({
+    center: css`
+      label: tooltip__text--center;
+      left: 0;
+      right: 0;
+      margin-left: auto;
+      margin-right: auto;
+    `,
+    right: css`
+      label: tooltip__text--right;
+      left: 50%;
+    `,
+    left: css`
+      label: tooltip__text--left;
+      right: 50%;
+    `
+  }[align]);
+
+const tooltipTextStyles = ({ theme }) => css`
+  label: tooltip__text;
+  visibility: hidden;
+  opacity: 0;
+  width: max-content;
+  min-width: 60px;
+  background-color: ${theme.colors.black};
+  color: ${theme.colors.white};
+  text-align: center;
+  border-radius: ${theme.borderRadius.mega};
+  padding: ${theme.spacings.byte};
+  position: absolute;
+  z-index: 1;
+  bottom: 125%;
+  transition: opacity 0.3s;
+  ${textKilo({ theme })};
+
+  &::after {
+    content: '';
+    position: absolute;
+    margin-left: -${theme.spacings.bit};
+    border-width: ${theme.spacings.bit};
+    border-style: solid;
+    border-color: ${theme.colors.black} transparent transparent transparent;
+    top: 100%;
+  }
+`;
+
+const TooltipContainer = styled('span')(
+  tooltipTextStyles,
+  tooltipTextAlignStyles,
+  arrowAlignsStyles
+);
+const TooltipElement = styled('div')(baseStyles);
+
+/**
+ * A Tooltip wrapper in order to show tooltips on top of other
+ * components.
+ */
+const Tooltip = ({ children, content, align, ...props }) => (
+  <TooltipElement {...props}>
+    <TooltipContainer {...{ align }}>{content}</TooltipContainer>
+    {children}
+  </TooltipElement>
+);
+
+Tooltip.Center = ALIGN_CENTER;
+Tooltip.Right = ALIGN_RIGHT;
+Tooltip.Left = ALIGN_LEFT;
+
+Tooltip.propTypes = {
+  children: PropTypes.node.isRequired,
+
+  /**
+   * The content inside of the tooltip being shown.
+   */
+  content: PropTypes.node.isRequired,
+  /**
+   * The alignment of the tooltip.
+   * It can be right, left or centered.
+   */
+  align: PropTypes.oneOf([Tooltip.Center, Tooltip.Right, Tooltip.Left])
+};
+
+Tooltip.defaultProps = {
+  align: Tooltip.Center
+};
+
+export default Tooltip;
---
+++ src/components/Tooltip/Tooltip.spec.js
@@ -0,0 +1,57 @@
+import React from 'react';
+
+import Tooltip from '.';
+
+describe('Tooltip', () => {
+  const DummyIcon = () => (
+    <svg fill="#000000" xmlns="http://www.w3.org/2000/svg">
+      <path />
+    </svg>
+  );
+
+  it('should align to the center', () => {
+    const component = create(
+      <Tooltip align={Tooltip.Center} content="The tooltip content">
+        Something with tooltip
+      </Tooltip>
+    );
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should align to the right', () => {
+    const component = create(
+      <Tooltip align={Tooltip.Right} content="The tooltip content">
+        Something with tooltip
+      </Tooltip>
+    );
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should align to the left', () => {
+    const component = create(
+      <Tooltip align={Tooltip.Left} content="The tooltip content">
+        Something with tooltip
+      </Tooltip>
+    );
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should render with icon', () => {
+    const component = create(
+      <Tooltip align={Tooltip.Center} content="The tooltip content">
+        <span>Text and a</span>
+        <DummyIcon />
+      </Tooltip>
+    );
+    expect(component).toMatchSnapshot();
+  });
+
+  it('should accept icon as content', () => {
+    const component = create(
+      <Tooltip align={Tooltip.Center} content={<DummyIcon />}>
+        Some text
+      </Tooltip>
+    );
+    expect(component).toMatchSnapshot();
+  });
+});
