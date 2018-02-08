8ad804e3d41ec5173d8e0b58589d2add85591f0a
Connor Bär <connor.baer@sumup.com>
2018-02-08 10:14:42 +0100

    Change module entry point to src/index.js

--- src/index.js
+++ src/index.js
@@ -2,27 +2,39 @@ import * as themes from './themes';
 import * as numbers from './util/numbers';
 import * as currency from './util/currency';
 
-// FIXME: these exports don't work unless we introduce a babel
-//        transform. This is future ES syntax.
-
 // Typography
 export { default as Heading } from './components/Heading';
+export { default as List } from './components/List';
+export { default as Markdown } from './components/Markdown';
 export { default as SubHeading } from './components/SubHeading';
 export { default as Text } from './components/Text';
 
 // Forms
+export { default as Checkbox } from './components/Checkbox';
 export { default as FormLabel } from './components/FormLabel';
 export { default as Input } from './components/Input';
-export { default as SearchInput } from './components/SearchInput';
 export { default as PasswordInput } from './components/PasswordInput';
+export { default as RadioButton } from './components/RadioButton';
+export { default as SearchInput } from './components/SearchInput';
+export { default as Select } from './components/Select';
 export { default as TextArea } from './components/TextArea';
 
 // Actions
 export { default as Button } from './components/Button';
+export { default as ButtonGroup } from './components/ButtonGroup';
+export { default as CloseButton } from './components/CloseButton';
 export { default as SvgButton } from './components/SvgButton';
 export { default as Toggle } from './components/Toggle';
 
 // Misc
+export { default as Badge } from './components/Badge';
+export { default as Card } from './components/Card';
+export { default as IconInputWrapper } from './components/IconInputWrapper';
+export { default as Image } from './components/Image';
+export { default as LoadingBar } from './components/LoadingBar';
 export { default as Price } from './components/Price';
+
+// Helpers
+export { default as State } from './components/State';
 
 export { themes, numbers, currency };
