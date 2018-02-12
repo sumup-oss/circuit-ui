1e70c45ad8c9cabb5d9eeae8f7d605a6e7e9aa08
Connor Bär <connor.baer@sumup.com>
2018-02-12 18:10:53 +0100

    Add spacing prop to <Card>

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -12,6 +12,9 @@ const SINGLE = 'single';
 const DOUBLE = 'double';
 const TRIPLE = 'triple';
 
+const KILO = 'kilo';
+const MEGA = 'mega';
+
 const baseStyles = ({ theme }) => css`
   label: card;
   background-color: ${theme.colors.white};
@@ -19,27 +22,49 @@ const baseStyles = ({ theme }) => css`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  min-width: 460px;
-  padding: ${theme.spacings.mega} ${theme.spacings.giga};
+  min-width: 400px;
 `;
 
-const shadowStyles = ({ theme, shadow }) => css`
-  label: card--shadow-${shadow};
-  ${shadow === SINGLE && shadowSingle({ theme })};
-  ${shadow === DOUBLE && shadowDouble({ theme })};
-  ${shadow === TRIPLE && shadowTriple({ theme })};
-`;
+const shadowStyles = ({ theme, shadow }) => {
+  const shadows = {
+    [SINGLE]: shadowSingle({ theme }),
+    [DOUBLE]: shadowDouble({ theme }),
+    [TRIPLE]: shadowTriple({ theme })
+  };
+  return css`
+    label: card--shadow-${shadow};
+    ${shadows[shadow]};
+  `;
+};
+
+const spacingStyles = ({ theme, spacing }) => {
+  const spacings = {
+    [KILO]: `
+      ${theme.spacings.mega} ${theme.spacings.mega}
+    `,
+    [MEGA]: `
+      ${theme.spacings.mega} ${theme.spacings.giga}
+    `
+  };
+  return css`
+    label: card--spacing-${spacing};
+    padding: ${spacings[spacing]};
+  `;
+};
 
 /**
  * Card component that is used for displaying content on a grid.
  */
 const Card = styled('div')`
-  ${baseStyles} ${shadowStyles};
+  ${baseStyles} ${shadowStyles} ${spacingStyles};
 `;
 
 Card.SINGLE = SINGLE;
 Card.DOUBLE = DOUBLE;
 Card.TRIPLE = TRIPLE;
+
+Card.KILO = KILO;
+Card.MEGA = MEGA;
 
 Card.propTypes = {
   /**
@@ -47,13 +72,18 @@ Card.propTypes = {
    */
   shadow: PropTypes.oneOf([Card.SINGLE, Card.DOUBLE, Card.TRIPLE]),
   /**
+   * Spacing
+   */
+  spacing: PropTypes.oneOf([Card.KILO, Card.MEGA]),
+  /**
    * Content to be rendered inside the Card.
    */
   children: childrenPropType
 };
 
 Card.defaultProps = {
-  depth: Card.SINGLE
+  spacing: Card.MEGA,
+  shadow: Card.SINGLE
 };
 
 /**
