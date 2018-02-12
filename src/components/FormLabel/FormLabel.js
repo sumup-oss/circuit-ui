93f4ac7470178ccd5c5ae54160d41bfc6b17f201
Connor Bär <connor.baer@sumup.com>
2018-02-12 23:54:13 +0100

    Reduce font weight of FormLabel

--- src/components/FormLabel/FormLabel.js
+++ src/components/FormLabel/FormLabel.js
@@ -6,7 +6,6 @@ import { textKilo } from '../../styles/style-helpers';
 const baseStyles = ({ theme }) => css`
   label: form-label;
   ${textKilo({ theme })};
-  font-weight: ${theme.fontWeight.bold};
   margin-bottom: ${theme.spacings.bit};
 `;
 
