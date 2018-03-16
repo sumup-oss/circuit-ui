b08af28ef86be20b0a0b691e27a69df5a9be3c8e
Julian Popov <julian.popov@sumup.com>
2018-03-16 17:20:34 +0200

    Remove PasswordInput and hide Price

--- src/components/Price/index.js
+++ src/components/Price/index.js
@@ -1,5 +1,5 @@
-import { withTheme } from 'emotion-theming';
-
-import Price from './Price';
-
-export default withTheme(Price);
+// import { withTheme } from 'emotion-theming';
+//
+// import Price from './Price';
+//
+// export default withTheme(Price);
