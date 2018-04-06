d7819819fecc36390f0fa8b175a5e285cf2253b3
Felix Jung <felix.jung@sumup.com>
2018-04-06 16:22:50 +0200

    Remove SecurityCodeInput state reducer code

--- src/components/CreditCardDetails/components/SecurityCodeInput/SecurityCodeInputService.js
+++ src/components/CreditCardDetails/components/SecurityCodeInput/SecurityCodeInputService.js
@@ -4,20 +4,3 @@ const { SCHEMES } = schemes;
 
 export const getPlaceholder = cardScheme =>
   cardScheme === SCHEMES.AMEX ? '1234' : '123';
-
-export const ACTIONS = {
-  TOGGLE_MODAL: 'toggle modal'
-};
-
-export const initialState = {
-  isShowingModal: false
-};
-
-export const stateReducer = action => prevState => {
-  switch (action) {
-    case ACTIONS.TOGGLE_MODAL:
-      return { isOpen: !prevState.isOpen };
-    default:
-      return prevState;
-  }
-};
