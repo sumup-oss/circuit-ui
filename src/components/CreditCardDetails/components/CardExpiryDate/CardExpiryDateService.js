fdc656546995759072da31dffcbae882db4af3a5
Felix Jung <felix.jung@sumup.com>
2018-04-06 11:45:58 +0200

    Add expiry date validation

--- src/components/CreditCardDetails/components/CardExpiryDate/CardExpiryDateService.js
+++ src/components/CreditCardDetails/components/CardExpiryDate/CardExpiryDateService.js
@@ -1,10 +1,38 @@
+import { pickBy, identity, keys } from '../../../../util/fp';
+
+// The month won't change while this is running, unless you're signing
+// up the night before your credit card expires, right before mindnight.
+const CURRENT_DATE = new Date();
+
+const CURRENT_MONTH = CURRENT_DATE.getMonth() + 1;
+const CURRENT_YEAR = CURRENT_DATE.getFullYear();
+const YEAR_PREFIX = `${CURRENT_YEAR}`.slice(0, 2);
+
 export const parseDate = value => {
   const matches = (value || '').replace(/[^\d]/g, '').match(/.{1,2}/g) || [];
   const [month = '', partialYear = ''] = matches;
   // We don't expect this to still run in the next century
-  const year = partialYear.length ? `20${partialYear}` : partialYear;
+  const year = partialYear.length
+    ? `${YEAR_PREFIX}${partialYear}`
+    : partialYear;
   return { month, year };
 };
 
 export const formatDate = ({ month, year }) =>
   year && year.length ? `${month}/${year}` : month;
+
+const validateIncompleteDate = ({ month, year }) =>
+  month.length < 2 || year.length < 4;
+
+const validatePastDate = ({ month, year }) => {
+  const isPastMonth = parseInt(month, 10) < CURRENT_MONTH;
+  const isPastYear = parseInt(year, 10) < CURRENT_YEAR;
+  return isPastMonth || isPastYear;
+};
+
+export const validate = expiryDate => {
+  const incomplete = validateIncompleteDate(expiryDate);
+  const past = validatePastDate(expiryDate);
+  const activeErrors = pickBy(identity, { incomplete, past });
+  return keys(activeErrors).length === 0 ? null : activeErrors;
+};
--- src/components/CreditCardDetails/components/CardExpiryDate/CardExpiryDateService.spec.js
+++ src/components/CreditCardDetails/components/CardExpiryDate/CardExpiryDateService.spec.js
@@ -1,4 +1,4 @@
-import { parseDate, formatDate } from './CardExpiryDateService';
+import { parseDate, formatDate, validate } from './CardExpiryDateService';
 
 describe('CardExpiryDateService', () => {
   describe('parsing date input values', () => {
@@ -8,6 +8,7 @@ describe('CardExpiryDateService', () => {
         { month: '', year: '' },
         { month: '1', year: '' },
         { month: '12', year: '' },
+        // This test will break in the year 2100.
         { month: '12', year: '2023' }
       ];
       values.forEach((value, index) => {
@@ -22,6 +23,7 @@ describe('CardExpiryDateService', () => {
         { month: '1', year: '' },
         { month: '12', year: '' },
         { month: '12', year: '202' },
+        // This test will break in the year 2100.
         { month: '12', year: '2023' }
       ];
       values.forEach((value, index) => {
@@ -58,6 +60,45 @@ describe('CardExpiryDateService', () => {
       const actual = formatDate(model);
       const expected = '12/2';
       expect(actual).toBe(expected);
+    });
+  });
+
+  describe('validating expiry dates', () => {
+    const today = new Date();
+    const currentMonth = today.getMonth() + 1;
+    const currentYear = today.getFullYear();
+
+    it('should detect the current month as valid', () => {
+      const month = currentMonth < 10 ? `0${currentMonth}` : `${currentMonth}`;
+      const year = `${currentYear}`;
+      const actual = validate({ month, year });
+      expect(actual).toBeNull();
+    });
+
+    it('should detect dates with incomplete months', () => {
+      const date = { month: '1', year: '' };
+      const actual = validate(date).incomplete;
+      expect(actual).toBeTruthy();
+    });
+
+    it('should detect dates with incomplete years', () => {
+      const date = { month: '01', year: '202' };
+      const actual = validate(date).incomplete;
+      expect(actual).toBeTruthy();
+    });
+
+    it('should detect past dates within the current year', () => {
+      const month = currentMonth === 1 ? '12' : `${currentMonth - 1}`;
+      const year = currentYear;
+      const actual = validate({ month, year }).past;
+      expect(actual).toBeTruthy();
+    });
+
+    it('should detect past dates in a past year', () => {
+      const month = `${currentMonth}`;
+      const year = `${currentYear - 1}`;
+      const actual = validate({ month, year }).past;
+      expect(actual).toBeTruthy();
     });
   });
 });
