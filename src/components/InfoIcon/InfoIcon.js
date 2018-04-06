46f2f65a3ccdc772e91daa44c625a12fcda43dc0
Felix Jung <felix.jung@sumup.com>
2018-04-06 15:11:45 +0200

    Remove sizing from InfoIcon

--- src/components/InfoIcon/InfoIcon.js
+++ src/components/InfoIcon/InfoIcon.js
@@ -1,17 +1,14 @@
 import styled, { css } from 'react-emotion';
-import { size } from 'polished';
 
 import Icon from './info.svg';
 
 const baseStyles = ({ theme }) => {
   const color = theme.colors.n500;
-  const iconSize = theme.iconSizes.kilo;
   return css`
     label: info-icon;
     border: 1px solid ${color};
-    border-radius: ${iconSize};
+    border-radius: 100%;
     fill: ${color};
-    ${size(iconSize)};
   `;
 };
 
