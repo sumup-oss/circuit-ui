43d6b7994a1a59460e1378f181237944cab1af0b
Felix Jung <felix@jung.fm>
2018-04-14 15:47:42 +0200

    Add RestrictedInput unit tests

--- src/components/RestrictedInput/RestrictedInput.spec.js
+++ src/components/RestrictedInput/RestrictedInput.spec.js
@@ -3,11 +3,30 @@ import React from 'react';
 import RestrictedInput from '.';
 
 describe('RestrictedInput', () => {
-  it.skip('should render an Input', () => {});
+  it('should render an Input', () => {
+    const wrapper = shallow(<RestrictedInput enabledKeys={[]} />);
+    const themedInput = wrapper.find('WithTheme(Input)');
+    expect(themedInput).toHaveLength(1);
+  });
 
-  it.skip('should overwrite the keyDown handler on the Input', () => {});
+  it('should overwrite the keyDown handler on the Input', () => {
+    const wrapper = shallow(<RestrictedInput enabledKeys={[]} />);
+    const themedInput = wrapper.find('WithTheme(Input)');
+    const actual = themedInput.props('onKeyDown');
+    expect(actual).toBeTruthy();
+  });
 
-  it.skip('should overwrite the focus handler on the input', () => {});
+  it('should overwrite the focus handler on the input', () => {
+    const wrapper = shallow(<RestrictedInput enabledKeys={[]} />);
+    const themedInput = wrapper.find('WithTheme(Input)');
+    const actual = themedInput.props('onFocus');
+    expect(actual).toBeTruthy();
+  });
 
-  it.skip('should overwrite the mouseUp handler on the Input', () => {});
+  it('should overwrite the mouseUp handler on the Input', () => {
+    const wrapper = shallow(<RestrictedInput enabledKeys={[]} />);
+    const themedInput = wrapper.find('WithTheme(Input)');
+    const actual = themedInput.props('onMouseUp');
+    expect(actual).toBeTruthy();
+  });
 });
