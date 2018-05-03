18779c8020dd608868a1cb713235c3adb5fe75f0
Diyan Chuburov <diyan.chuburov@sumup.com>
2018-05-03 17:19:37 +0200

    Fix eslint error

--- src/components/Calendar/SingleDayPicker.story.js
+++ src/components/Calendar/SingleDayPicker.story.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { withStateHandlers } from 'recompose';
