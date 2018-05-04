694025d9fa3d260b6dd7f05d155b5799a333e54d
Fernando <fernandofleuryg@gmail.com>
2018-05-04 17:41:53 +0200

    Update Col/Row stories

--- src/components/Col/Col.story.js
+++ src/components/Col/Col.story.js
@@ -1,10 +1,34 @@
 import React from 'react';
+import styled from 'react-emotion';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
+
+import { standard } from '../../themes';
 
 import withTests from '../../util/withTests';
 import Col from './Col';
 
+const StyledCol = styled(Col)`
+  background-color: ${standard.colors.b500};
+  color: ${standard.colors.white};
+  font-size: 14px;
+  font-weight: bold;
+  line-height: 20px;
+  height: 40px;
+  padding: 10px;
+`;
+
+StyledCol.defaultProps = {
+  skip: '0'
+};
+
 storiesOf('Col', module)
   .addDecorator(withTests('Col'))
-  .add('Default Col', withInfo()(() => <Col />));
+  .add(
+    'Default Col',
+    withInfo()(() => (
+      <div style={{ width: '100vw' }}>
+        <StyledCol span="12">Default Column</StyledCol>
+      </div>
+    ))
+  );
