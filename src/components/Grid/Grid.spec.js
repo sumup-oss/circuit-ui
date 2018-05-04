909e1ee5ce4ffffa39c781e4a9f429edb693e620
Fernando <fernandofleuryg@gmail.com>
2018-05-04 13:26:13 +0200

    Add Grid specs

--- src/components/Grid/Grid.spec.js
+++ src/components/Grid/Grid.spec.js
@@ -1,3 +1,22 @@
+import React from 'react';
+
+import Grid from '.';
+
 describe('Grid', () => {
-  it.skip('should', () => {});
+  /**
+   * Style tests.
+   */
+  it('should render with default styles', () => {
+    const actual = create(<Grid />);
+    expect(actual).toMatchSnapshot();
+  });
+
+  /**
+   * Accessibility tests.
+   */
+  it('should meet accessibility guidelines', async () => {
+    const wrapper = renderToHtml(<Grid />);
+    const actual = await axe(wrapper);
+    expect(actual).toHaveNoViolations();
+  });
 });
