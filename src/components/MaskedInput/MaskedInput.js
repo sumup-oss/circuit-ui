28296a64b1a5d7a8454539cc10f3476f61decde4
Felix Jung <felix.jung@sumup.com>
2018-05-07 15:27:48 +0200

    Make MaskedInput a controlled input

--- src/components/MaskedInput/MaskedInput.js
+++ src/components/MaskedInput/MaskedInput.js
@@ -8,12 +8,24 @@ import Input from '../Input';
  * the input value. Refer to the
  * [README](https://github.com/text-mask/text-mask/tree/master/react#readme)
  * of text-mask for details regarding usage.
+ *
+ * React Text Mask uses `defaultValue` instead of `value`, which
+ * causes issues with controlled inputs. We remap it to make it
+ * work more nicely with Final Form.
+ * This might cause issues with IE10 (https://github.com/text-mask/text-mask/issues/349#issuecomment-282612339).
+ * There are some more discussions around this problem (https://github.com/text-mask/text-mask/issues/530).
  */
 const MaskedInput = props => (
   <TextMaskInput
     guide={false}
     {...props}
-    render={(ref, renderProps) => <Input {...renderProps} deepRef={ref} />}
+    render={(ref, { defaultValue, ...renderProps }) => (
+      <Input
+        value={defaultValue}
+        {...renderProps}
+        deepRef={ref}
+      />
+    )}
   />
 );
 
