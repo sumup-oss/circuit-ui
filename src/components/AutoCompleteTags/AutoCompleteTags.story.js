796e3594afac87617dfbf371f894590da0a85d0c
Diyan Chuburov <diyan.chuburov@sumup.com>
2018-05-10 18:07:36 +0200

    Story with many tags provided

--- src/components/AutoCompleteTags/AutoCompleteTags.story.js
+++ src/components/AutoCompleteTags/AutoCompleteTags.story.js
@@ -6,18 +6,33 @@ import { action } from '@storybook/addon-actions';
 import withTests from '../../util/withTests';
 import AutoCompleteTags from './AutoCompleteTags';
 
+const randomItems = [];
+const mails = ['@sumup.com', '@gmail.com', '@hotmail.com'];
+const names = ['liam', 'josh', 'tom', 'adam', 'aaron', 'john', 'killjoy'];
+const pushRandom = () => {
+  const name =
+    names[Math.floor(Math.random() * names.length)] +
+    Math.floor(Math.random() * 20000) +
+    mails[Math.floor(mails.length * Math.random())];
+
+  if (randomItems.includes(name)) {
+    pushRandom();
+  } else {
+    randomItems.push(name);
+  }
+};
+for (let i = 0; i < 10000; i += 1) {
+  pushRandom();
+}
+
 storiesOf('AutoCompleteTags', module)
   .addDecorator(withTests('AutoCompleteTags'))
   .add(
     'Default AutoCompleteTags',
     withInfo()(() => (
-      <div style={{ width: 'initial' }}>
+      <div style={{ width: '300px' }}>
         <AutoCompleteTags
-          availableTags={[
-            'liam.murphy@sumup.com',
-            'liam.burdock@sumup.com',
-            'lilijane.giordano@sumup.com'
-          ]}
+          availableTags={randomItems}
           handleChange={action('handleChange')}
         />
       </div>
