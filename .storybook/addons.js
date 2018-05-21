36f08ca37bb325c118117ea4364c8cf19721f4b5
Monica Lent <lent.monica@gmail.com>
2018-05-21 14:58:42 +0200

    Update storybook to 3.4, add storysource addon.

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -1,3 +1,5 @@
+import '@storybook/addon-options/register';
+import '@storybook/addon-storysource/register';
 import '@storybook/addon-actions/register';
 import '@storybook/addon-links/register';
 import '@storybook/addon-jest/register';
---
+++ .storybook/hierarchySeparators.js
@@ -0,0 +1,13 @@
+export const GROUPS = {
+  COMPONENTS: 'Components',
+  GRID: 'Grid',
+  FORMS: 'Forms',
+  LAYOUT: 'Layout',
+  TYPOGRAPHY: 'Typography',
+  ICONS: 'Icons'
+};
+
+export const OPTIONS = {
+  hierarchySeparator: /\//,
+  hierarchyRootSeparator: /\|/
+};
