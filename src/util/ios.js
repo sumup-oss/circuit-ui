70221061f144b7a03347748e678617d3c68c6e33
Connor Bär <connor.baer@sumup.com>
2018-06-01 14:12:07 +0300

    Safely access window.navigator

--- src/util/ios.js
+++ src/util/ios.js
@@ -1,5 +1,10 @@
-const { platform } = navigator || {};
-
-const isIos = !!platform && /iPad|iPhone|iPod/.test(platform);
+const isIos = (() => {
+  // For SSR compatibility
+  if (typeof window === 'undefined') {
+    return undefined;
+  }
+  const { platform } = window.navigator || {};
+  return !!platform && /iPad|iPhone|iPod/.test(platform);
+})();
 
 export default isIos;
