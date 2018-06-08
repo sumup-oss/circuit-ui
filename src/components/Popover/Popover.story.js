7b78dfe3ac15bffd338a6242bc7fde4ee5b2496b
elanoism <diyan.chuburov@sumup.com>
2018-06-08 03:28:25 +0300

    Add story hierarchies and onClose callback

--- src/components/Popover/Popover.story.js
+++ src/components/Popover/Popover.story.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
 import { select } from '@storybook/addon-knobs/react';
+import { action } from '@storybook/addon-actions';
+
+import { GROUPS } from '../../../.storybook/hierarchySeparators';
 
 import withTests from '../../util/withTests';
 import Popover from './Popover';
@@ -10,13 +13,14 @@ import Button from '../Button';
 const positions = [Popover.TOP, Popover.BOTTOM, Popover.LEFT, Popover.RIGHT];
 const alignments = [Popover.START, Popover.END, Popover.CENTER];
 
-storiesOf('Popover', module)
+storiesOf(`${GROUPS.COMPONENTS}|Popover`, module)
   .addDecorator(withTests('Popover'))
   .add(
     'Default Popover',
     withInfo()(() => (
       <div>
         <Popover
+          onClose={action('onClose')}
           position={select('position', positions, Popover.BOTTOM)}
           align={select('align', alignments, Popover.START)}
           renderPopover={() => (
