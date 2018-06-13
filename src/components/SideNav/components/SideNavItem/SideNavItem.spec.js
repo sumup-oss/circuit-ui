fc0cb426cf7111fa829bedf2e1bcb24836463bbb
Felix Jung <felix.jung@sumup.com>
2018-06-13 18:25:26 +0200

    Add SideNavItem unit tests

--- src/components/SideNav/components/SideNavItem/SideNavItem.spec.js
+++ src/components/SideNav/components/SideNavItem/SideNavItem.spec.js
@@ -2,27 +2,42 @@ import React from 'react';
 
 import SideNavItem from './SideNavItem';
 
-describe.skip('SideNavItem', () => {
-  /**
-   * Style tests.
-   */
-  it('should render with default styles', () => {
-    const actual = create(<SideNavItem />);
-    expect(actual).toMatchSnapshot();
+import FavoriteIcon from '../icons/favorite.svg';
+
+describe('SideNavItem', () => {
+  describe('style tests', () => {
+    it('should render with default styles', () => {
+      const actual = create(
+        <SideNavItem icon={FavoriteIcon}>Label</SideNavItem>
+      );
+      expect(actual).toMatchSnapshot();
+    });
+
+    it('should render with active styles', () => {
+      const actual = create(
+        <SideNavItem isActive icon={FavoriteIcon}>
+          Label
+        </SideNavItem>
+      );
+      expect(actual).toMatchSnapshot();
+    });
   });
 
-  /**
-   * Logic tests.
-   */
+  describe('logic tests', () => {
+    it('should not render an icon when no icon is passed', () => {
+      const wrapper = shallow(<SideNavItem isActive>Label</SideNavItem>).dive();
+      const actual = wrapper.find('IconWrapper');
+      expect(actual).toBeEmpty();
+    });
+  });
 
-  it('should have tests');
-
-  /**
-   * Accessibility tests.
-   */
-  it('should meet accessibility guidelines', async () => {
-    const wrapper = renderToHtml(<SideNavItem />);
-    const actual = await axe(wrapper);
-    expect(actual).toHaveNoViolations();
+  describe('accessibility tests', () => {
+    it('should meet accessibility guidelines', async () => {
+      const wrapper = renderToHtml(
+        <SideNavItem icon={FavoriteIcon}>Label</SideNavItem>
+      );
+      const actual = await axe(wrapper);
+      expect(actual).toHaveNoViolations();
+    });
   });
 });
