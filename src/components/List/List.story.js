8a2525cb0f67ad5441e4a123e418be8fd7a510a9
Connor Bär <connor.baer@sumup.com>
2018-06-26 22:15:58 +0200

    Add giga size to List

--- src/components/List/List.story.js
+++ src/components/List/List.story.js
@@ -1,59 +1,26 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
+import { boolean, select } from '@storybook/addon-knobs';
 import { GROUPS } from '../../../.storybook/hierarchySeparators';
 
 import withTests from '../../util/withTests';
 import List from './List';
 
+const sizes = [List.KILO, List.MEGA, List.GIGA];
+
 storiesOf(`${GROUPS.COMPONENTS}|List`, module)
   .addDecorator(withTests('List'))
   .add(
-    'List default',
+    'List',
     withInfo()(() => (
-      <List>
+      <List size={select('Size', sizes, sizes[0])} ordered={boolean('Ordered')}>
         <li>This is a list</li>
         <li>A very fine list</li>
-        <List>
-          <li>Sometimes a nested list</li>
-        </List>
-        <li>The kind of list you like</li>
-      </List>
-    ))
-  )
-  .add(
-    'List ordered',
-    withInfo()(() => (
-      <List ordered>
-        <li>This is a list</li>
-        <li>A very fine list</li>
-        <List>
-          <li>Sometimes a nested list</li>
-        </List>
-        <li>The kind of list you like</li>
-      </List>
-    ))
-  )
-  .add(
-    'List kilo',
-    withInfo()(() => (
-      <List size={List.KILO}>
-        <li>This is a list</li>
-        <li>A very fine list</li>
-        <List size={List.KILO}>
-          <li>Sometimes a nested list</li>
-        </List>
-        <li>The kind of list you like</li>
-      </List>
-    ))
-  )
-  .add(
-    'List mega',
-    withInfo()(() => (
-      <List size={List.MEGA}>
-        <li>This is a list</li>
-        <li>A very fine list</li>
-        <List size={List.MEGA}>
+        <List
+          size={select('Size', sizes, sizes[0])}
+          ordered={boolean('Ordered')}
+        >
           <li>Sometimes a nested list</li>
         </List>
         <li>The kind of list you like</li>
