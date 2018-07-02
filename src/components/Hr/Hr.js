64e51d2413136a6637698ef8e0e16ec49f43f2f7
Connor Bär <connor-baer@users.noreply.github.com>
2018-07-02 10:57:40 +0200

    Add horizontal rule (#217)

---
+++ src/components/Hr/Hr.js
@@ -0,0 +1,18 @@
+import styled, { css } from 'react-emotion';
+
+const baseStyles = ({ theme }) => css`
+  label: hr;
+  border: 1px solid ${theme.colors.n300};
+  margin-top: ${theme.spacings.mega};
+  margin-bottom: ${theme.spacings.mega};
+`;
+
+/**
+ * A horizontal rule to visually and semantically separate text.
+ */
+const Hr = styled('hr')(baseStyles);
+
+/**
+ * @component
+ */
+export default Hr;
---
+++ src/components/Hr/Hr.story.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import styled from 'react-emotion';
+import { storiesOf } from '@storybook/react';
+import { withInfo } from '@storybook/addon-info';
+
+import { GROUPS } from '../../../.storybook/hierarchySeparators';
+import Text from '../Text';
+import Hr from './Hr';
+
+const Container = styled('div')`
+  width: 500px;
+  max-width: 90%;
+`;
+
+storiesOf(`${GROUPS.COMPONENTS}|Hr`, module)
+  .add(
+    'Hr',
+    withInfo()(() => (
+      <Container>
+        <Hr />
+      </Container>
+    ))
+  )
+  .add(
+    'Hr with Text',
+    withInfo()(() => (
+      <Container>
+        <Text>
+          Lorem ipsum dolor amet echo park XOXO activated charcoal banjo deep v
+          crucifix pinterest yr af tumeric literally. Tbh four loko tattooed
+          kickstarter artisan. Lumbersexual tote bag selfies truffaut, tofu vape
+          tbh adaptogen green juice lo-fi kombucha.
+        </Text>
+        <Hr />
+        <Text>
+          Roof party cronut seitan pitchfork keytar small batch migas ugh XOXO
+          kickstarter pork belly tumblr. Taiyaki brunch vegan XOXO meggings.
+          Kinfolk air plant edison bulb vexillologist helvetica chambray disrupt
+          mixtape man braid banjo viral.
+        </Text>
+      </Container>
+    ))
+  );
