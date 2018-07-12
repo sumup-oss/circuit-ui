3a9f0fd0c941d351516e99201d362ead07bc7d8d
Ilya Noskov <ilya.noskov@sumup.com>
2018-07-12 13:58:29 +0200

    trying out docz

--- doczrc.js
+++ doczrc.js
@@ -2,11 +2,5 @@ import { babel } from 'docz-plugin-babel6';
 
 export default {
   dist: './dist',
-  plugins: [babel()],
-  modifyBabelRc: config => {
-    config.plugins.push('react-docgen');
-    console.log(config);
-
-    return config;
-  }
+  plugins: [babel()]
 };
