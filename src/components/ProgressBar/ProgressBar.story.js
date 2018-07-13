ea142e51a624ad1e2e591df9dfceb2e0f30b82c2
Lyubomir Georgiev <lyubomir.georgiev@sumup.com>
2018-07-13 17:05:51 +0300

    [ProgressBar] Change default story and update knobs

--- src/components/ProgressBar/ProgressBar.story.js
+++ src/components/ProgressBar/ProgressBar.story.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withInfo } from '@storybook/addon-info';
-import { select, number, text } from '@storybook/addon-knobs/react';
+import { select, number, text, boolean } from '@storybook/addon-knobs/react';
 import { GROUPS } from '../../../.storybook/hierarchySeparators';
 
 import withTests from '../../util/withTests';
@@ -11,32 +11,6 @@ storiesOf(`${GROUPS.COMPONENTS}|ProgressBar`, module)
   .addDecorator(withTests('ProgressBar'))
   .add(
     'ProgressBar',
-    withInfo()(() => (
-      <div style={{ width: '25vw' }}>
-        <ProgressBar value={3} max={10} size={ProgressBar.GIGA} />
-        <ProgressBar value={5} max={10} size={ProgressBar.MEGA} />
-        <ProgressBar value={8} max={10} size={ProgressBar.KILO} />
-      </div>
-    ))
-  )
-  .add(
-    'ProgressBar with label',
-    withInfo()(() => (
-      <div style={{ width: '25vw' }}>
-        <ProgressBar value={3} max={10} size={ProgressBar.GIGA}>
-          3/10
-        </ProgressBar>
-        <ProgressBar value={5} max={10} size={ProgressBar.MEGA}>
-          50%
-        </ProgressBar>
-        <ProgressBar value={8} max={10} size={ProgressBar.KILO}>
-          Loading...
-        </ProgressBar>
-      </div>
-    ))
-  )
-  .add(
-    'ProgressBar with knobs',
     withInfo()(() => {
       const size = select(
         'Size',
@@ -47,9 +21,13 @@ storiesOf(`${GROUPS.COMPONENTS}|ProgressBar`, module)
         },
         ProgressBar.KILO
       );
-      const max = number('Maximum number', 10);
+      const max = number('Maximum value', 10);
       const value = number('Value', 5);
-      const children = text('Text value', `${value}/${max}`);
+      const percentage = boolean('Label in percentage', false);
+      const defaultLabel = percentage
+        ? `${value / max * 100}%`
+        : `${value}/${max}`;
+      const children = text('Label', defaultLabel);
       return (
         <div style={{ width: '25vw' }}>
           <ProgressBar value={value} max={max} size={size}>
