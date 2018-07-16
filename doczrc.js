da51867b18d1034a65871ea75cc09008a440f31f
Ilya Noskov <ilya.noskov@sumup.com>
2018-07-16 11:55:41 +0200

    adding a proptable component

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,18 @@
 import { babel } from 'docz-plugin-babel6';
 
 export default {
+<<<<<<< HEAD
   dist: './dist',
   plugins: [babel()]
+=======
+  plugins: [babel()],
+  modifyBundlerConfig: config => {
+    let newConfig = config;
+    newConfig.module.rules.push({
+      test: /\.txt/,
+      use: 'raw-loader'
+    });
+    return newConfig;
+  }
+>>>>>>> 5726436... adding a proptable component
 };
