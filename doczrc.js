dfe65aa7729542343294d138b4984d7b889dc824
Ilya Noskov <ilya.noskov@sumup.com>
2018-07-18 13:34:40 +0200

    moving PropTable and service functions to utils folder

--- doczrc.js
+++ doczrc.js
@@ -1,6 +1,16 @@
 import { babel } from 'docz-plugin-babel6';
 
 export default {
+<<<<<<< HEAD
   dest: './dist',
   plugins: [babel()]
+=======
+  plugins: [babel()],
+  modifyBabelRc: config => {
+    config.plugins.push('react-docgen');
+    console.log(config);
+
+    return config;
+  }
+>>>>>>> eb51365... moving PropTable and service functions to utils folder
 };
