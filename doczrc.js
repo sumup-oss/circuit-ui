cf2c2a07d48f86982f30c0d4da244dcf109e32d3
Ilya Noskov <ilya.noskov@sumup.com>
2018-07-19 15:40:09 +0200

    adding new build procedure including docz

--- doczrc.js
+++ doczrc.js
@@ -1,16 +1,6 @@
 import { babel } from 'docz-plugin-babel6';
 
 export default {
-<<<<<<< HEAD
   dist: './dist',
   plugins: [babel()]
-=======
-  plugins: [babel()],
-  modifyBabelRc: config => {
-    config.plugins.push('react-docgen');
-    console.log(config);
-
-    return config;
-  }
->>>>>>> 5726436... adding a proptable component
 };
