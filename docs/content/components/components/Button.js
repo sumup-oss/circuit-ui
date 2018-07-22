5e2cb2f37a649c2abf48c1e5e7a2c7b21724e156
Monica Lent <lent.monica@gmail.com>
2018-07-22 10:31:16 +0200

    Organize wrapper components into child dir.

--- docs/content/components/components/Button.js
+++ docs/content/components/components/Button.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ThemeProvider } from 'emotion-theming';
-import OriginalButton from '../../../src/components/Button/Button';
-import { standard } from '../../../src/themes/index';
+import OriginalButton from '../../../../src/components/Button';
+import { standard } from '../../../../src/themes';
 
 const DocButton = () => (
   <ThemeProvider theme={standard}>
