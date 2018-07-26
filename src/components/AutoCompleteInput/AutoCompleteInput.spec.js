1e2adec2add0f0cb93243509ff822a00ac844ce7
Diyan Chuburov <diyan.chuburov@sumup.com>
2018-07-26 16:00:35 +0300

    Add test

--- src/components/AutoCompleteInput/AutoCompleteInput.spec.js
+++ src/components/AutoCompleteInput/AutoCompleteInput.spec.js
@@ -7,9 +7,27 @@ describe('AutoCompleteInput', () => {
    * Style tests.
    */
   it('should render with default styles', () => {
-    const actual = create(
-      <AutoCompleteInput handleChange={() => {}} items={[]} />
-    );
+    const actual = create(<AutoCompleteInput onChange={() => {}} items={[]} />);
     expect(actual).toMatchSnapshot();
+  });
+
+  /**
+   * Logic tests.
+   */
+  it('should filter items when input is changed', () => {
+    const handleChange = jest.fn();
+    const wrapper = mount(
+      <AutoCompleteInput
+        onChange={handleChange}
+        items={['1111111111111', '2222222222222', '3333333333333']}
+      />
+    );
+
+    wrapper.find('input').simulate('change', { target: { value: '222' } });
+
+    const item = wrapper.find('Items Item');
+
+    expect(item).toHaveLength(1);
+    expect(item.text()).toEqual('2222222222222');
   });
 });
