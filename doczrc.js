8c69f2781e41b2f55d0c1e235567916d3dc1ea2e
Ilya Noskov <ilya.noskov@sumup.com>
2018-08-02 16:49:52 +0200

    fixing Button propTypes

--- doczrc.js
+++ doczrc.js
@@ -1,12 +1,6 @@
 import { babel } from 'docz-plugin-babel6';
 
 export default {
-  dist: './dist',
   plugins: [babel()],
-  modifyBabelRc: config => {
-    config.plugins.push('react-docgen');
-    console.log(config);
-
-    return config;
-  }
+  modifyBabelRc: config => config
 };
--- prettier.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  singleQuote: true,
-  tabWidth: 2,
-  useTabs: false,
-  trailingComma: 'none',
-  bracketSpacing: true,
-  jsxBracketSameLine: false
-};
