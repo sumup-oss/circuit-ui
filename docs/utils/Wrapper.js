6c45fceb1fba6a516f06fc8856cf55a1d4b06be8
Ilya Noskov <ilya.noskov@sumup.com>
2018-08-03 10:31:29 +0200

    fixing component.mdx

--- docs/utils/Wrapper.js
+++ docs/utils/Wrapper.js
@@ -1,4 +1,7 @@
+/* eslint-disable */
+
 import React from 'react';
+import PropTypes from 'prop-types';
 import { ThemeProvider } from 'emotion-theming';
 import { standard } from '../../src/themes';
 
