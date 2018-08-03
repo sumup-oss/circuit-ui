49fed24c4fd7a0eb1867346350b1ef686681028e
Ilya Noskov <ilya.noskov@sumup.com>
2018-08-03 11:38:21 +0200

    updating how-to-document.mdx

---
+++ .docz/app/imports.js
@@ -0,0 +1,38 @@
+export const imports = {
+  'docs/content/changelog.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-content-changelog" */ 'docs/content/changelog.mdx'),
+  'docs/content/index.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-content-index" */ 'docs/content/index.mdx'),
+  'docs/content/principles.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-content-principles" */ 'docs/content/principles.mdx'),
+  'docs/content/component-status/index.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-content-component-status-index" */ 'docs/content/component-status/index.mdx'),
+  'docs/content/components/button.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-content-components-button" */ 'docs/content/components/button.mdx'),
+  'docs/content/components/component.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-content-components-component" */ 'docs/content/components/component.mdx'),
+  'docs/content/getting-started/designers.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-content-getting-started-designers" */ 'docs/content/getting-started/designers.mdx'),
+  'docs/content/getting-started/developers.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-content-getting-started-developers" */ 'docs/content/getting-started/developers.mdx'),
+  'docs/content/getting-started/faq.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-content-getting-started-faq" */ 'docs/content/getting-started/faq.mdx'),
+  'docs/content/getting-started/how-to-document.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-content-getting-started-how-to-document" */ 'docs/content/getting-started/how-to-document.mdx'),
+  'docs/content/guidelines/accessibility.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-content-guidelines-accessibility" */ 'docs/content/guidelines/accessibility.mdx'),
+  'docs/content/guidelines/fonts.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-content-guidelines-fonts" */ 'docs/content/guidelines/fonts.mdx'),
+  'docs/content/guidelines/media-queries.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-content-guidelines-media-queries" */ 'docs/content/guidelines/media-queries.mdx'),
+  'docs/content/styles/colors.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-content-styles-colors" */ 'docs/content/styles/colors.mdx'),
+  'docs/content/styles/grid.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-content-styles-grid" */ 'docs/content/styles/grid.mdx'),
+  'docs/content/styles/spacing.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-content-styles-spacing" */ 'docs/content/styles/spacing.mdx'),
+  'docs/content/styles/theming.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-content-styles-theming" */ 'docs/content/styles/theming.mdx'),
+  'docs/content/styles/typography.mdx': () =>
+    import(/* webpackPrefetch: true, webpackChunkName: "docs-content-styles-typography" */ 'docs/content/styles/typography.mdx'),
+}
