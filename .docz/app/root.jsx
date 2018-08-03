853d272fe514efc7a95521371a3c958a5142d44f
Ilya Noskov <ilya.noskov@sumup.com>
2018-08-03 10:17:30 +0200

    updating docz version, now we are ready for production

--- .docz/app/root.jsx
+++ .docz/app/root.jsx
@@ -1,34 +1,10 @@
-import React from 'react';
-import { hot } from 'react-hot-loader';
-import Theme from 'docz-theme-default';
+import React from 'react'
+import { hot } from 'react-hot-loader'
+import Theme from 'docz-theme-default'
 
-const socket = new WebSocket(`ws://127.0.0.1:8089`);
+import { imports } from './imports'
+import db from './db.json'
 
-class Root extends React.Component {
-  state = {
-    config: {},
-    entries: {}
-  };
+const Root = () => <Theme db={db} imports={imports} hashRouter={false} />
 
-  async componentDidMount() {
-    socket.onmessage = ev => {
-      const message = JSON.parse(ev.data);
-
-      if (message.type === 'docz.entries') {
-        this.setState({ entries: message.data });
-      }
-
-      if (message.type === 'docz.config') {
-        this.setState({ config: message.data });
-      }
-    };
-  }
-
-  render() {
-    const { imports } = this.props;
-
-    return <Theme {...this.state} imports={imports} hashRouter={false} />;
-  }
-}
-
-export default hot(module)(Root);
+export default hot(module)(Root)
