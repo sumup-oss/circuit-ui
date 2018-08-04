d79e138898616fc97169ae52d4d69b43c210935b
Alexander Marinov <aleksandar.marinov@sumup.com>
2018-08-04 19:08:37 +0300

    Remove unused transitions

--- src/styles/transitions.js
+++ src/styles/transitions.js
@@ -3,20 +3,8 @@
 
 import warning from 'warning';
 
-// Follow https://material.google.com/motion/duration-easing.html#duration-easing-natural-easing-curves
-// to learn the context in which each easing should be used.
 export const easing = {
-  // Legacy
-  default: `200ms ease-in-out`,
-  // This is the most common easing curve.
-  easeInOut: 'cubic-bezier(0.4, 0, 0.2, 1)',
-  // Objects enter the screen at full velocity from off-screen and
-  // slowly decelerate to a resting point.
-  easeOut: 'cubic-bezier(0.0, 0, 0.2, 1)',
-  // Objects leave the screen at full velocity. They do not decelerate when off-screen.
-  easeIn: 'cubic-bezier(0.4, 0, 1, 1)',
-  // The sharp curve is used by objects that may return to the screen at any time.
-  sharp: 'cubic-bezier(0.4, 0, 0.6, 1)'
+  default: `200ms ease-in-out`
 };
 
 // Follow https://material.io/guidelines/motion/duration-easing.html#duration-easing-common-durations
@@ -67,7 +55,7 @@ export default {
   create(props = ['all'], options = {}) {
     const {
       duration: durationOption = duration.standard,
-      easing: easingOption = easing.easeInOut,
+      easing: easingOption = easing.default,
       delay = 0,
       ...other
     } = options;
