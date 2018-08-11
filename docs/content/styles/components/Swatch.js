321802db9b43fe37e71f0dab7867862a04af6021
Monica Lent <lent.monica@gmail.com>
2018-08-11 21:34:46 +0200

    Improve color swatches.

--- docs/content/styles/components/Swatch.js
+++ docs/content/styles/components/Swatch.js
@@ -3,35 +3,62 @@ import PropTypes from 'prop-types';
 import styled, { css } from 'react-emotion';
 import { ThemeProvider } from 'emotion-theming';
 import { standard } from '../../../../src/themes';
+import { shadowSingle } from '../../../../src/styles/style-helpers';
+import Text from '../../../../src/components/Text';
 
-const SWATCH_SIZE = '150px';
+const SWATCH_HEIGHT = '150px';
+const SWATCH_WIDTH = '135px';
 
 const Color = styled('div')`
   ${({ theme, colorName }) => css`
-    width: ${SWATCH_SIZE};
-    height: ${SWATCH_SIZE};
-    border-radius: ${theme.borderRadius.giga};
+    width: ${SWATCH_HEIGHT};
+    height: ${SWATCH_WIDTH};
+    border-top-left-radius: ${theme.borderRadius.giga};
+    border-top-right-radius: ${theme.borderRadius.giga};
     background-color: ${theme.colors[colorName]};
+    display: inline-block;
+  `};
+`;
+
+const ColorName = styled('div')`
+  ${({ theme, colorName }) => css`
+    display: flex;
+    justify-content: space-between;
+    padding: ${theme.spacings.mega};
+    content: '${colorName}';
+  `};
+`;
+
+const ColorHex = styled(Text)`
+  ${({ theme }) => css`
+    color: ${theme.colors.n500};
+  `};
+`;
+
+const ColorWrapper = styled('div')`
+  ${({ theme }) => css`
     display: inline-block;
     margin-right: ${theme.spacings.mega};
     margin-bottom: ${theme.spacings.mega};
-    position: relative;
-
-    &:before {
-      content: '${colorName}';
-      position: absolute;
-      background-color: ${theme.colors.n100};
-      padding: ${theme.spacings.byte} ${theme.spacings.mega};
-      border-radius: ${theme.borderRadius.mega};
-      top: ${theme.spacings.kilo};
-      right: ${theme.spacings.kilo};
-    }
+    border-bottom-left-radius: ${theme.borderRadius.giga};
+    border-bottom-right-radius: ${theme.borderRadius.giga};
+    ${shadowSingle({ theme })};
   `};
 `;
 
 const Swatch = ({ colorName }) => (
   <ThemeProvider theme={standard}>
-    <Color colorName={colorName} />
+    <ColorWrapper>
+      <Color colorName={colorName} />
+      <ColorName>
+        <ColorHex element="span" size="kilo" noMargin>
+          {standard.colors[colorName]}
+        </ColorHex>
+        <Text bold element="span" size="kilo" noMargin>
+          {colorName}
+        </Text>
+      </ColorName>
+    </ColorWrapper>
   </ThemeProvider>
 );
 
