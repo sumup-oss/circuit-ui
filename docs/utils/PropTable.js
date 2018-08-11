6f362ff4bb7e88d1038be31f024a937312f3f8f9
Monica Lent <lent.monica@gmail.com>
2018-08-11 17:44:03 +0200

    Use theme and fix default props bug in PropTable.

--- docs/utils/PropTable.js
+++ docs/utils/PropTable.js
@@ -1,55 +1,42 @@
 /* eslint-disable */
 import React from 'react';
-import styled from 'react-emotion';
+import styled, { css } from 'react-emotion';
 import { parse } from 'react-docgen';
 import Table from '../../src/components/Table';
 import { standard } from '../../src/themes/index';
 import { ThemeProvider } from 'emotion-theming';
 
-const StyledTable = styled('table')`
-  color: #999;
-  margin-top: 64px;
-  border-collapse: collapse;
+const TableWrapper = styled('div')`
+  ${({ theme }) => css`
+    overflow: scroll;
+    border: 1px solid ${theme.colors.n300};
+    border-radius: ${theme.borderRadius.mega};
+  `};
 `;
 
-const StyledTh = styled('th')`
-  font-weight: 600;
-  text-align: left;
-  color: #000;
-
-  font-size: 16px;
-  padding: 16px;
-  border: 1px solid #111111;
-`;
-
-const StyledTd = styled('td')`
-  font-size: 16px;
-  padding: 16px;
-  border: 1px solid #999;
-`;
-
-const getRows = props => Object.keys(props).map(prop => [
-  prop,
-  props[prop].type.name,
-  props[prop].required.toString(),
-  props[prop].defaultValue.value.toString(),
-  props[prop].description
-]);
+const getRows = props =>
+  Object.keys(props).map(prop => [
+    prop,
+    props[prop].type.name,
+    props[prop].required.toString(),
+    props[prop].defaultValue ? props[prop].defaultValue.value.toString() : '',
+    props[prop].description
+  ]);
 
 const PropTable = ({ component }) => {
   const parsed = component.__docgenInfo;
-  let { props, description } = parsed;
+  const { props, description } = parsed;
+
   return (
-    <div style={{ overflow: 'scroll', border: '1px solid lightgray' }}>
-      <ThemeProvider theme={standard}>
+    <ThemeProvider theme={standard}>
+      <TableWrapper>
         <Table
           headers={['Name', 'Type', 'Required', 'Default Value', 'Description']}
           rows={getRows(props)}
         />
-      </ThemeProvider>
-    </div>
+      </TableWrapper>
+    </ThemeProvider>
   );
 };
-
 
 export default PropTable;
