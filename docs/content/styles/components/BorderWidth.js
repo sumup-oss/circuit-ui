ff40872835a1d3c35fd54d37fe50662d044c630c
Monica Lent <lent.monica@gmail.com>
2018-08-12 12:54:58 +0200

    Add border width docs.

---
+++ docs/content/styles/components/BorderWidth.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled, { css } from 'react-emotion';
+import { ThemeProvider } from 'emotion-theming';
+import Text from '../../../../src/components/Text';
+import { standard } from '../../../../src/themes';
+
+const Box = styled('div')`
+  ${({ theme, size }) => css`
+    width: ${theme.spacings.tera};
+    height: ${theme.spacings.tera};
+    border-radius: ${theme.borderRadius.mega};
+    border: ${theme.borderWidth[size]} solid ${theme.colors.r500};
+    background-color: ${theme.colors.r300};
+    margin-right: ${theme.spacings.mega};
+  `};
+`;
+
+const Wrapper = styled('div')`
+  ${({ theme }) => css`
+    display: flex;
+    align-items: center;
+    margin-bottom: ${theme.spacings.mega};
+  `};
+`;
+
+const BorderWidthSize = styled('span')`
+  ${({ theme }) => css`
+    color: ${theme.colors.n500};
+  `};
+`;
+
+const BorderWidthName = styled(Text)`
+  ${({ theme }) => css`
+    margin-left: ${theme.spacings.kilo};
+    color: ${theme.colors.n500};
+  `};
+`;
+
+const BorderWidth = ({ size }) => (
+  <ThemeProvider theme={standard}>
+    <Wrapper>
+      <Box size={size} />
+      <div>
+        <Text element="span">{size}</Text>
+        <BorderWidthSize>
+          <BorderWidthName size={Text.KILO} element="span">
+            {standard.borderWidth[size]}
+          </BorderWidthName>
+        </BorderWidthSize>
+      </div>
+    </Wrapper>
+  </ThemeProvider>
+);
+
+BorderWidth.propTypes = {
+  // eslint-disable-next-line
+  theme: PropTypes.object.isRequired,
+  size: PropTypes.string.isRequired
+};
+
+export default BorderWidth;
