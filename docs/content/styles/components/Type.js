f03a55a06654c14084c791beeb6168ead5986239
Monica Lent <lent.monica@gmail.com>
2018-08-12 12:15:57 +0200

    Show pixels next to Type variables.

--- docs/content/styles/components/Type.js
+++ docs/content/styles/components/Type.js
@@ -1,21 +1,50 @@
 import React, { Fragment, createElement } from 'react';
 import PropTypes from 'prop-types';
+import styled, { css } from 'react-emotion';
 import { ThemeProvider } from 'emotion-theming';
-import { standard } from '../../../../src/themes';
+import { standard as theme } from '../../../../src/themes';
+import Text from '../../../../src/components/Text';
 
-const Type = ({ size, component, ...props }) => (
-  <ThemeProvider theme={standard}>
-    {createElement(component, {
-      children: <Fragment>A better way to get {size}.</Fragment>,
-      size,
-      ...props
-    })}
-  </ThemeProvider>
-);
+const TypePx = styled(Text)`
+  ${({ theme: t }) => css`
+    color: ${t.colors.n500};
+    margin-left: ${t.spacings.mega};
+    text-transform: lowercase;
+  `};
+`;
+
+const Type = ({ size, component, name, fontWeight, ...props }) => {
+  const typeSetting = theme.typography[name][size];
+  const { fontSize, lineHeight } = typeSetting;
+  const weight = theme.fontWeight[fontWeight];
+
+  return (
+    <ThemeProvider theme={theme}>
+      {createElement(component, {
+        children: (
+          <Fragment>
+            A better way to get {size}.
+            <TypePx element="span" size={Text.KILO}>
+              {weight ? `${weight}` : `${fontSize}, ${lineHeight}`}
+            </TypePx>
+          </Fragment>
+        ),
+        size,
+        ...props
+      })}
+    </ThemeProvider>
+  );
+};
 
 Type.propTypes = {
-  component: PropTypes.object.isRequired, // eslint-disable-line
-  size: PropTypes.string.isRequired
+  component: PropTypes.func.isRequired, // eslint-disable-line
+  size: PropTypes.string.isRequired,
+  fontWeight: PropTypes.string,
+  name: PropTypes.string.isRequired
+};
+
+Type.defaultProps = {
+  fontWeight: null
 };
 
 export default Type;
