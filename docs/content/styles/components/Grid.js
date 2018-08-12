3249b2c27d59702f3846060cf236b71c3a0106e0
Monica Lent <lent.monica@gmail.com>
2018-08-12 11:35:17 +0200

    Begin grid docs.

---
+++ docs/content/styles/components/Grid.js
@@ -0,0 +1,33 @@
+import styled, { css } from 'react-emotion';
+import OriginalRow from '../../../../src/components/Row';
+import OriginalGrid from '../../../../src/components/Grid';
+import OriginalCol from '../../../../src/components/Col';
+
+const Grid = styled(OriginalGrid)`
+  ${({ theme }) => css`
+    background-color: ${theme.colors.n100};
+  `};
+`;
+
+const Col = styled(OriginalCol)`
+  ${({ theme }) => css`
+    &:nth-of-type(n) {
+      background-color: ${theme.colors.b500};
+    }
+
+    &:nth-of-type(2n) {
+      background-color: ${theme.colors.b300};
+    }
+    color: ${theme.colors.white};
+    text-align: center;
+  `};
+`;
+
+const Row = styled(OriginalRow)`
+  ${({ theme }) => css`
+    background-color: ${theme.colors.b500};
+    color: ${theme.colors.white};
+  `};
+`;
+
+export { Grid, Col, Row };
