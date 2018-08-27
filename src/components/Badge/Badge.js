e1450d33d77a9bd895be9e455c6acebbda063ea0
Ilya Noskov <ilyanoskov@users.noreply.github.com>
2018-08-27 18:04:05 +0200

    Revert "Docz  (#267)"
    This reverts commit 4f8599548e1589f635f2c4a5402f2ec640f1dd98.

--- src/components/Badge/Badge.js
+++ src/components/Badge/Badge.js
@@ -62,7 +62,6 @@ const baseStyles = ({ theme, onClick }) => css`
   border-radius: 100px;
   color: ${theme.colors.white};
   cursor: ${onClick ? 'pointer' : 'default'};
-  display: inline-block;
   padding: 0 ${theme.spacings.byte};
   ${subHeadingKilo({ theme })};
   text-transform: uppercase;
