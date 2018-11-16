19a3345429b299e7d000e51e85eb9ff19a5a7f65
Connor Bär <connor.baer@sumup.com>
2018-11-16 17:01:40 +0100

    Don't add onKeyDown if onClick is undefined

--- src/util/withKeyboardEvents.js
+++ src/util/withKeyboardEvents.js
@@ -2,12 +2,18 @@ import { withProps } from 'recompose';
 
 import { isEnter, isSpacebar } from './key-codes';
 
-export const createOnKeyDown = onClick => event => {
-  // Most clickable HTML elements can also be triggered by pressing the
-  // spacebar or enter key.
-  if (isEnter(event) || isSpacebar(event)) {
-    onClick(event);
+export const createOnKeyDown = onClick => {
+  if (!onClick) {
+    return null;
   }
+
+  return event => {
+    // Most clickable HTML elements can also be triggered by pressing the
+    // spacebar or enter key.
+    if (isEnter(event) || isSpacebar(event)) {
+      onClick(event);
+    }
+  };
 };
 
 export default withProps(({ onClick, ...props }) => ({
