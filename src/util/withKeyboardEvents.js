e79c5b67fe941d981b62c8105668ae4e9af36ccb
Connor Bär <connor.baer@sumup.com>
2018-11-16 16:33:13 +0100

    Add HOC for keyboard events

---
+++ src/util/withKeyboardEvents.js
@@ -0,0 +1,17 @@
+import { withProps } from 'recompose';
+
+import { isEnter, isSpacebar } from './key-codes';
+
+export const createOnKeyDown = onClick => event => {
+  // Most clickable HTML elements can also be triggered by pressing the
+  // spacebar or enter key.
+  if (isEnter(event) || isSpacebar(event)) {
+    onClick(event);
+  }
+};
+
+export default withProps(({ onClick, ...props }) => ({
+  onKeyDown: createOnKeyDown(onClick),
+  onClick,
+  ...props
+}));
