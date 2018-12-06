b7017eec12b67bf0668d140a2dab67f3c1f16bd6
Felix Jung <felix.jung@sumup.com>
2018-12-06 23:15:42 +0100

    Improve App spec file

--- packages/create-sumup-react-app/files/App.spec.js
+++ packages/create-sumup-react-app/files/App.spec.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import { render } from 'react-testing-library';
 
-import App from './App.js';
+import App from './App';
 
 /**
- * Having a separate rendering function for your components
- * makes it easier to render a separate component for each
- * test and reduces boilerplate.
- *
  * defaultProps help you have sensible defaults that work
- * for most tests. You can pass in custom props to customize
- * rendering for your current test.
- * */
+ * for most tests.
+ */
 const defaultProps = {
   // This will look more complex for real component.
 };
+
+/**
+ * Having a dedicated test render function for your component
+ * makes it easy to keep your tests decoupled and reduces
+ * boilerplate.
+ */
 const renderApp = (props = {}) =>
   render(<App {...{ ...defaultProps, ...props }} />);
 
@@ -22,7 +23,7 @@ describe('App', () => {
   /**
    * Testing by what your user sees gives you more confidence
    * in your tests.
-   * */
+   */
   it('should show a title inside the card', () => {
     const { getByText } = renderApp();
     const heading = getByText('Welcome to SumUp React', { selector: 'h2' });
@@ -32,7 +33,7 @@ describe('App', () => {
   /**
    * For something like a logo, it might make sense to use a test-id
    * data attribute.
-   * */
+   */
   it('should show the SumUp logo', () => {
     const { queryByTestId } = renderApp();
     expect(queryByTestId('sumup-logo')).toBeInTheDocument();
