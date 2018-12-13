48d263f8f03bb78aa6c175b22e61b04d726fd723
Diyan Chuburov <diyan.chuburov@sumup.com>
2018-12-13 12:52:45 +0200

    Keep focus styles

--- src/components/ListView/components/Item/components/Wrapper/Wrapper.js
+++ src/components/ListView/components/Item/components/Wrapper/Wrapper.js
@@ -19,24 +19,6 @@ const baseStyles = ({ theme }) => css`
   cursor: pointer;
   border-bottom: ${theme.borderWidth.kilo} solid ${theme.colors.n300};
 
-  @media (hover: hover) {
-    &:hover,
-    &:focus {
-      outline: none;
-
-      &::after {
-        content: ' ';
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        left: 0;
-        top: 0;
-        ${shadowBorder(theme.colors.b500, theme.borderWidth.mega)};
-        border-radius: ${theme.borderRadius.mega};
-      }
-    }
-  }
-
   &:first-child {
     border-top-left-radius: ${theme.borderRadius.mega};
     border-top-right-radius: ${theme.borderRadius.mega};
@@ -62,7 +44,39 @@ const selectedStyles = ({ theme, selected }) =>
     background: ${theme.colors.p100};
   `;
 
-const Wrapper = styled('div')(baseStyles, paddingStyles, selectedStyles);
+const getBorderStyles = theme => css`
+  outline: none;
+
+  &::after {
+    content: ' ';
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    ${shadowBorder(theme.colors.b500, theme.borderWidth.mega)};
+    border-radius: ${theme.borderRadius.mega};
+  }
+`;
+
+const hoverStyles = ({ theme }) => css`
+  @media (hover: hover) {
+    &:hover {
+      ${getBorderStyles(theme)};
+    }
+  }
+
+  &:focus {
+    ${getBorderStyles(theme)};
+  }
+`;
+
+const Wrapper = styled('div')(
+  baseStyles,
+  paddingStyles,
+  selectedStyles,
+  hoverStyles
+);
 
 Wrapper.propTypes = {
   /**
