26ce7236d00797dcafd186f16300439b3a4c40b8
Felix Jung <felix@jung.fm>
2018-12-18 17:46:58 +0100

    Update Grid component

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { css } from 'emotion';
+import { css } from '@emotion/core';
 
 const getBreakPointStyles = (theme, breakpoint) => {
   const config = theme.grid[breakpoint];
