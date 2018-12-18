b332a15eea2ad4d67349f5fdde9ed65e2e4dfb2c
Felix Jung <felix@jung.fm>
2018-12-18 09:53:46 +0100

    Add emotion eslint plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,106 @@
-module.exports = require('@sumup/foundry/eslint').react;
+module.exports = {
+  extends: ['airbnb', 'plugin:jest/recommended', 'prettier', 'prettier/react'],
+  plugins: ['prettier', 'jest', 'emotion'],
+  rules: {
+    /**
+     * Emotion 10 migration rules (start)
+     */
+    'emotion/jsx-import': 'error',
+    'emotion/no-vanilla': 'error',
+    'emotion/import-from-emotion': 'error',
+    'emotion/styled-import': 'error',
+    /**
+     * Emotion 10 migration rules (end)
+     */
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
+    'no-use-before-define': ['error', { functions: false }],
+    'no-underscore-dangle': 'off',
+    'no-param-reassign': 'off',
+    'max-len': [
+      'error',
+      {
+        code: 80,
+        tabWidth: 2,
+        ignoreComments: true,
+        ignoreUrls: true
+      }
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        singleQuote: true
+      }
+    ],
+    'import/prefer-default-export': 0,
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: [
+          '*.js',
+          'src/**/*.story.js',
+          'src/**/*.disabledStory.js',
+          'src/**/*.spec.js',
+          'build/**/*.js',
+          'src/util/withTests.js'
+        ]
+      }
+    ]
+  },
+  globals: {
+    STORYBOOK: true
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    allowImportExportEverywhere: true,
+    ecmaFeatures: {
+      ecmaVersion: 2017,
+      impliedStrict: true,
+      jsx: true
+    }
+  },
+  env: {
+    browser: true
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: '.storybook/webpack.config.js'
+      }
+    }
+  },
+  overrides: [
+    {
+      files: ['src/**/*spec.js'],
+      rules: {
+        'max-len': [
+          'error',
+          {
+            code: 80,
+            tabWidth: 2,
+            ignorePattern: '^\\s*it\\(',
+            ignoreComments: true,
+            ignoreUrls: true
+          }
+        ]
+      },
+      globals: {
+        mount: true,
+        shallow: true,
+        render: true,
+        create: true,
+        axe: true,
+        renderToHtml: true,
+        describe: true,
+        beforeEach: true,
+        inject: true,
+        it: true,
+        test: true,
+        expect: true,
+        afterEach: true
+      },
+      env: {
+        'jest/globals': true
+      }
+    }
+  ]
+};
