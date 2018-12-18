3cd2a0afa2d33454b2cf601e2b52ea850209606a
Felix Jung <felix@jung.fm>
2018-12-18 13:25:48 +0100

    Update Blockquote component

--- src/components/Blockquote/Blockquote.js
+++ src/components/Blockquote/Blockquote.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from '@emotion/styled';
-import { css } from 'emotion';
+import { css } from '@emotion/core';
 
 import { childrenPropType } from '../../util/shared-prop-types';
 import Text from '../Text';
