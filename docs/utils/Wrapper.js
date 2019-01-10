bbaec13b223f36be5a48e91eb862cd59533425f5
Fernando Fleury <fernandofleuryg@gmail.com>
2019-01-10 11:10:25 +0100

    Update docz Wrapper to properly use circuit theme

--- docs/utils/Wrapper.js
+++ docs/utils/Wrapper.js
@@ -5,6 +5,12 @@ import PropTypes from 'prop-types';
 import { ThemeProvider } from 'emotion-theming';
 import { circuit } from '../../src/themes';
 
+/**
+ * Current emotion version doesn't allow [object Module] as theme,
+ * only [object Object]
+ */
+const theme = { ...circuit };
+
 export const Wrapper = props => (
-  <ThemeProvider theme={circuit}>{props.children}</ThemeProvider>
+  <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
 );
