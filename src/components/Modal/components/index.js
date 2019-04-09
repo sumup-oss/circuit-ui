4439fb280172c8cce9859c105cfdc650523aab60
Fernando Fleury <fernandofleuryg@gmail.com>
2019-04-09 13:46:46 +0200

    Update docz

--- src/components/Modal/components/index.js
+++ src/components/Modal/components/index.js
@@ -1,3 +1,5 @@
-export { default as ModalWrapper } from './ModalWrapper';
-export { default as ModalHeader } from './ModalHeader';
+import ModalWrapper from './ModalWrapper';
+import ModalHeader from './ModalHeader';
+
+export { ModalWrapper, ModalHeader };
 export { CardFooter as ModalFooter } from '../../Card';
