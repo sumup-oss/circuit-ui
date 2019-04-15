ba1b1607bb7344756f04bf04a95ae2f92ba66228
Zdravko <zdravko.iliev@sumup.com>
2019-04-15 13:41:38 +0300

    Export table row cell header (#393)

--- src/components/Table/index.js
+++ src/components/Table/index.js
@@ -1,3 +1,8 @@
 import Table from './Table';
+import TableRow from './components/TableRow';
+import TableCell from './components/TableCell';
+import TableHeader from './components/TableHeader';
+
+export { TableRow, TableCell, TableHeader };
 
 export default Table;
