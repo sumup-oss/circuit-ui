c88a40ec8303a88797fddf3eb548cb3666868a1f
Afonso Delgado <afonsodelgadoss@gmail.com>
2019-05-20 11:43:09 -0300

    Add isArray type check function

--- src/util/type-check.js
+++ src/util/type-check.js
@@ -1,2 +1,4 @@
 export const isFunction = val => typeof val === 'function';
 export const isString = val => typeof val === 'string';
+export const isArray = value =>
+  value && typeof value === 'object' && value.constructor === Array;
