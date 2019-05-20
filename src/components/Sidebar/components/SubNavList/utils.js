374569b8594495d0176a5d2422ee0694633dfe6d
Afonso Delgado <afonsodelgadoss@gmail.com>
2019-05-20 12:14:40 -0300

    Use lodash findIndex

--- src/components/Sidebar/components/SubNavList/utils.js
+++ src/components/Sidebar/components/SubNavList/utils.js
@@ -1,6 +1,7 @@
+import { findIndex } from 'lodash/fp';
 import { isArray } from '../../../../util/type-check';
 
 const getSelectedChildIndex = children =>
-  isArray(children) ? children.findIndex(child => child.props.selected) : 0;
+  isArray(children) ? findIndex(child => child.props.selected, children) : 0;
 
 export default getSelectedChildIndex;
