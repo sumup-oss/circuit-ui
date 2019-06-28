77bc973022fbb255b5c59f1709eae8cb90fe018d
Fernando Fleury <fernandofleuryg@gmail.com>
2019-06-28 15:20:25 +0200

    Update foundry to 1.4.0 (#418)

--- src/components/CreditCardDetails/components/scheme-icons/card-scheme-icons.js
+++ src/components/CreditCardDetails/components/scheme-icons/card-scheme-icons.js
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2019, SumUp Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import React from 'react';
 
 import { flow, toPairs, reduce } from '../../../../util/fp';
@@ -56,9 +71,11 @@ const accessibleIconComponents = flow(
   reduce(
     (acc, [name, IconComponent]) => ({
       ...acc,
-      [name]: props => (
-        <IconComponent aria-label={`icon ${name}`} role="img" {...props} />
-      )
+      [name]: function Component({ props }) {
+        return (
+          <IconComponent aria-label={`icon ${name}`} role="img" {...props} />
+        );
+      }
     }),
     {}
   )
