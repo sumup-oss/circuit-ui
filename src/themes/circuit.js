98ae6580322d9888ad11ee5f4575b035875b50d9
Diego G. Lima <diego.gomes@sumup.com>
2019-07-16 15:04:33 -0300

    feat(font-stack): Add new default fonts (#440)

--- src/themes/circuit.js
+++ src/themes/circuit.js
@@ -204,7 +204,9 @@ export const typography = {
 };
 
 export const fontStack = {
-  default: 'aktiv-grotesk, -apple-system, BlinkMacSystemFont, "Segoe UI"',
+  default:
+    // eslint-disable-next-line
+    'aktiv-grotesk, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
   mono: 'Consolas, monaco, monospace'
 };
 
