9c08680588ffc53c818a80b15b148aae5d9e2977
Connor Bär <connor.baer@sumup.com>
2019-09-16 18:08:48 -0300

    test(configs): update setup and llinting rules for unit tests

--- jest.setup.js
+++ jest.setup.js
@@ -15,48 +15,50 @@
 
 import React from 'react';
 import { renderToStaticMarkup } from 'react-dom/server';
-import '@testing-library/jest-dom/extend-expect';
 import 'jest-enzyme';
+import '@testing-library/jest-dom/extend-expect';
 import Enzyme, { mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import { createSerializer } from 'jest-emotion';
 import { axe, toHaveNoViolations } from 'jest-axe';
 import { render, fireEvent, wait, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { ThemeProvider } from 'emotion-theming';
 
 import { circuit } from './src/themes';
 
 Enzyme.configure({ adapter: new Adapter() });
 
-const renderWithTheme = renderFn => (component, ...rest) =>
-  renderFn(<ThemeProvider theme={circuit}>{component}</ThemeProvider>, rest);
+// eslint-disable-next-line react/prop-types
+const WithProviders = ({ children }) => (
+  <ThemeProvider theme={circuit}>{children}</ThemeProvider>
+);
+
+const renderWithProviders = renderFn => (component, ...rest) =>
+  renderFn(<WithProviders>{component}</WithProviders>, rest);
 
 global.shallow = (...args) => {
   console.error(
     'We do not use Shallow anymore. Automatically switching to mount.'
   );
   return mount(...args, {
-    wrappingComponent: ThemeProvider,
-    wrappingComponentProps: {
-      theme: circuit
-    }
+    wrappingComponent: WithProviders
   });
 };
 
 global.mount = (...args) =>
   mount(...args, {
-    wrappingComponent: ThemeProvider,
-    wrappingComponentProps: {
-      theme: circuit
-    }
+    wrappingComponent: WithProviders
   });
 
 global.axe = axe;
 global.act = act;
 global.wait = wait;
 global.fireEvent = fireEvent;
-global.render = renderWithTheme(render);
-global.renderToHtml = renderWithTheme(renderToStaticMarkup);
+global.userEvent = userEvent;
+global.render = (component, options) =>
+  render(component, { wrapper: WithProviders, ...options });
+global.renderToHtml = renderWithProviders(renderToStaticMarkup);
 global.create = (...args) => {
   const { container } = global.render(...args);
   return container.children.length > 1
