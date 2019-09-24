60826a94e54f01c9b3460d756781823156703b11
Connor Bär <connor.baer@sumup.com>
2019-09-24 10:21:54 -0300

    feat(scripts): remove old static style extraction

--- build/stylesheet/lib/custom-styled.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import * as emotion from 'emotion';
-import createEmotionStyled from 'create-emotion-styled';
-
-export default createEmotionStyled(emotion, React);
-export * from 'emotion';
