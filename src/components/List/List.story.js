21ac3dc24df3d6f4fa74b9ef6649941da524c9bd
Connor Bär <connor.baer@sumup.com>
2019-10-24 16:50:37 -0300

    feat(docs): migrate List

--- src/components/List/List.story.js
+++ src/components/List/List.story.js
@@ -16,30 +16,61 @@
 import React from 'react';
 import { boolean, select } from '@storybook/addon-knobs';
 
+import docs from './List.docs.mdx';
 import List from './List';
 
 const sizes = [List.KILO, List.MEGA, List.GIGA];
 
 export default {
   title: 'Components|List',
-
+  component: List,
   parameters: {
-    component: List,
+    docs: { page: docs },
     jest: ['List']
   }
 };
 
-export const list = () => (
-  <List size={select('Size', sizes, sizes[0])} ordered={boolean('Ordered')}>
+const ListItems = () => (
+  <>
     <li>This is a list</li>
     <li>A very fine list</li>
-    <List size={select('Size', sizes, sizes[0])} ordered={boolean('Ordered')}>
-      <li>Sometimes a nested list</li>
-    </List>
-    <li>The kind of list you like</li>
+  </>
+);
+
+export const base = () => (
+  <List
+    size={select('Size', sizes, sizes[0])}
+    ordered={boolean('Ordered', false)}
+  >
+    <ListItems />
   </List>
 );
 
-list.story = {
-  name: 'List'
-};
+export const ordered = () => (
+  <List size={select('Size', sizes, sizes[0])} ordered>
+    <ListItems />
+  </List>
+);
+
+export const size = () => (
+  <>
+    <List size={sizes[0]}>
+      <ListItems />
+    </List>
+    <List size={sizes[1]}>
+      <ListItems />
+    </List>
+    <List size={sizes[2]}>
+      <ListItems />
+    </List>
+  </>
+);
+
+export const nested = () => (
+  <List>
+    <ListItems />
+    <List>
+      <li>Sometimes a nested list</li>
+    </List>
+  </List>
+);
