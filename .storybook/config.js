18bac7254afd3e1e865a0ca4b8630da02054a41b
Connor Bär <connor.baer@sumup.com>
2019-10-29 11:48:44 -0300

    fix(docs): import test decorator correctly

--- .storybook/config.js
+++ .storybook/config.js
@@ -71,7 +71,7 @@ addDecorator(withKnobs);
 
 // These decorators need to be disabled for StoryShots to work.
 if (!__TEST__) {
-  const withTests = require('./util/withTests');
+  const withTests = require('./util/withTests').default;
   addDecorator(withTests);
   addDecorator(withStoryStyles);
 }
