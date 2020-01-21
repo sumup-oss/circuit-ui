d1860d2a168a514fde95d0d3496f337189732fb3
Connor Bär <connor.baer@sumup.com>
2020-01-21 19:23:34 -0300

    feat(components): remove Markdown component
    BREAKING CHANGE:
    Markdown has been deprecated.

--- src/index.js
+++ src/index.js
@@ -33,7 +33,6 @@ const currencyAmountUtils = {
 export { default as Heading } from './components/Heading';
 export { default as List } from './components/List';
 
-export { default as Markdown } from './components/Markdown';
 export { default as SubHeading } from './components/SubHeading';
 export { default as Text } from './components/Text';
 export { default as Blockquote } from './components/Blockquote';
