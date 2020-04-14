ac3650e8e5a7651c46ec7004387cef68f2debaf2
Mariela Kasovska <mariela.kasovska@sumup.com>
2020-04-14 20:59:52 +0300

    feat(components): update InfoIcon (#567)
    * Update InfoIcon component

--- src/components/InfoIcon/InfoIcon.js
+++ src/components/InfoIcon/InfoIcon.js
@@ -14,25 +14,13 @@
  */
 
 import React from 'react';
-import styled from '@emotion/styled';
-import { css } from '@emotion/core';
-
 import { ReactComponent as Icon } from './info.svg';
-
-const baseStyles = ({ theme }) => css`
-  label: info-icon;
-  border: 1px solid ${theme.colors.n500};
-  border-radius: 100%;
-  fill: ${theme.colors.n500};
-`;
-
-const StyledIcon = styled(Icon)(baseStyles);
 
 /**
  * A small info icon used for triggering tooltips
  * and other informational content.
  */
-const InfoIcon = props => <StyledIcon {...props} />;
+const InfoIcon = props => <Icon {...props} />;
 
 /**
  * @component
