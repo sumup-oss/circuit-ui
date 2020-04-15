15b1e26eb966762e1aaddc230748791635fa9e06
Connor Bär <connor.baer@me.com>
2020-04-15 23:29:13 +0200

    feat(node): add script to export SVGs as React components

--- packages/icons/.eslintrc.js
+++ packages/icons/.eslintrc.js
@@ -1,6 +1,19 @@
-module.exports = require('@sumup/foundry/eslint')({
-  language: 'JavaScript',
-  environments: ['Node'],
-  frameworks: ['React', 'Jest'],
-  openSource: true,
-});
+module.exports = require('@sumup/foundry/eslint')(
+  {
+    language: 'JavaScript',
+    environments: ['Node'],
+    frameworks: ['React'],
+    openSource: true,
+  },
+  {
+    overrides: [
+      {
+        files: ['scripts/*'],
+        rules: {
+          'import/no-extraneous-dependencies': 'off',
+          'node/no-unpublished-require': 'off',
+        },
+      },
+    ],
+  },
+);
