7c349598536de27ffd2652e7dcfc2b0ba3bb5312
lucent1090 <lucent1090@gmail.com>
2020-06-02 16:00:52 +0200

    Apply suggestions from code review
    Co-authored-by: Connor Bär <connor-baer@users.noreply.github.com>

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,11 +2,11 @@ import { Dispatch } from '@sumup/collector/build/types';
 
 export interface TrackingProps {
   /**
-   * Set enable to activate tracking mechanism. Default: false.
+   * Whether to dispatch a tracking event when a user interacts with the component. Default: false.
    */
   enableTracking: boolean;
   /**
-   * Dispatch
+   * Additional data that is dispatched with the tracking event.
    */
   trackingPayload: Dispatch;
 }
