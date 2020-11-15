c6d3ffcf2bbe65669629c53b531ce049f39767e7
Connor Bär <connor.baer@me.com>
2020-11-15 12:02:58 +0100

    feat(cli): extract docgen info from TS components

---
+++ babel.config.js
@@ -0,0 +1,81 @@
+module.exports = {
+  plugins: [
+    ['@babel/plugin-proposal-class-properties', { loose: true }],
+    'lodash',
+  ],
+  presets: [
+    ['@babel/preset-env', { loose: true }],
+    '@babel/preset-react',
+    [
+      '@emotion/babel-preset-css-prop',
+      {
+        autoLabel: false,
+        labelFormat: '[filename]--[local]',
+      },
+    ],
+  ],
+  env: {
+    static: {
+      plugins: [
+        ['@babel/plugin-proposal-class-properties', { loose: true }],
+        'lodash',
+        'react-docgen',
+        [
+          'babel-plugin-react-docgen-typescript',
+          {
+            shouldExtractLiteralValuesFromEnum: true,
+            propFilter: (prop, component) => {
+              if (prop.parent) {
+                return !prop.parent.fileName.includes('node_modules');
+              }
+
+              return true;
+            },
+            exclude: 'stories\\.tsx$',
+          },
+        ],
+      ],
+      presets: [
+        '@babel/preset-typescript',
+        [
+          '@babel/preset-env',
+          {
+            loose: true,
+          },
+        ],
+        '@babel/preset-react',
+        [
+          '@emotion/babel-preset-css-prop',
+          {
+            autoLabel: false,
+            labelFormat: '[filename]--[local]',
+          },
+        ],
+      ],
+    },
+    test: {
+      plugins: [
+        ['@babel/plugin-proposal-class-properties', { loose: true }],
+        'lodash',
+        'react-docgen',
+      ],
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              node: 'current',
+            },
+          },
+        ],
+        '@babel/preset-react',
+        [
+          '@emotion/babel-preset-css-prop',
+          {
+            autoLabel: false,
+          },
+        ],
+      ],
+    },
+  },
+};
