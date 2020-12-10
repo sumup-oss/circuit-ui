b980a492a003b9a791c808901c75f59c0502875f
Connor Bär <connor-baer@users.noreply.github.com>
2020-12-10 11:25:21 +0100

    feat: add bundle analyzer plugin (#23)

--- packages/create-sumup-next-app/template/next.config.js
+++ packages/create-sumup-next-app/template/next.config.js
@@ -1,4 +1,7 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
 const withTM = require('next-transpile-modules')([
   '@sumup/circuit-ui',
   '@sumup/design-tokens',
@@ -6,16 +9,18 @@ const withTM = require('next-transpile-modules')([
   '@sumup/icons',
 ]);
 
-module.exports = withTM({
-  /**
-   * Environment variables can be used to inject dynamic configuration
-   * into the Next.js app at built time. Learn more at
-   * https://nextjs.org/docs/api-reference/next.config.js/environment-variables
-   */
-  env: {
-    SITE_NAME: 'SumUp',
-    SITE_LOCALE: 'en',
-    SITE_BASEURL: 'https://example.sumup.com',
-    STATIC_ASSET_BASEURL: 'https://static.sumup.com',
-  },
-});
+module.exports = withBundleAnalyzer(
+  withTM({
+    /**
+     * Environment variables can be used to inject dynamic configuration
+     * into the Next.js app at built time. Learn more at
+     * https://nextjs.org/docs/api-reference/next.config.js/environment-variables
+     */
+    env: {
+      SITE_NAME: 'SumUp',
+      SITE_LOCALE: 'en',
+      SITE_BASEURL: 'https://example.sumup.com',
+      STATIC_ASSET_BASEURL: 'https://static.sumup.com',
+    },
+  }),
+);
