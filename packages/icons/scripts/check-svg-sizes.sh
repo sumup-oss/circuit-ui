0cc3c80d76b11384732b1739b99120659773e60c
Connor Bär <connor-baer@users.noreply.github.com>
2021-01-21 18:03:24 +0100

    ci: ensure small file sizes (#40)

---
+++ packages/icons/scripts/check-svg-sizes.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Reports all SVGs in the /web folder that exceed the size limit.
+
+size_limit=15
+offenders=$(find ./web -type f -name "*.svg" -size +${size_limit}k ! -path '*flags*')
+
+if [ ! -z "$offenders" ]
+then
+  offender_array=()
+
+  while read line; do
+    offender_array+=("$line")
+  done <<< "$offenders"
+
+  file_count=${#offender_array[@]} # $(echo -n "$offenders" | grep -c '^')
+
+  echo "The following $file_count SVG files exceed ${size_limit}kb and need to be optimized:"
+  echo "Size | Path"
+
+  for file_path in "${offender_array[@]}"
+  do
+    file_size=$(du -h "$file_path" | cut -f1)
+    echo "$file_size | $file_path"
+  done
+
+  exit 1;
+else
+  echo "No SVG files exceed ${size_limit}kb. That's great!"
+
+  exit 0;
+fi
