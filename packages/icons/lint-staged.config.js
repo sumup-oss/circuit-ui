24710e3d6bac3504fdf4af6514819982291951c8
Connor Bär <connor-baer@users.noreply.github.com>
2021-03-04 15:16:30 +0100

    fix(icons): use 16x16 viewbox for small icons (#64)

--- packages/icons/lint-staged.config.js
+++ packages/icons/lint-staged.config.js
@@ -3,6 +3,6 @@ module.exports = require('@sumup/foundry/lint-staged')(
     language: 'TypeScript',
   },
   {
-    '*.svg': ['svgo --config svgo.config.json --pretty'],
+    '*.svg': ['svgo --config svgo.config.js --pretty'],
   },
 );
