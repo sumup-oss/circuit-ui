sumup-oss/circuit-ui

commit 2832425bd0cecbb3879752d6e9bd2d8b210c0b6d

refactor(components): migrate Card to Typescript (#702)
Co-authored-by: Connor Bär <connor.baer@sumup.com>

src/components/Card/components/Header/Header.tsx
@@ -1,0 +1,103 @@
+/**
+ * Copyright 2019, SumUp Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import React, { FC, ReactNode, MouseEvent, KeyboardEvent } from 'react';
+import { css } from '@emotion/core';
+import { Dispatch as TrackingProps } from '@sumup/collector';
+
+import styled, { StyleProps } from '../../../../styles/styled';
+import {
+  CloseButton,
+  CloseButtonProps,
+} from '../../../CloseButton/CloseButton';
+
+export interface CardHeaderProps {
+  /**
+   * Heading to be shown.
+   */
+  children?: ReactNode;
+  /**
+   * Callback for the close button. If not specified, the button won't
+   * be shown.
+   */
+  onClose?: (event: MouseEvent | KeyboardEvent) => void;
+  /**
+   * Text label for the close button for screen readers.
+   * Important for accessibility.
+   */
+  labelCloseButton?: string;
+  /**
+   * Additional data that is dispatched with the tracking event.
+   */
+  tracking?: TrackingProps;
+}
+
+type ContainerElProps = Pick<CardHeaderProps, 'children'>;
+
+const baseStyles = ({ theme }: StyleProps) => css`
+  label: card__header;
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: ${theme.spacings.giga};
+`;
+
+const noHeadingStyles = ({ children }: ContainerElProps) =>
+  !children &&
+  css`
+    label: card__header--no-heading;
+    justify-content: flex-end;
+  `;
+
+const CardHeaderContainer = styled('header')<ContainerElProps>(
+  baseStyles,
+  noHeadingStyles,
+);
+
+const closeButtonStyles = ({ theme }: StyleProps) => css`
+  margin-top: -${theme.spacings.byte};
+  margin-right: -${theme.spacings.kilo};
+  margin-bottom: -${theme.spacings.byte};
+`;
+
+const CardHeaderCloseButton = styled(CloseButton)<CloseButtonProps>(
+  closeButtonStyles,
+);
+
+/**
+ * Header used in the Card component. Used for styling and alignment
+ * purposes only.
+ */
+export const CardHeader: FC<CardHeaderProps> = ({
+  onClose,
+  children,
+  labelCloseButton,
+  tracking = {},
+  ...props
+}) => (
+  <CardHeaderContainer {...props}>
+    {children}
+    {onClose && (
+      <CardHeaderCloseButton
+        onClick={onClose}
+        // TODO: The labelCloseButton should be added to the conditional
+        //       on LOC 92 in the next major version.
+        label={labelCloseButton as string}
+        data-testid="header-close"
+        tracking={{ component: 'close-button', ...tracking }}
+      />
+    )}
+  </CardHeaderContainer>
+);

src/components/Card/components/Header/Header.spec.tsx
@@ -14,15 +14,26 @@
  */
 
 import React from 'react';
+import { fireEvent } from '@testing-library/dom';
 
-import { CardHeader } from '../..';
+import {
+  act,
+  create,
+  render,
+  renderToHtml,
+  axe,
+} from '../../../../util/test-utils';
+
+import { CardHeader } from './Header';
 
 describe('CardHeader', () => {
+  const children = <p>This is a content.</p>;
+
   /**
    * Style tests.
    */
   it('should render with default styles', () => {
-    const actual = create(<CardHeader />);
+    const actual = create(<CardHeader>{children}</CardHeader>);
     expect(actual).toMatchSnapshot();
   });
 
@@ -30,7 +41,9 @@ describe('CardHeader', () => {
    * Accessibility tests.
    */
   it('should meet accessibility guidelines', async () => {
-    const wrapper = renderToHtml(<CardHeader />);
+    const wrapper = renderToHtml(
+      <CardHeader labelCloseButton="Close">{children}</CardHeader>,
+    );
     const actual = await axe(wrapper);
     expect(actual).toHaveNoViolations();
   });
@@ -39,14 +52,22 @@ describe('CardHeader', () => {
    * Logic tests.
    */
   it('should render a close button when an onClose prop is passed', () => {
-    const { getByTestId } = render(<CardHeader onClose={() => {}} />);
+    const { getByTestId } = render(
+      <CardHeader labelCloseButton="Close" onClose={() => {}}>
+        {children}
+      </CardHeader>,
+    );
     const actual = getByTestId('header-close');
     expect(actual).not.toBeNull();
   });
 
   it('should call the onClose prop when the close button is clicked', () => {
     const onClose = jest.fn();
-    const { getByTestId } = render(<CardHeader onClose={onClose} />);
+    const { getByTestId } = render(
+      <CardHeader labelCloseButton="Close" onClose={onClose}>
+        {children}
+      </CardHeader>,
+    );
 
     act(() => {
       fireEvent.click(getByTestId('header-close'));

.storybook/components/Teaser.js
@@ -41,7 +41,7 @@ const Wrapper = styled(Card)(
 
 const Teaser = ({ title, children, ...props }) => (
   <ThemeProvider theme={light}>
-    <Wrapper shadow={Card.DOUBLE}>
+    <Wrapper shadow="double">
       <Heading as="h2" size="giga">
         {title}
       </Heading>

src/components/Card/Card.spec.tsx
@@ -15,6 +15,8 @@
 
 import React from 'react';
 
+import { create, renderToHtml, axe } from '../../util/test-utils';
+
 import Card from '.';
 
 describe('Card', () => {

src/components/Card/Card.stories.tsx
@@ -17,6 +17,7 @@
 import { Fragment } from 'react';
 import { action } from '@storybook/addon-actions';
 import { css, jsx } from '@emotion/core';
+import { Theme } from '@sumup/design-tokens';
 
 import { Stack } from '../../../.storybook/components';
 import CardList from '../CardList';
@@ -53,7 +54,7 @@ const squareStyles = () => css`
   max-height: 90vh;
 `;
 
-const contentStyles = (theme) => css`
+const contentStyles = (theme: Theme) => css`
   background: ${theme.colors.n300};
   width: 100%;
   height: 118px;

src/components/Card/Card.tsx
@@ -13,19 +13,32 @@
  * limitations under the License.
  */
 
-import PropTypes from 'prop-types';
-import styled from '@emotion/styled';
-import { css } from '@emotion/core';
+import { FC } from 'react';
 import isPropValid from '@emotion/is-prop-valid';
+import { css } from '@emotion/core';
 
-import { childrenPropType } from '../../util/shared-prop-types';
+import styled, { StyleProps } from '../../styles/styled';
 import {
   shadowSingle,
   shadowDouble,
   shadowTriple,
 } from '../../styles/style-helpers';
 
-const baseStyles = ({ theme }) => css`
+type Shadow = 'single' | 'double' | 'triple';
+type Spacing = 'mega' | 'giga';
+
+export interface CardProps {
+  /**
+   * The shadow depth of the Card.
+   */
+  shadow?: Shadow;
+  /**
+   * The padding of the Card.
+   */
+  spacing?: Spacing;
+}
+
+const baseStyles = ({ theme }: StyleProps) => css`
   label: card;
   background-color: ${theme.colors.white};
   border-radius: ${theme.borderRadius.mega};
@@ -34,7 +47,7 @@ const baseStyles = ({ theme }) => css`
   justify-content: space-between;
 `;
 
-const shadowStyles = ({ theme, shadow }) => {
+const shadowStyles = ({ theme, shadow = 'single' }: StyleProps & CardProps) => {
   if (!shadow) {
     return null;
   }
@@ -49,7 +62,7 @@ const shadowStyles = ({ theme, shadow }) => {
   `;
 };
 
-const spacingStyles = ({ theme, spacing }) => {
+const spacingStyles = ({ theme, spacing = 'giga' }: StyleProps & CardProps) => {
   if (!spacing) {
     return null;
   }
@@ -67,34 +80,6 @@ const spacingStyles = ({ theme, spacing }) => {
   `;
 };
 
-/**
- * Card component that is used for displaying content on a grid.
- */
-const Card = styled('div', {
+export const Card: FC<CardProps> = styled('div', {
   shouldForwardProp: (prop) => isPropValid(prop) && prop !== 'spacing',
-})(baseStyles, shadowStyles, spacingStyles);
-
-Card.propTypes = {
-  /**
-   * The shadow depth of the Card.
-   */
-  shadow: PropTypes.oneOf(['single', 'double', 'triple']),
-  /**
-   * The padding of the Card.
-   */
-  spacing: PropTypes.oneOf(['mega', 'giga']),
-  /**
-   * Content to be rendered inside the Card.
-   */
-  children: childrenPropType,
-};
-
-Card.defaultProps = {
-  spacing: 'giga',
-  shadow: 'single',
-};
-
-/**
- * @component
- */
-export default Card;
+})<CardProps>(baseStyles, shadowStyles, spacingStyles);

src/components/Card/__snapshots__/Card.spec.tsx.snap
@@ -20,7 +20,7 @@ exports[`Card should render with default styles 1`] = `
 }
 
 <div
-  class="circuit-0 circuit-1"
+  class="circuit-0"
 />
 `;
 
@@ -44,7 +44,7 @@ exports[`Card should render with shadow styles 1`] = `
 }
 
 <div
-  class="circuit-0 circuit-1"
+  class="circuit-0"
 />
 `;
 
@@ -68,7 +68,7 @@ exports[`Card should render with shadow styles 2`] = `
 }
 
 <div
-  class="circuit-0 circuit-1"
+  class="circuit-0"
 />
 `;
 
@@ -92,7 +92,7 @@ exports[`Card should render with shadow styles 3`] = `
 }
 
 <div
-  class="circuit-0 circuit-1"
+  class="circuit-0"
 />
 `;
 
@@ -116,7 +116,7 @@ exports[`Card should render with spacing styles 1`] = `
 }
 
 <div
-  class="circuit-0 circuit-1"
+  class="circuit-0"
 />
 `;
 
@@ -140,6 +140,6 @@ exports[`Card should render with spacing styles 2`] = `
 }
 
 <div
-  class="circuit-0 circuit-1"
+  class="circuit-0"
 />
 `;

src/components/Card/components/Footer/Footer.spec.tsx
@@ -15,6 +15,7 @@
 
 import React from 'react';
 
+import { create, renderToHtml, axe } from '../../../../util/test-utils';
 import { CardFooter } from '../..';
 
 describe('CardFooter', () => {

commit 8ef4fc942bfb7e111bc54854f955e5e24d1d8939

fix(components): align close button correctly when Modal title is missing
Regression introduced in
https://github.com/sumup-oss/circuit-ui/commit/2832425bd0cecbb3879752d6e9bd2d8b210c0b6d#diff-e8d4fbe19bc0e35b6c3d6e76d917126cff122126fb5b519881a613f5c3a0fdb8R58

src/components/Card/components/Header/Header.tsx
@@ -55,7 +55,8 @@ const baseStyles = ({ theme }: StyleProps) => css`
 `;
 
 const noHeadingStyles = ({ children }: ContainerElProps) =>
-  !children &&
+  Array.isArray(children) &&
+  !children[0] &&
   css`
     label: card__header--no-heading;
     justify-content: flex-end;

src/components/Card/components/Header/Header.spec.tsx
@@ -16,13 +16,7 @@
 import React from 'react';
 import { fireEvent } from '@testing-library/dom';
 
-import {
-  act,
-  create,
-  render,
-  renderToHtml,
-  axe,
-} from '../../../../util/test-utils';
+import { create, render, renderToHtml, axe } from '../../../../util/test-utils';
 
 import { CardHeader } from './Header';
 
@@ -38,17 +32,6 @@ describe('CardHeader', () => {
   });
 
   /**
-   * Accessibility tests.
-   */
-  it('should meet accessibility guidelines', async () => {
-    const wrapper = renderToHtml(
-      <CardHeader labelCloseButton="Close">{children}</CardHeader>,
-    );
-    const actual = await axe(wrapper);
-    expect(actual).toHaveNoViolations();
-  });
-
-  /**
    * Logic tests.
    */
   it('should render a close button when an onClose prop is passed', () => {
@@ -58,7 +41,7 @@ describe('CardHeader', () => {
       </CardHeader>,
     );
     const actual = getByTestId('header-close');
-    expect(actual).not.toBeNull();
+    expect(actual).toBeVisible();
   });
 
   it('should call the onClose prop when the close button is clicked', () => {
@@ -69,10 +52,19 @@ describe('CardHeader', () => {
       </CardHeader>,
     );
 
-    act(() => {
-      fireEvent.click(getByTestId('header-close'));
-    });
+    fireEvent.click(getByTestId('header-close'));
 
     expect(onClose).toHaveBeenCalledTimes(1);
+  });
+
+  /**
+   * Accessibility tests.
+   */
+  it('should meet accessibility guidelines', async () => {
+    const wrapper = renderToHtml(
+      <CardHeader labelCloseButton="Close">{children}</CardHeader>,
+    );
+    const actual = await axe(wrapper);
+    expect(actual).toHaveNoViolations();
   });
 });