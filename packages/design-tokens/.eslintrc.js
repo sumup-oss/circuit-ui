690913db2c9eeca66cee434ec7deddee9caa790c
Connor Bär <connor.baer@me.com>
2021-03-29 17:21:32 +0200

    Import @sumup/design-tokens

--- packages/design-tokens/.eslintrc.js
+++
@@ -1,6 +0,0 @@
-module.exports = require('@sumup/foundry/eslint')({
-  language: 'TypeScript',
-  environments: ['Node'],
-  frameworks: ['Jest'],
-  openSource: true,
-});
--- packages/design-tokens/jest.config.js
+++ packages/design-tokens/jest.config.js
@@ -1,9 +1,31 @@
+/**
+ * Copyright 2021, SumUp Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+const baseConfig = require('../../jest.config.base');
+
+const packageJson = require('./package.json');
+
+const packageName = packageJson.name.split('@sumup/').pop();
+
 module.exports = {
-  preset: 'ts-jest',
+  ...baseConfig,
+  name: packageName,
+  displayName: packageName,
+  rootDir: '../..',
+  roots: [`<rootDir>/packages/${packageName}/src`],
   clearMocks: true,
-  rootDir: 'src',
-  coverageDirectory: './__reports__',
-  coveragePathIgnorePatterns: ['node_modules'],
   testEnvironment: 'node',
   setupFilesAfterEnv: ['jest-extended'],
 };
