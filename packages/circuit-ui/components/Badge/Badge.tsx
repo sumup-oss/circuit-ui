0ad3f5f876152c21e2d65bb34d620046e6fa71d1
Robin Métral <robin.metral@sumup.com>
2021-06-22 11:43:03 +0200

    Remove deprecated onClick and primary variant from the Badge

--- packages/circuit-ui/components/Badge/Badge.tsx
+++ packages/circuit-ui/components/Badge/Badge.tsx
@@ -13,29 +13,20 @@
  * limitations under the License.
  */
 
-import { forwardRef, MouseEvent, KeyboardEvent, Ref, HTMLProps } from 'react';
+import { Ref, HTMLProps } from 'react';
 import { css } from '@emotion/core';
 
 import styled, { StyleProps } from '../../styles/styled';
-import { focusOutline } from '../../styles/style-mixins';
-import deprecate from '../../util/deprecate';
 
 export interface BadgeProps extends HTMLProps<HTMLDivElement> {
   /**
    * Choose from 4 style variants. Default: 'neutral'.
    */
-  variant?: 'neutral' | 'success' | 'warning' | 'danger' | 'primary' | 'promo';
+  variant?: 'neutral' | 'success' | 'warning' | 'danger' | 'promo';
   /**
    * Use the circular badge to indicate a count of items related to an element.
    */
   circle?: boolean;
-  /**
-   * @deprecated
-   * Callback for the click event.
-   */
-  onClick?: (
-    event: MouseEvent<HTMLDivElement> | KeyboardEvent<HTMLDivElement>,
-  ) => void;
   /**
    * The ref to the HTML DOM element
    */
@@ -57,11 +48,6 @@ const COLOR_MAP = {
     text: 'white',
     default: 'r500',
     hover: 'r700',
-  },
-  primary: {
-    text: 'white',
-    default: 'p500',
-    hover: 'p700',
   },
   neutral: {
     text: 'bodyColor',
@@ -122,68 +108,14 @@ const circleStyles = ({ circle = false, children }: BadgeProps) =>
     width: ${isDynamicWidth(children) ? 'auto' : '24px'};
   `;
 
-const clickableStyles = ({
-  theme,
-  onClick,
-  variant = 'neutral',
-}: StyleProps & BadgeProps) => {
-  const currentColor = COLOR_MAP[variant];
-  if (!onClick || !currentColor) {
-    return null;
-  }
-  return css`
-    label: badge--clickable;
-    border: 0;
-    outline: 0;
-    cursor: pointer;
-    transition: background-color ${theme.transitions.default};
-
-    &:hover,
-    &:active {
-      background-color: ${theme.colors[currentColor.hover]};
-    }
-
-    &:focus {
-      ${focusOutline({ theme })};
-    }
-  `;
-};
-
-const StyledBadge = styled('div')<BadgeProps>(
-  baseStyles,
-  variantStyles,
-  circleStyles,
-  clickableStyles,
-);
-
 /**
  * A badge communicates the status of an element or the count of items
  * related to an element.
  */
-export const Badge = forwardRef((props: BadgeProps, ref: BadgeProps['ref']) => {
-  if (props.onClick) {
-    deprecate(
-      [
-        'The `onClick` prop of the Badge component has been deprecated.',
-        'Badges are not meant to be interactive and should only',
-        'communicate the status of an element.',
-        'Use the Tag component for interactive elements instead.',
-      ].join(' '),
-    );
-  }
-
-  if (props.variant === 'primary') {
-    deprecate(
-      [
-        'The "primary" color of the Badge component has been deprecated.',
-        'It conflicts with the color of the primary Button variant.',
-        'Use the "neutral" variant instead.',
-      ].join(' '),
-    );
-  }
-
-  const as = props.onClick ? 'button' : 'div';
-  return <StyledBadge as={as} ref={ref} {...props} />;
-});
+export const Badge = styled('div')<BadgeProps>(
+  baseStyles,
+  variantStyles,
+  circleStyles,
+);
 
 Badge.displayName = 'Badge';
