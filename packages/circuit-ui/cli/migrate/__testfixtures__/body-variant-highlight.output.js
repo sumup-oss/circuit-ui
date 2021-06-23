5686fc33dac16134abbc661920dab8215716b305
Robin Métral <robin.metral@sumup.com>
2021-06-23 18:24:33 +0200

    Extend body-variant-highlight to support the Anchor component

--- packages/circuit-ui/cli/migrate/__testfixtures__/body-variant-highlight.output.js
+++ packages/circuit-ui/cli/migrate/__testfixtures__/body-variant-highlight.output.js
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
-import { Body, Text } from '@sumup/circuit-ui';
+import { Body, Text, Anchor } from '@sumup/circuit-ui';
 
 const BoldBody = () => (
   <>
@@ -15,6 +15,13 @@ const BoldText = () => (
   </>
 );
 
+const BoldAnchor = () => (
+  <>
+    <Anchor variant="highlight">bold</Anchor>
+    <Anchor bold={!true}>bold</Anchor>
+  </>
+);
+
 const RedBody = styled(Body)`
   color: red;
 `;
@@ -23,9 +30,14 @@ const RedText = styled(Text)`
   color: red;
 `;
 
+const RedAnchor = styled(Anchor)`
+  color: red;
+`;
+
 const Styled = () => (
   <>
     <RedBody variant="highlight">bold body</RedBody>
     <RedText variant="highlight">bold text</RedText>
+    <RedAnchor variant="highlight">bold anchor</RedAnchor>
   </>
 );
