8036a5e91c652ad74bd571e069d91ba4265807d3
Connor Bär <connor.baer@me.com>
2021-07-02 14:26:41 +0200

    Restrict headline components' as prop to heading elements

--- packages/circuit-ui/components/SubHeadline/SubHeadline.tsx
+++ packages/circuit-ui/components/SubHeadline/SubHeadline.tsx
@@ -26,7 +26,7 @@ export interface SubHeadlineProps
    * without skipping any levels. Learn more at
    * https://www.w3.org/WAI/tutorials/page-structure/headings/.
    */
-  as: string;
+  as: 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 }
 
 const baseStyles = ({ theme }: StyleProps) => css`
