7109b11acff416aec2b9649896c199a33cd6299f
Connor Bär <connor.baer@me.com>
2021-08-12 09:43:35 +0200

    Visually hide the avatar placeholder

--- packages/circuit-ui/components/TopNavigation/components/ProfileMenu/ProfileMenu.tsx
+++ packages/circuit-ui/components/TopNavigation/components/ProfileMenu/ProfileMenu.tsx
@@ -24,7 +24,13 @@ import Body from '../../../Body';
 import Popover, { PopoverProps } from '../../../Popover';
 
 const AvatarPlaceholder = () => (
-  <svg width="24" height="24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg
+    width="24"
+    height="24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    role="presentation"
+  >
     <path
       d="M22.981 21.22A11.074 11.074 0 0012.001 12a11.074 11.074 0 00-10.982 9.22A1.576 1.576 0 002.6 23h18.8a1.577 1.577 0 001.581-1.78zm-10.98-1.72a1.5 1.5 0 110-3 1.5 1.5 0 010 3zm4.5-14a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0z"
       fill="currentColor"
