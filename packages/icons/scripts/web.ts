2f9b2b4fdd1c7952e64dcf9daa0df7e78d35d613
Robin Métral <robin.metral@sumup.com>
2021-08-26 10:53:12 +0200

    Migrate icons sizes from small/large to 16/24

--- packages/icons/scripts/web.ts
+++ packages/icons/scripts/web.ts
@@ -23,12 +23,12 @@ import { transformSync } from '@babel/core';
 import manifest from '../manifest.json';
 
 const BASE_DIR = path.join(__dirname, '..');
-const ICON_DIR = path.join(BASE_DIR, './web/v1');
+const ICON_DIR = path.join(BASE_DIR, './web/v2');
 const DIST_DIR = path.join(BASE_DIR, 'dist');
 
 enum IconSize {
-  SMALL = 'small',
-  LARGE = 'large',
+  SIZE_16 = '16',
+  SIZE_24 = '24',
 }
 
 type Icon = {
@@ -85,7 +85,7 @@ function buildComponentFile(component: Component): string {
       ${sizeMap.join('\n')}
     }
 
-    export const ${component.name} = ({ size = 'small', ...props }) => {
+    export const ${component.name} = ({ size = '24', ...props }) => {
       const Icon = sizeMap[size];
       return <Icon {...props} />;
     };
@@ -110,9 +110,9 @@ function buildDeclarationFile(components: Component[]): string {
 
     export interface IconProps extends React.SVGProps<SVGSVGElement> {
       /**
-       * Choose between the small (16px) or large (24px) size. Default \`small\`.
+       * Choose between the 16 or 24 pixels size. Defaults to \`24\`.
        */
-      size?: 'small' | 'large';
+      size?: '16' | '24';
     }
 
     ${declarationStatements.join('\n')}
