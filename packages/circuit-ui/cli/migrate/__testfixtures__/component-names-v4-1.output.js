b40cef6324fd7b52b6ab4104519a2c817df6934b
Connor Bär <connor.baer@me.com>
2021-10-14 09:51:03 +0200

    Add codemod for LoadingButton → Button

---
+++ packages/circuit-ui/cli/migrate/__testfixtures__/component-names-v4-1.output.js
@@ -0,0 +1,8 @@
+import styled from '@emotion/styled';
+import { Button } from '@sumup/circuit-ui';
+
+const BaseLoadingButton = () => <Button />;
+
+const LoadingButtonStyle = styled(Button)`
+  color: red;
+`;
