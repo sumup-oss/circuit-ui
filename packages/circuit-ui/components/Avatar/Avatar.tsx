be5e1285539b4cdb84ff7c18972c83a291654120
giedoka <pasiak5@gmail.com>
2021-11-10 15:54:43 +0100

    borderStyles added and snapshots updated

--- packages/circuit-ui/components/Avatar/Avatar.tsx
+++ packages/circuit-ui/components/Avatar/Avatar.tsx
@@ -46,20 +46,6 @@ const avatarSizes = {
   giga: '48px',
 };
 
-const avatarRadius = (
-  theme: Theme,
-  size?: 'giga' | 'yotta',
-  variant?: 'object' | 'identity',
-) => {
-  let radius = theme.borderRadius.kilo;
-  if (variant === 'identity') {
-    radius = theme.borderRadius.circle;
-  } else if (size === 'giga') {
-    radius = theme.borderRadius.byte;
-  }
-  return radius;
-};
-
 const placeholders = {
   object:
     '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 96 96"><path fill="white" d="M30 25c0-4.9706 4.0294-9 9-9s9 4.0294 9 9-4.0294 9-9 9-9-4.0294-9-9zM41.1571 60.5691L30.6742 48.3905c-1.6438-1.9097-4.6225-1.8422-6.1782.1399L8 69.5483v12.4515c0 3.3137 2.6863 6 6 6h5.9592l21.1979-27.4307zM70.4856 32.878c1.5553-2.002 4.5569-2.0705 6.202-.1417l11.312 13.2623v36c0 3.3137-2.6863 6-6 6H27.6611L70.4856 32.878z"/></svg>',
@@ -67,25 +53,35 @@ const placeholders = {
     '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 96 96"><path fill="white" d="M48 18c-7.732 0-14 6.268-14 14s6.268 14 14 14 14-6.268 14-14-6.268-14-14-14zM47.9998 88C61.53 88 73.4913 81.2822 80.73 71c-7.2387-10.2822-19.2-17-32.7303-17-13.5302 0-25.4914 6.7178-32.7302 17 7.2388 10.2822 19.2 17 32.7303 17z"/></svg>',
 };
 
-const baseStyles = ({
-  theme,
-  variant,
-  size = 'yotta',
-}: AvatarProps & StyleProps) => css`
+const baseStyles = ({ theme, size }: AvatarProps & StyleProps) => css`
   display: block;
   width: ${avatarSizes[size]};
   height: ${avatarSizes[size]};
   box-shadow: 0 0 0 ${theme.borderWidth.kilo} rgba(0, 0, 0, 0.1);
 
   background-color: ${theme.colors.n300};
-  border-radius: ${avatarRadius(theme, size, variant)};
   object-fit: cover;
   object-position: center;
 `;
 
+const borderStyles = ({ theme, variant, size }: AvatarProps & StyleProps) => {
+  if (variant === 'identity') {
+    return css`
+      border-radius: ${theme.borderRadius.circle};
+    `;
+  } else if (size === 'giga') {
+    return css`
+      border-radius: ${theme.borderRadius.byte};
+    `;
+  }
+  return css`
+    border-radius: ${theme.borderRadius.kilo};
+  `;
+};
+
 const StyledImage = styled('img', {
   shouldForwardProp: (prop) => isPropValid(prop),
-})<AvatarProps>(baseStyles);
+})<AvatarProps>(baseStyles, borderStyles);
 
 /**
  * The Avatar component displays an identity or an object image.
@@ -94,7 +90,7 @@ export const Avatar = ({
   src,
   alt = '',
   variant = 'object',
-  size,
+  size = 'yotta',
   ...props
 }: AvatarProps): JSX.Element => {
   const placeholder = `data:image/svg+xml;utf8,${placeholders[variant]}`;
