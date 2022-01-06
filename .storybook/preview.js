47e37a1f9575c7176adab5332d7f6aa57420dc85
Robin Métral <robin.metral@sumup.com>
2022-01-06 10:19:15 +0100

    Revert story layout

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -55,6 +55,7 @@ const SORT_ORDER = {
 };
 
 export const parameters = {
+  layout: 'centered',
   viewMode: 'docs',
   previewTabs: { 'storybook/docs/panel': { index: -1 } },
   actions: { argTypesRegex: '^on.*' },
