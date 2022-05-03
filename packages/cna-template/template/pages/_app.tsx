ac52d5f8ceaeaa514d0f38ef9ea7dd2b1711169e
Robin Métral <robin.metral@sumup.com>
2022-05-03 19:30:24 +0200

    Template fixes

--- packages/cna-template/template/pages/_app.tsx
+++ packages/cna-template/template/pages/_app.tsx
@@ -1,4 +1,3 @@
-import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { ThemeProvider, CacheProvider } from '@emotion/react';
 import { cache } from '@emotion/css';
---
+++ packages/cna-template/template/test-utils.tsx
@@ -0,0 +1,24 @@
+import { FC, ReactNode } from 'react';
+import { configureAxe } from 'jest-axe';
+import { ThemeProvider } from '@emotion/react';
+import { render as renderTest } from '@testing-library/react';
+import { light } from '@sumup/design-tokens';
+import {} from 'react';
+
+const WithProviders: FC<{ children: ReactNode }> = ({ children }) => (
+  <ThemeProvider theme={light}>{children}</ThemeProvider>
+);
+
+export const render = (
+  component: Parameters<typeof renderTest>[0],
+  options: Parameters<typeof renderTest>[1] = {},
+) => renderTest(component, { wrapper: WithProviders, ...options });
+
+export const axe = configureAxe({
+  rules: {
+    // Disable landmark rules when testing isolated components.
+    region: { enabled: false },
+  },
+});
+
+export * from '@testing-library/react';
