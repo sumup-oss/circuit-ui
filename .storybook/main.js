b273a42f3381ff3794b85ea521b604622fb91da6
Robin Métral <robin.metral@sumup.com>
2022-06-14 11:08:04 +0200

    Upgrade to Storybook 6.5

--- .storybook/main.js
+++ .storybook/main.js
@@ -33,31 +33,16 @@ module.exports = {
   ],
   features: {
     postcss: false,
-    emotionAlias: false,
   },
   core: {
     builder: 'webpack5',
+    disableTelemetry: true,
   },
   webpackFinal: createWebpackConfig,
   managerWebpack: createWebpackConfig,
 };
 
 function createWebpackConfig(config) {
-  // Transpile all node_modules under the @sumup/* namespace.
-  config.module.rules = config.module.rules.map((rule) => {
-    // Modify all rules that apply to story files.
-    if (
-      rule.test &&
-      rule.test instanceof RegExp &&
-      ['.js', '.ts', '.tsx'].some((extension) => rule.test.test(extension))
-    ) {
-      return {
-        ...rule,
-        exclude: /node_modules\/(?!(@sumup|acorn-jsx)\/).*/,
-      };
-    }
-    return rule;
-  });
   // Expose environment variables to Storybook
   config.plugins = [
     ...config.plugins,
@@ -67,14 +52,6 @@ function createWebpackConfig(config) {
       ),
     }),
   ];
-  // Emotion 11
-  config.resolve.alias = {
-    ...config.resolve.alias,
-    '@emotion/core': toPath('node_modules/@emotion/react'),
-    '@emotion/styled/base': toPath('node_modules/@emotion/styled'),
-    '@emotion/styled': toPath('node_modules/@emotion/styled'),
-    'emotion-theming': toPath('node_modules/@emotion/react'),
-  };
 
   return config;
 }
