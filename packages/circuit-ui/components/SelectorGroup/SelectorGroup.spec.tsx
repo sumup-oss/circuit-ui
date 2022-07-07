2a2d34cb973db353c9d43fc9b78a64c768154190
anacarol47 <ana.salles@sumup.com>
2022-07-07 18:03:57 +0200

    changed testing structure according to new standards

--- packages/circuit-ui/components/SelectorGroup/SelectorGroup.spec.tsx
+++ packages/circuit-ui/components/SelectorGroup/SelectorGroup.spec.tsx
@@ -15,7 +15,7 @@
 
 import { createRef } from 'react';
 
-import { create, render, renderToHtml, axe } from '../../util/test-utils';
+import { axe, render, screen } from '../../util/test-utils';
 
 import { SelectorGroup } from './SelectorGroup';
 import { MultipleLines } from './SelectorGroup.stories';
@@ -45,7 +45,7 @@ describe('SelectorGroup', () => {
    * Style tests.
    */
   it('should render with default styles', () => {
-    const actual = create(<SelectorGroup {...defaultProps} />);
+    const actual = render(<SelectorGroup {...defaultProps} />);
     expect(actual).toMatchSnapshot();
   });
 
@@ -54,22 +54,18 @@ describe('SelectorGroup', () => {
    */
   it('should check the selected option', () => {
     const value = 'second';
-    const { getByLabelText } = render(
-      <SelectorGroup {...defaultProps} value={value} />,
-    );
-    expect(getByLabelText('Option 1')).not.toHaveAttribute('checked');
-    expect(getByLabelText('Option 2')).toHaveAttribute('checked');
-    expect(getByLabelText('Option 3')).not.toHaveAttribute('checked');
+    render(<SelectorGroup {...defaultProps} value={value} />);
+    expect(screen.getByLabelText('Option 1')).not.toHaveAttribute('checked');
+    expect(screen.getByLabelText('Option 2')).toHaveAttribute('checked');
+    expect(screen.getByLabelText('Option 3')).not.toHaveAttribute('checked');
   });
 
   it('should check the selected options', () => {
     const value = ['second', 'third'];
-    const { getByLabelText } = render(
-      <SelectorGroup {...defaultProps} value={value} multiple />,
-    );
-    expect(getByLabelText('Option 1')).not.toHaveAttribute('checked');
-    expect(getByLabelText('Option 2')).toHaveAttribute('checked');
-    expect(getByLabelText('Option 3')).toHaveAttribute('checked');
+    render(<SelectorGroup {...defaultProps} value={value} multiple />);
+    expect(screen.getByLabelText('Option 1')).not.toHaveAttribute('checked');
+    expect(screen.getByLabelText('Option 2')).toHaveAttribute('checked');
+    expect(screen.getByLabelText('Option 3')).toHaveAttribute('checked');
   });
 
   it('should accept a working ref', () => {
@@ -82,7 +78,7 @@ describe('SelectorGroup', () => {
   });
 
   it('should render with horizontal layout by default', () => {
-    const actual = create(<SelectorGroup {...defaultProps} />);
+    const actual = render(<SelectorGroup {...defaultProps} />);
     expect(actual).toMatchSnapshot();
   });
 
@@ -96,10 +92,10 @@ describe('SelectorGroup', () => {
    */
   it('should meet accessibility guidelines', async () => {
     const value = 'second';
-    const wrapper = renderToHtml(
+    const { container } = render(
       <SelectorGroup {...defaultProps} value={value} />,
     );
-    const actual = await axe(wrapper);
+    const actual = await axe(container);
     expect(actual).toHaveNoViolations();
   });
 });
