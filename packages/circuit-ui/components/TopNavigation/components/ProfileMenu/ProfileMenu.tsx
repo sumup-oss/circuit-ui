8f5e5d8cfd7284a6b41bf87679932b7ec0f5432a
An Tran <hoangan08ece@gmail.com>
2022-07-15 14:10:23 +0200

    fix ProfileMenu to use the updated Popover API

--- packages/circuit-ui/components/TopNavigation/components/ProfileMenu/ProfileMenu.tsx
+++ packages/circuit-ui/components/TopNavigation/components/ProfileMenu/ProfileMenu.tsx
@@ -166,7 +166,7 @@ export function ProfileMenu({
   tracking,
 }: ProfileMenuProps): JSX.Element {
   const [isOpen, setOpen] = useState(false);
-  const offsetModifier = { name: 'offset', options: { offset: [-16, 8] } };
+  const offsetProp = { mainAxis: 8, crossAxis: -16 };
 
   return (
     <TrackingElement
@@ -187,7 +187,7 @@ export function ProfileMenu({
         )}
         actions={actions}
         placement="bottom-end"
-        modifiers={[offsetModifier]}
+        offsetProp={offsetProp}
         tracking={
           tracking ? { ...tracking, component: 'profile_menu' } : undefined
         }
