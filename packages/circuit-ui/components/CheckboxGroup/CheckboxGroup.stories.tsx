c7a9b01ab8d705e85d730b22a01552938e9dca16
An Tran <hoangan08ece@gmail.com>
2022-11-27 19:09:18 +0100

    add CheckboxGroup

---
+++ packages/circuit-ui/components/CheckboxGroup/CheckboxGroup.stories.tsx
@@ -0,0 +1,235 @@
+/**
+ * Copyright 2022, SumUp Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { useState, ChangeEvent } from 'react';
+
+import { CheckboxGroup, CheckboxGroupProps } from './CheckboxGroup';
+
+export default {
+  title: 'Forms/Checkbox/CheckboxGroup',
+  component: CheckboxGroup,
+  argTypes: {
+    name: { control: 'text' },
+    value: { control: 'array' },
+    options: { control: 'array' },
+  },
+};
+
+const CheckboxGroupWithState = ({
+  options,
+  value: initialValue,
+  ...props
+}: CheckboxGroupProps) => {
+  const [value, setValue] = useState<CheckboxGroupProps['value']>(initialValue);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const onChangeValue = event.target.value;
+    if (value) {
+      const index = value.indexOf(onChangeValue);
+      if (index === -1) {
+        setValue([...value, onChangeValue]);
+      } else {
+        setValue([...value.slice(0, index), ...value.slice(index + 1)]);
+      }
+    } else {
+      setValue([event.target.value]);
+    }
+  };
+
+  return (
+    <CheckboxGroup
+      {...props}
+      options={options}
+      value={value}
+      onChange={handleChange}
+    />
+  );
+};
+
+export const Base = (args: CheckboxGroupProps) => (
+  <CheckboxGroupWithState {...args} />
+);
+Base.args = {
+  name: 'checkbox-group-base',
+  label: 'Choose your favourite fruit',
+  options: [
+    {
+      label: 'Apple',
+      value: 'apple',
+    },
+    { label: 'Banana', value: 'banana' },
+    { label: 'Mango', value: 'mango' },
+  ],
+};
+
+export const Preselected = (args: CheckboxGroupProps) => (
+  <CheckboxGroupWithState {...args} />
+);
+Preselected.args = {
+  name: 'checkbox-group-preselected',
+  label: 'Choose your favourite fruit',
+  value: ['banana', 'mango'],
+  options: [
+    {
+      label: 'Apple',
+      value: 'apple',
+    },
+    { label: 'Banana', value: 'banana' },
+    { label: 'Mango', value: 'mango' },
+  ],
+};
+
+export const PartialyDisabled = (args: CheckboxGroupProps) => (
+  <CheckboxGroupWithState {...args} />
+);
+PartialyDisabled.args = {
+  name: 'checkbox-group-partially disabled',
+  label: 'Choose your favourite fruit',
+  options: [
+    {
+      label: 'Apple',
+      value: 'apple',
+      disabled: true,
+    },
+    { label: 'Banana', value: 'banana', disabled: true },
+    { label: 'Mango', value: 'mango', disabled: false },
+  ],
+};
+
+export const FullyDisabled = (args: CheckboxGroupProps) => (
+  <CheckboxGroupWithState {...args} />
+);
+FullyDisabled.args = {
+  name: 'checkbox-group-fully-disabled',
+  label: 'Choose your favourite fruit',
+  disabled: true,
+  options: [
+    {
+      label: 'Apple',
+      value: 'apple',
+    },
+    { label: 'Banana', value: 'banana' },
+    { label: 'Mango', value: 'mango' },
+  ],
+};
+
+const CheckboxGroupWithStateInvalid = ({
+  options: defaultOptions,
+  value: initialValue,
+  validationHint,
+  ...props
+}: CheckboxGroupProps) => {
+  const [value, setValue] = useState<CheckboxGroupProps['value']>(initialValue);
+
+  const requiredFields = ['banana', 'apple'];
+  const initialOptions: CheckboxGroupProps['options'] = defaultOptions.map(
+    (option) => {
+      const newOption = { ...option };
+      if (option?.value && requiredFields.includes(option?.value?.toString())) {
+        newOption.invalid = true;
+      }
+      return newOption;
+    },
+  );
+
+  const [options, setOptions] =
+    useState<CheckboxGroupProps['options']>(initialOptions);
+
+  const isInvalid = options.some((option) => option.invalid);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const onChangeValue = event.target.value;
+    if (value) {
+      const index = value.indexOf(onChangeValue);
+      if (index === -1) {
+        setValue([...value, onChangeValue]);
+      } else {
+        setValue([...value.slice(0, index), ...value.slice(index + 1)]);
+      }
+    } else {
+      setValue([event.target.value]);
+    }
+
+    if (requiredFields.includes(onChangeValue)) {
+      setOptions(
+        options.map((option) => {
+          const newOption = { ...option };
+          if (newOption.value === onChangeValue) {
+            newOption.invalid = !newOption.invalid;
+          }
+          return newOption;
+        }),
+      );
+    }
+  };
+
+  return (
+    <CheckboxGroup
+      {...props}
+      options={options}
+      value={value}
+      invalid={isInvalid}
+      onChange={handleChange}
+      validationHint={isInvalid ? validationHint : undefined}
+    />
+  );
+};
+
+export const Invalid = (args: CheckboxGroupProps) => (
+  <CheckboxGroupWithStateInvalid {...args} />
+);
+Invalid.args = {
+  name: 'checkbox-group-invalid',
+  label: 'Choose your favourite fruit',
+  validationHint: 'Some fields are required.',
+  options: [
+    {
+      label: 'Apple',
+      value: 'apple',
+    },
+    { label: 'Banana', value: 'banana' },
+    { label: 'Mango', value: 'mango' },
+  ],
+};
+
+export const Valid = (args: CheckboxGroupProps) => (
+  <CheckboxGroupWithState {...args} />
+);
+Valid.args = {
+  name: 'radio-button-group',
+  label: 'Choose your favourite fruit',
+  options: [
+    { label: 'Apple', value: 'apple' },
+    { label: 'Banana', value: 'banana' },
+    { label: 'Mango', value: 'mango' },
+  ],
+  showValid: true,
+  validationHint: 'You chose an option.',
+};
+
+export const Warning = (args: CheckboxGroupProps) => (
+  <CheckboxGroupWithState {...args} />
+);
+Warning.args = {
+  name: 'radio-button-group',
+  label: 'Choose your favourite fruit',
+  options: [
+    { label: 'Apple', value: 'apple' },
+    { label: 'Banana', value: 'banana' },
+    { label: 'Mango', value: 'mango' },
+  ],
+  hasWarning: true,
+  validationHint: 'This choice might cause errors.',
+};
---
+++ packages/circuit-ui/components/CheckboxGroup/CheckboxGroup.tsx
@@ -0,0 +1,220 @@
+/**
+ * Copyright 2022, SumUp Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  FieldsetHTMLAttributes,
+  InputHTMLAttributes,
+  Ref,
+  forwardRef,
+} from 'react';
+
+import styled from '../../styles/styled';
+import { typography } from '../../styles/style-mixins';
+import { uniqueId } from '../../util/id';
+import { Checkbox, CheckboxProps } from '../Checkbox/Checkbox';
+import {
+  FieldWrapper,
+  FieldLabelText,
+  FieldValidationHint,
+} from '../FieldAtoms';
+import { AccessibilityError } from '../../util/errors';
+
+export interface CheckboxGroupProps
+  extends Omit<FieldsetHTMLAttributes<HTMLFieldSetElement>, 'onChange'> {
+  /**
+   * A collection of available options. Each option must have at least a label and a value
+   * for the respective Checkbox.
+   */
+  options: (Omit<CheckboxProps, 'onChange'> & {
+    label: string;
+  })[];
+  /**
+   * The values of the Checkboxes that are checked by default.
+   */
+  value?: CheckboxProps['value'][];
+  /**
+   * A callback that is called when the any of the checkboxes change their values.
+   * Passed on to the Checkboxes.
+   */
+  onChange: CheckboxProps['onChange'];
+  /**
+   * A description of the selector group.
+   */
+  label: string;
+  /**
+   * Label to indicate that the input group is optional. Only displayed when the
+   * `required` prop is falsy.
+   */
+  optionalLabel?: string;
+  /**
+   * The ref to the HTML DOM element.
+   */
+  ref?: Ref<HTMLFieldSetElement>;
+  /**
+   * Makes the input group required.
+   */
+  required?: InputHTMLAttributes<HTMLInputElement>['required'];
+  /**
+   * An information, warning or error message, displayed below the Checkboxes.
+   */
+  validationHint?: string;
+  /**
+   * Triggers error message below the Checkboxes.
+   */
+  invalid?: boolean;
+  /**
+   * Triggers warning message below the Checkboxes.
+   */
+  hasWarning?: boolean;
+  /**
+   * Triggers valid message below the Checkboxes.
+   */
+  showValid?: boolean;
+  /**
+   * Visually hide the label. This should only be used in rare cases and only
+   * if the purpose of the field can be inferred from other context.
+   */
+  hideLabel?: boolean;
+}
+
+const Legend = styled('legend')<Record<string, unknown>>(typography('two'));
+
+/**
+ * A group of Checkboxes.
+ */
+export const CheckboxGroup = forwardRef(
+  (
+    {
+      options,
+      value,
+      onChange,
+      'name': customName,
+      label,
+      invalid,
+      validationHint,
+      showValid,
+      disabled,
+      hasWarning,
+      hideLabel,
+      optionalLabel,
+      required,
+      'aria-describedby': descriptionId,
+      ...props
+    }: CheckboxGroupProps,
+    ref: CheckboxGroupProps['ref'],
+  ) => {
+    if (
+      process.env.NODE_ENV !== 'production' &&
+      process.env.NODE_ENV !== 'test' &&
+      !label
+    ) {
+      throw new AccessibilityError(
+        'CheckboxGroup',
+        'The `label` prop is missing. Pass `hideLabel` if you intend to hide the label visually.',
+      );
+    }
+    const name = customName || uniqueId('checkbox-button-group_');
+    const validationHintId = uniqueId('validation-hint_');
+    const descriptionIds = `${
+      descriptionId ? `${descriptionId} ` : ''
+    }${validationHintId}`;
+
+    // Check if any of the Checkboxes are checked by default.
+    let checkedCheckboxes: { [k: string]: boolean } = {};
+    if (value) {
+      checkedCheckboxes = value.reduce(
+        (
+          accumulator: { [k: string]: boolean },
+          checkboxValue: CheckboxProps['value'],
+        ) => {
+          if (checkboxValue) {
+            accumulator[checkboxValue.toString()] = options.some(
+              (option) => checkboxValue === option.value,
+            );
+          }
+          return accumulator;
+        },
+        {},
+      );
+    }
+
+    // The CheckboxGroup is invalid when any of the individual Checkboxes are invalid.
+    // const isCheckboxGroupInvalid = options.some((option) => option.invalid);
+
+    return (
+      <FieldWrapper
+        as="fieldset"
+        role="radiogroup"
+        aria-describedby={descriptionIds}
+        aria-orientation="vertical"
+        name={name}
+        // @ts-expect-error TypeScript isn't smart enough to recognize the `as` prop.
+        ref={ref}
+        disabled={disabled}
+        {...props}
+      >
+        <Legend>
+          <FieldLabelText
+            label={label}
+            hideLabel={hideLabel}
+            optionalLabel={optionalLabel}
+            required={required}
+          />
+        </Legend>
+        {options &&
+          options.map(
+            ({
+              value: checkboxValue,
+              className,
+              style,
+              label: checkboxLabel,
+              ...rest
+            }) => (
+              <div
+                key={checkboxValue && checkboxValue.toString()}
+                className={className}
+                style={style}
+              >
+                <Checkbox
+                  {...{
+                    ...rest,
+                    value: checkboxValue,
+                    name,
+                    required,
+                    onChange,
+                    checked:
+                      !!checkboxValue &&
+                      checkedCheckboxes[checkboxValue.toString()],
+                  }}
+                >
+                  {checkboxLabel}
+                </Checkbox>
+              </div>
+            ),
+          )}
+        <FieldValidationHint
+          id={validationHintId}
+          invalid={invalid}
+          showValid={showValid}
+          disabled={disabled}
+          hasWarning={hasWarning}
+          validationHint={validationHint}
+        />
+      </FieldWrapper>
+    );
+  },
+);
+
+CheckboxGroup.displayName = 'CheckboxGroup';
