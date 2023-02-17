c1fcf299b69b232f6e1849b7f8880155c245baec
Connor Bär <connor.baer@sumup.com>
2023-02-17 14:40:01 +0100

    Upgrade to Storybook v7

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,56 +1,42 @@
 const webpack = require('webpack');
 const path = require('path');
-const toPath = (_path) => path.join(process.cwd(), _path);
-
+const toPath = _path => path.join(process.cwd(), _path);
 module.exports = {
   staticDirs: [toPath('.storybook/public')],
-  stories: [
-    '../packages/**/*.stories.@(js|ts|tsx|mdx)',
-    '../docs/**/*.stories.@(js|ts|tsx|mdx)',
-  ],
-  addons: [
-    {
-      name: '@storybook/preset-typescript',
-      options: { transpileManager: true },
-    },
-    {
-      name: '@storybook/addon-docs',
-      options: {
-        sourceLoaderOptions: {
-          injectStoryParameters: false,
-        },
-      },
-    },
-    '@storybook/addon-storysource',
-    '@storybook/addon-controls',
-    '@storybook/addon-actions',
-    '@storybook/addon-a11y',
-    '@storybook/addon-links',
-    '@storybook/addon-viewport',
-    '@storybook/addon-backgrounds',
-    '@storybook/addon-interactions',
-  ],
+  stories: ['../packages/**/*.@(mdx|stories.@(js|ts|tsx))', '../docs/**/*.@(mdx|stories.@(js|ts|tsx))'],
+  addons: [{
+    name: '@storybook/preset-typescript',
+    options: {
+      transpileManager: true
+    }
+  }, {
+    name: '@storybook/addon-docs',
+    options: {
+      sourceLoaderOptions: {
+        injectStoryParameters: false
+      }
+    }
+  }, '@storybook/addon-storysource', '@storybook/addon-controls', '@storybook/addon-actions', '@storybook/addon-a11y', '@storybook/addon-links', '@storybook/addon-viewport', '@storybook/addon-backgrounds', '@storybook/addon-interactions'],
   features: {
-    postcss: false,
+    postcss: false
   },
   core: {
-    builder: 'webpack5',
-    disableTelemetry: true,
+    disableTelemetry: true
   },
   webpackFinal: createWebpackConfig,
   managerWebpack: createWebpackConfig,
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
 };
-
 function createWebpackConfig(config) {
   // Expose environment variables to Storybook
-  config.plugins = [
-    ...config.plugins,
-    new webpack.DefinePlugin({
-      'process.env.UNSAFE_DISABLE_NO_MARGIN_ERRORS': JSON.stringify(
-        process.env.UNSAFE_DISABLE_NO_MARGIN_ERRORS,
-      ),
-    }),
-  ];
-
+  config.plugins = [...config.plugins, new webpack.DefinePlugin({
+    'process.env.UNSAFE_DISABLE_NO_MARGIN_ERRORS': JSON.stringify(process.env.UNSAFE_DISABLE_NO_MARGIN_ERRORS)
+  })];
   return config;
 }
