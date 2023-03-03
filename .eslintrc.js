226e6cd927ec71bb91538b57cf5fe474443ccabb
Connor Bär <connor-baer@users.noreply.github.com>
2023-03-03 15:26:22 +0100

    Remove codemods from previous Circuit UI versions (#1973
    * Remove obsolete codemods and CLI
    
    * Add changeset

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,38 +11,6 @@ module.exports = require('@sumup/foundry/eslint')({
       },
     },
     {
-      files: [
-        'packages/circuit-ui/cli/migrate/__testfixtures__/**/*.input.*',
-        'packages/circuit-ui/cli/migrate/__testfixtures__/**/*.output.*',
-      ],
-      rules: {
-        'import/no-unresolved': 'off',
-        'import/no-extraneous-dependencies': 'off',
-        'notice/notice': 'off',
-        '@typescript-eslint/no-unused-vars': 'off',
-        'prettier/prettier': 'off',
-        'no-unused-vars': 'off',
-        'react/prop-types': 'off',
-        'no-console': 'off',
-      },
-    },
-    {
-      files: [
-        'packages/circuit-ui/cli/migrate/*.ts',
-        'packages/circuit-ui/cli/migrate/utils/*.ts',
-      ],
-      rules: {
-        // jscodeshift expects no return value for files
-        // that should not be transformed.
-        'consistent-return': 'off',
-        'no-console': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-unsafe-call': 'off',
-        '@typescript-eslint/no-unsafe-assignment': 'off',
-        '@typescript-eslint/no-unsafe-member-access': 'off',
-      },
-    },
-    {
       files: ['packages/icons/scripts/*'],
       rules: {
         'import/no-extraneous-dependencies': 'off',
--- packages/circuit-ui/cli/migrate/__testfixtures__/component-names-v5.output.js
+++
@@ -1,8 +0,0 @@
-import styled from '@emotion/styled';
-import { Button } from '@sumup/circuit-ui';
-
-const BaseLoadingButton = () => <Button />;
-
-const LoadingButtonStyle = styled(Button)`
-  color: red;
-`;
