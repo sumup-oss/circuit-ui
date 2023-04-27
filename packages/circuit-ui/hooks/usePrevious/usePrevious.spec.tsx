df9d81f261a73916bd2542066c6f1a629d5f32e8
Connor Bär <connor.baer@sumup.com>
2023-04-27 17:18:33 +0200

    Add unit tests for useLatest and usePrevious

---
+++ packages/circuit-ui/hooks/usePrevious/usePrevious.spec.tsx
@@ -0,0 +1,45 @@
+/**
+ * Copyright 2019, SumUp Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { describe, expect, it } from 'vitest';
+
+import { renderHook } from '../../util/test-utils.js';
+
+import { usePrevious } from './usePrevious.js';
+
+describe('usePrevious', () => {
+  afterEach(() => {});
+
+  it('should be undefined on first render', () => {
+    const { result } = renderHook((value = 'initialValue') =>
+      usePrevious(value),
+    );
+
+    expect(result.current).toBeUndefined();
+  });
+
+  it('should return the previous value on the next render', () => {
+    const initialValue = 'value';
+    const { result, rerender } = renderHook((value = initialValue) =>
+      usePrevious(value),
+    );
+
+    const newValue = 'newValue';
+
+    rerender(newValue);
+
+    expect(result.current).toEqual(initialValue);
+  });
+});
