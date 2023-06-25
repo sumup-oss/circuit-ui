2289ec37161e5cb9d5162a5043a4e60066925c6f
Connor Bär <connor.baer@sumup.com>
2023-06-26 00:37:30 +0200

    Migrate Anchor component to CSS modules

--- packages/circuit-ui/components/Anchor/Anchor.spec.tsx
+++ packages/circuit-ui/components/Anchor/Anchor.spec.tsx
@@ -15,7 +15,6 @@
 
 import { describe, expect, it, vi } from 'vitest';
 import { createRef } from 'react';
-import { css } from '@emotion/react';
 
 import { render, axe, userEvent, screen } from '../../util/test-utils.js';
 import { ClickEvent } from '../../types/events.js';
@@ -25,104 +24,93 @@ import { Anchor } from './Anchor.js';
 describe('Anchor', () => {
   const baseProps = { children: 'Anchor' };
 
-  describe('styles', () => {
-    it('should render with default styles', () => {
-      const { container } = render(
-        <Anchor {...baseProps} href="https://sumup.com" />,
-      );
-      expect(container).toMatchSnapshot();
-    });
-
-    it('should render with custom styles', () => {
-      const { container } = render(
-        <Anchor
-          {...baseProps}
-          href="https://sumup.com"
-          css={css`
-            color: rebeccapurple;
-          `}
-        />,
-      );
-      expect(container).toMatchSnapshot();
-    });
+  it('should merge a custom class name with the default ones', () => {
+    const className = 'foo';
+    const { container } = render(
+      <Anchor className={className} href="https://sumup.com">
+        Anchor
+      </Anchor>,
+    );
+    const anchor = container.querySelector('a');
+    expect(anchor?.className).toContain(className);
   });
 
-  describe('business logic', () => {
-    it('should render as a `span` when neither href nor onClick is passed', () => {
-      const { container } = render(<Anchor {...baseProps} />);
-      const actual = container.querySelector('span');
-      expect(actual).toBeVisible();
-    });
-
-    it('should render as an `a` when an href (and onClick) is passed', () => {
-      const { container } = render(
-        <Anchor {...baseProps} href="https://sumup.com" onClick={vi.fn} />,
-      );
-      const actual = container.querySelector('a');
-      expect(actual).toBeVisible();
-    });
-
-    it('should render as a `button` when an onClick is passed', () => {
-      const { container } = render(<Anchor {...baseProps} onClick={vi.fn} />);
-      const actual = container.querySelector('button');
-      expect(actual).toBeVisible();
-    });
-
-    it('should call the onClick handler when rendered as a link', async () => {
-      const onClick = vi.fn((event: ClickEvent) => {
-        event.preventDefault(); // navigation is not implemented in jsdom
-      });
-      render(
-        <Anchor {...baseProps} href="https://sumup.com" onClick={onClick} />,
-      );
-
-      await userEvent.click(screen.getByRole('link'));
-
-      expect(onClick).toHaveBeenCalledTimes(1);
-    });
-
-    it('should call the onClick handler when rendered as a button', async () => {
-      const onClick = vi.fn();
-      render(<Anchor {...baseProps} onClick={onClick} />);
-
-      await userEvent.click(screen.getByRole('button'));
-
-      expect(onClick).toHaveBeenCalledTimes(1);
-    });
-
-    it('should accept a working ref for a button', () => {
-      const tref = createRef<any>();
-      const { container } = render(
-        <Anchor {...baseProps} onClick={vi.fn} ref={tref} />,
-      );
-      const button = container.querySelector('button');
-      expect(tref.current).toBe(button);
-    });
-
-    it('should accept a working ref for a link', () => {
-      const tref = createRef<any>();
-      const { container } = render(
-        <Anchor {...baseProps} href="https://sumup.com" ref={tref} />,
-      );
-      const anchor = container.querySelector('a');
-      expect(tref.current).toBe(anchor);
-    });
-
-    it('should accept a working ref for a span', () => {
-      const tref = createRef<any>();
-      const { container } = render(<Anchor {...baseProps} ref={tref} />);
-      const span = container.querySelector('span');
-      expect(tref.current).toBe(span);
-    });
+  it('should forward a ref for a button', () => {
+    const ref = createRef<HTMLButtonElement>();
+    const { container } = render(
+      <Anchor ref={ref} onClick={vi.fn}>
+        Anchor
+      </Anchor>,
+    );
+    const button = container.querySelector('button');
+    expect(ref.current).toBe(button);
   });
 
-  describe('accessibility', () => {
-    it('should meet accessibility guidelines', async () => {
-      const { container } = render(
-        <Anchor {...baseProps} href="https://sumup.com" onClick={vi.fn} />,
-      );
-      const actual = await axe(container);
-      expect(actual).toHaveNoViolations();
+  it('should forward a ref for a link', () => {
+    const ref = createRef<HTMLAnchorElement>();
+    const { container } = render(
+      <Anchor ref={ref} href="https://sumup.com">
+        Anchor
+      </Anchor>,
+    );
+    const anchor = container.querySelector('a');
+    expect(ref.current).toBe(anchor);
+  });
+
+  it('should forward a ref for a span', () => {
+    const ref = createRef<HTMLSpanElement>();
+    const { container } = render(<Anchor ref={ref}>Anchor</Anchor>);
+    const span = container.querySelector('span');
+    expect(ref.current).toBe(span);
+  });
+
+  it('should render as a `span` when neither href nor onClick is passed', () => {
+    const { container } = render(<Anchor {...baseProps} />);
+    const actual = container.querySelector('span');
+    expect(actual).toBeVisible();
+  });
+
+  it('should render as an `a` when an href (and onClick) is passed', () => {
+    const { container } = render(
+      <Anchor {...baseProps} href="https://sumup.com" onClick={vi.fn} />,
+    );
+    const actual = container.querySelector('a');
+    expect(actual).toBeVisible();
+  });
+
+  it('should render as a `button` when an onClick is passed', () => {
+    const { container } = render(<Anchor {...baseProps} onClick={vi.fn} />);
+    const actual = container.querySelector('button');
+    expect(actual).toBeVisible();
+  });
+
+  it('should call the onClick handler when rendered as a link', async () => {
+    const onClick = vi.fn((event: ClickEvent) => {
+      event.preventDefault(); // navigation is not implemented in jsdom
     });
+    render(
+      <Anchor {...baseProps} href="https://sumup.com" onClick={onClick} />,
+    );
+
+    await userEvent.click(screen.getByRole('link'));
+
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  it('should call the onClick handler when rendered as a button', async () => {
+    const onClick = vi.fn();
+    render(<Anchor {...baseProps} onClick={onClick} />);
+
+    await userEvent.click(screen.getByRole('button'));
+
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  it('should meet accessibility guidelines', async () => {
+    const { container } = render(
+      <Anchor {...baseProps} href="https://sumup.com" onClick={vi.fn} />,
+    );
+    const actual = await axe(container);
+    expect(actual).toHaveNoViolations();
   });
 });
