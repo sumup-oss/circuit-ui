b3af2c208d37978e913ce48f14a1a105993c4a33
Connor Bär <connor.baer@sumup.com>
2023-06-25 17:15:58 +0100

    Migrate Body component to CSS modules

--- packages/circuit-ui/components/Body/Body.tsx
+++ packages/circuit-ui/components/Body/Body.tsx
@@ -13,21 +13,20 @@
  * limitations under the License.
  */
 
-import { forwardRef, HTMLAttributes, Ref } from 'react';
-import { css } from '@emotion/react';
+import { forwardRef, HTMLAttributes } from 'react';
 
-import isPropValid from '../../styles/is-prop-valid.js';
-import styled, { StyleProps } from '../../styles/styled.js';
-import { AsPropType, EmotionAsPropType } from '../../types/prop-types.js';
+import type { AsPropType } from '../../types/prop-types.js';
+import { clsx } from '../../styles/clsx.js';
 
-type Size = 'one' | 'two';
+import classes from './Body.module.css';
+
 type Variant = 'highlight' | 'quote' | 'confirm' | 'alert' | 'subtle';
 
 export interface BodyProps extends HTMLAttributes<HTMLParagraphElement> {
   /**
    * Choose from 2 font sizes. Default `one`.
    */
-  size?: Size;
+  size?: 'one' | 'two';
   /**
    * Choose from style variants.
    */
@@ -36,61 +35,7 @@ export interface BodyProps extends HTMLAttributes<HTMLParagraphElement> {
    * Render the text using any HTML element.
    */
   as?: AsPropType;
-  /**
-   * The ref to the HTML DOM element.
-   */
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  ref?: Ref<any>;
 }
-
-const baseStyles = ({ theme }: StyleProps) => css`
-  font-weight: ${theme.fontWeight.regular};
-`;
-
-const sizeStyles = ({ theme, size = 'one' }: BodyProps & StyleProps) => css`
-  font-size: ${theme.typography.body[size].fontSize};
-  line-height: ${theme.typography.body[size].lineHeight};
-`;
-
-const variantStyles = ({ theme, variant }: BodyProps & StyleProps) => {
-  // TODO: Align variant names with token names in the next major.
-  switch (variant) {
-    case 'highlight': {
-      return css`
-        font-weight: ${theme.fontWeight.bold};
-      `;
-    }
-    case 'quote': {
-      return css`
-        font-style: italic;
-        padding-left: ${theme.spacings.kilo};
-        border-left: ${theme.borderWidth.mega} solid var(--cui-border-accent);
-      `;
-    }
-    case 'confirm': {
-      return css`
-        color: var(--cui-fg-success);
-      `;
-    }
-    case 'alert': {
-      return css`
-        color: var(--cui-fg-danger);
-      `;
-    }
-    case 'subtle': {
-      return css`
-        color: var(--cui-fg-subtle);
-      `;
-    }
-    default: {
-      return null;
-    }
-  }
-};
-
-const StyledBody = styled('p', {
-  shouldForwardProp: (prop) => isPropValid(prop) && prop !== 'size',
-})<BodyProps>(baseStyles, sizeStyles, variantStyles);
 
 function getHTMLElement(variant?: Variant): AsPropType {
   if (variant === 'highlight') {
@@ -106,9 +51,22 @@ function getHTMLElement(variant?: Variant): AsPropType {
  * The Body component is used to present the core textual content
  * to our users.
  */
-export const Body = forwardRef((props: BodyProps, ref?: BodyProps['ref']) => {
-  const as = props.as || getHTMLElement(props.variant);
-  return <StyledBody {...props} ref={ref} as={as as EmotionAsPropType} />;
-});
+export const Body = forwardRef<HTMLParagraphElement, BodyProps>(
+  ({ className, as, size = 'one', variant, ...props }, ref) => {
+    const Element = as || getHTMLElement(variant);
+    return (
+      <Element
+        {...props}
+        ref={ref}
+        className={clsx(
+          classes.base,
+          classes[size],
+          variant && classes[variant],
+          className,
+        )}
+      />
+    );
+  },
+);
 
 Body.displayName = 'Body';
