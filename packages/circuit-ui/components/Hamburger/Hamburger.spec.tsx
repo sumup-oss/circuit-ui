8fa83e044e6514fe8a330e141a5a0e0cd7603197
Connor Bär <connor.baer@sumup.com>
2023-06-27 13:49:18 +0200

    Migrate Hamburger component to CSS modules

--- packages/circuit-ui/components/Hamburger/Hamburger.spec.tsx
+++ packages/circuit-ui/components/Hamburger/Hamburger.spec.tsx
@@ -14,70 +14,54 @@
  */
 
 import { describe, expect, it, vi } from 'vitest';
+import { createRef } from 'react';
 
-import {
-  create,
-  renderToHtml,
-  axe,
-  render,
-  userEvent,
-  RenderFn,
-} from '../../util/test-utils.js';
+import { axe, render, userEvent, screen } from '../../util/test-utils.js';
 
-import { Hamburger, HamburgerProps } from './Hamburger.js';
+import { Hamburger } from './Hamburger.js';
 
 describe('Hamburger', () => {
-  function renderHamburger<T>(renderFn: RenderFn<T>, props: HamburgerProps) {
-    return renderFn(<Hamburger {...props} />);
-  }
-
   const baseProps = {
     activeLabel: 'Close menu',
     inactiveLabel: 'Open menu',
   };
 
-  /**
-   * Style tests.
-   */
-  it('should render with default styles', () => {
-    const actual = renderHamburger(create, baseProps);
-    expect(actual).toMatchSnapshot();
+  it('should merge a custom class name with the default ones', () => {
+    const className = 'foo';
+    const { container } = render(
+      <Hamburger {...baseProps} className={className} />,
+    );
+    const hamburger = container.querySelector('button');
+    expect(hamburger?.className).toContain(className);
   });
 
-  it('should render with active styles when passed the isActive prop', () => {
-    const actual = renderHamburger(create, { ...baseProps, isActive: true });
-    expect(actual).toMatchSnapshot();
+  it('should forward a ref', () => {
+    const ref = createRef<any>();
+    const { container } = render(<Hamburger {...baseProps} ref={ref} />);
+    const hamburger = container.querySelector('button');
+    expect(ref.current).toBe(hamburger);
   });
 
-  const sizes: HamburgerProps['size'][] = ['kilo', 'giga'];
-
-  it.each(sizes)('should render with %s styles', (size) => {
-    const actual = renderHamburger(create, { ...baseProps, size });
-    expect(actual).toMatchSnapshot();
+  it('should have the relevant aria attribute when active', () => {
+    render(<Hamburger {...baseProps} isActive />);
+    const button = screen.getByRole('button');
+    expect(button).toHaveAttribute('aria-pressed', 'true');
   });
 
-  /**
-   * Logic tests.
-   */
   it('should call the onClick prop when clicked', async () => {
     const onClick = vi.fn();
-    const { getByTestId } = renderHamburger(render, {
-      ...baseProps,
-      onClick,
-      'data-testid': 'hamburger',
-    });
+    const { getByTestId } = render(
+      <Hamburger {...baseProps} onClick={onClick} data-testid="hamburger" />,
+    );
 
     await userEvent.click(getByTestId('hamburger'));
 
     expect(onClick).toHaveBeenCalledTimes(1);
   });
 
-  /**
-   * Accessibility tests.
-   */
-  it('should meet accessibility guidelines', async () => {
-    const wrapper = renderHamburger(renderToHtml, baseProps);
-    const actual = await axe(wrapper);
+  it('should have no accessibility violations', async () => {
+    const { container } = render(<Hamburger {...baseProps} />);
+    const actual = await axe(container);
     expect(actual).toHaveNoViolations();
   });
 });
