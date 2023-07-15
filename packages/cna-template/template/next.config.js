bf376929dbf3f68bef70d53b95a0733bc67d9f85
Connor Bär <connor.baer@sumup.com>
2023-07-15 21:22:44 +0200

    Transpile @sumup/circuit

--- packages/cna-template/template/next.config.js
+++ packages/cna-template/template/next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['@sumup/circuit-ui'],
 };
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
