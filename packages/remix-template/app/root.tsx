870997d7874b0e7fe0ae12b21a4cd478f77dab9e
Connor Bär <connor.baer@sumup.com>
2023-07-15 23:15:37 +0200

    Customize the homepage

--- packages/remix-template/app/root.tsx
+++ packages/remix-template/app/root.tsx
@@ -8,6 +8,12 @@ import {
   Scripts,
   ScrollRestoration,
 } from '@remix-run/react';
+import { SumUpLogo } from '@sumup/icons';
+
+import '@sumup/design-tokens/light.css';
+import '@sumup/circuit-ui/styles.css';
+
+import styles from './root.module.css';
 
 export const links: LinksFunction = () => [
   ...(cssBundleHref ? [{ rel: 'stylesheet', href: cssBundleHref }] : []),
@@ -23,7 +29,21 @@ export default function App() {
         <Links />
       </head>
       <body>
-        <Outlet />
+        <div className={styles.base}>
+          <main className={styles.main}>
+            <Outlet />
+          </main>
+          <footer className={styles.footer}>
+            <a
+              href="https://sumup.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              aria-label="Open SumUp's homepage in a new tab"
+            >
+              <SumUpLogo className={styles.logo} />
+            </a>
+          </footer>
+        </div>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
---
+++ packages/remix-template/app/routes/_index/route.tsx
@@ -0,0 +1,47 @@
+import type { V2_MetaFunction } from '@remix-run/node';
+import { Title, BodyLarge } from '@sumup/circuit-ui';
+
+import { DocCard } from '../../components/DocCard';
+
+import styles from './route.module.css';
+
+const title = 'Welcome to Remix + Circuit UI';
+
+export const meta: V2_MetaFunction = () => [
+  { title },
+  { name: 'description', content: 'Welcome to Remix!' },
+];
+
+export default function Index() {
+  return (
+    <>
+      <Title as="h1" size="three">
+        {title}
+      </Title>
+
+      <BodyLarge className={styles.intro}>
+        Get started by editing <code>app/routes/_index/route.tsx</code>
+      </BodyLarge>
+
+      <div className={styles.cards}>
+        <DocCard
+          title="Remix"
+          description="Find in-depth information about Remix features and API."
+          href="https://remix.run/docs"
+        />
+
+        <DocCard
+          title="Circuit UI"
+          description="Discover SumUp's design system for the web."
+          href="https://circuit.sumup.com"
+        />
+
+        <DocCard
+          title="Foundry"
+          description="Learn about SumUp's toolkit for building TypeScript applications."
+          href="https://github.com/sumup-oss/foundry"
+        />
+      </div>
+    </>
+  );
+}
