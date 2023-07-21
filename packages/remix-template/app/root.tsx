821715db0cc232a199c45ea08d50dc7e0ec2df2b
Connor Bär <connor.baer@sumup.com>
2023-07-21 13:25:26 +0200

    Add favicons

--- packages/remix-template/app/root.tsx
+++ packages/remix-template/app/root.tsx
@@ -25,6 +25,55 @@ export default function App() {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
+
+        {/* Browser icons */}
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="https://static.sumup.com/favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="https://static.sumup.com/favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="https://static.sumup.com/favicons/favicon-16x16.png"
+        />
+        <link
+          rel="manifest"
+          href="https://static.sumup.com/favicons/site.webmanifest"
+        />
+        <link
+          rel="mask-icon"
+          href="https://static.sumup.com/favicons/safari-pinned-tab.svg"
+          color="#ffffff"
+        />
+        <link
+          rel="shortcut icon"
+          href="https://static.sumup.com/favicons/favicon.ico"
+        />
+        <meta
+          name="msapplication-config"
+          content="https://static.sumup.com/favicons/browserconfig.xml"
+        />
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="theme-color" content="#ffffff" />
+
+        {/* Performance optimizations */}
+        <link
+          rel="preload"
+          href="https://static.sumup.com/fonts/latin-greek-cyrillic/aktiv-grotest-400.woff2"
+        />
+        <link
+          rel="preload"
+          href="https://static.sumup.com/fonts/latin-greek-cyrillic/aktiv-grotest-700.woff2"
+        />
+
         <Meta />
         <Links />
       </head>
