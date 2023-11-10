93aa3a3198e2ffe85def192d480c1345f8b6bced
Connor Bär <github@connorbaer.com>
2023-11-10 14:23:58 +0100

    Migrate CNA template to app folder

--- packages/cna-template/template/next.config.js
+++ packages/cna-template/template/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  experimental: {
+    optimizePackageImports: ['@sumup/circuit-ui'],
+  },
 };
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
