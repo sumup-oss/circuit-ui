92046bad53098ac4bb3816279fcc393b5e9fbfee
Connor Bär <connor-baer@users.noreply.github.com>
2024-01-08 14:36:50 +0100

    Improve icon search (#2378)

--- .storybook/components/Icons.tsx
+++ .storybook/components/Icons.tsx
@@ -13,7 +13,12 @@
  * limitations under the License.
  */
 
-import { useState } from 'react';
+import {
+  type Dispatch,
+  type SetStateAction,
+  useState,
+  ChangeEvent,
+} from 'react';
 import { Unstyled } from '@storybook/addon-docs';
 import * as iconComponents from '@sumup/icons';
 import type { IconsManifest } from '@sumup/icons';
@@ -24,12 +29,15 @@ import {
   SearchInput,
   Select,
   Badge,
+  SelectorGroup,
+  clsx,
+  utilClasses,
 } from '../../packages/circuit-ui/index.js';
 import classes from './Icons.module.css';
 
 function groupBy(
   icons: IconsManifest['icons'],
-  key: keyof IconsManifest['icons'][0],
+  key: 'name' | 'category' | 'size',
 ) {
   return icons.reduce((groups, icon) => {
     (groups[icon[key]] = groups[icon[key]] || []).push(icon);
@@ -39,7 +47,7 @@ function groupBy(
 
 function sortBy(
   icons: IconsManifest['icons'],
-  key: keyof IconsManifest['icons'][0],
+  key: 'name' | 'category' | 'size',
 ) {
   return icons.sort((iconA, iconB) => {
     return iconA[key].localeCompare(iconB[key]);
@@ -60,24 +68,20 @@ const Icons = () => {
   const [search, setSearch] = useState('');
   const [size, setSize] = useState('all');
   const [color, setColor] = useState('var(--cui-fg-normal)');
+  const [scale, setScale] = useState('one-x');
 
-  const handleSearch = (event) => {
-    setSearch(event.target.value);
-  };
-
-  const handleSizeChange = (event) => {
-    setSize(event.target.value);
-  };
-
-  const handleColorChange = (event) => {
-    setColor(event.target.value);
-  };
+  const handleChange =
+    (setState: Dispatch<SetStateAction<string>>) =>
+    (event: ChangeEvent<any>) => {
+      setState(event.target.value);
+    };
 
   const sizeOptions = [
     { label: 'All sizes', value: 'all' },
     { label: '16', value: '16' },
     { label: '24', value: '24' },
     { label: '32', value: '32' },
+    { label: '48', value: '48' },
   ];
 
   const colorOptions = [
@@ -90,35 +94,50 @@ const Icons = () => {
     { label: 'On Strong', value: 'var(--cui-fg-on-strong)' },
   ];
 
-  const activeIcons = iconsManifest.icons.filter(
-    (icon) =>
-      icon.name.includes(search) && (size === 'all' || size === icon.size),
-  );
+  const scaleOptions = [
+    { label: '1x', value: 'one-x' },
+    { label: '2x', value: 'two-x' },
+  ];
+
+  const activeIcons = iconsManifest.icons.filter((icon) => {
+    const matchesKeyword = [icon.name, ...(icon.keywords || [])].some(
+      (keyword) => keyword.toLowerCase().includes(search.toLowerCase()),
+    );
+    const matchesSize = size === 'all' || size === icon.size;
+    return matchesKeyword && matchesSize;
+  }) as IconsManifest['icons'];
 
   return (
     <Unstyled>
-      <div className={classes.filters}>
+      <fieldset className={classes.filters}>
+        <legend className={utilClasses.hideVisually}>Icon filters</legend>
         <SearchInput
-          label="Filter icons by name"
+          label="Search by name or keyword"
           placeholder="Search..."
           value={search}
-          onChange={handleSearch}
+          onChange={handleChange(setSearch)}
           onClear={() => setSearch('')}
           clearLabel="Clear"
         />
         <Select
-          label="Select icon size"
+          label="Size"
           options={sizeOptions}
           value={size}
-          onChange={handleSizeChange}
+          onChange={handleChange(setSize)}
         />
         <Select
-          label="Select icon color"
+          label="Color"
           options={colorOptions}
           value={color}
-          onChange={handleColorChange}
+          onChange={handleChange(setColor)}
         />
-      </div>
+        <SelectorGroup
+          label="Scale"
+          options={scaleOptions}
+          value={scale}
+          onChange={handleChange(setScale)}
+        />
+      </fieldset>
 
       {activeIcons.length <= 0 ? (
         <Body>No icons found</Body>
@@ -137,7 +156,9 @@ const Icons = () => {
                 const Icon = iconComponents[componentName];
                 return (
                   <div key={id} className={classes.wrapper}>
-                    <div className={classes['icon-wrapper']}>
+                    <div
+                      className={clsx(classes['icon-wrapper'], classes[scale])}
+                    >
                       <Icon
                         aria-labelledby={id}
                         size={icon.size}
