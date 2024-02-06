b52f5c31f1f4ef129e29e6987360118c7e530ade
Connor Bär <connor-baer@users.noreply.github.com>
2024-02-06 11:36:50 +0000

    Fix border color token names in the dark theme (#2426)

--- packages/design-tokens/types/index.ts
+++ packages/design-tokens/types/index.ts
@@ -185,8 +185,35 @@ interface BaseToken {
   value: unknown;
 }
 
+type ColorUsage = 'fg' | 'bg' | 'border';
+type ColorSentiment =
+  | 'normal'
+  | 'subtle'
+  | 'highlight'
+  | 'accent'
+  | 'placeholder'
+  | 'promo'
+  | 'divider'
+  | 'success'
+  | 'warning'
+  | 'danger';
+type ColorVariant = 'strong' | 'on-strong';
+type ColorInteraction = 'hovered' | 'pressed' | 'disabled';
+
 interface ColorToken extends BaseToken {
   type: 'color';
+  name: // usage - sentiment - variant - interaction, with sentiment, variant and interaction being optional
+  | `--cui-${ColorUsage}-${ColorSentiment}`
+    | `--cui-${ColorUsage}-${ColorVariant}`
+    | `--cui-${ColorUsage}-${ColorSentiment}-${ColorVariant | ColorInteraction}`
+    | `--cui-${ColorUsage}-${ColorSentiment}-${ColorVariant}-${ColorInteraction}`
+    // Special colors
+    | '--cui-bg-elevated'
+    | '--cui-bg-overlay'
+    | '--cui-border-focus'
+    // FIXME: The '--cui-fg-on-strong-subtle' tokens don't follow the naming scheme.
+    | `--cui-${ColorUsage}-${ColorVariant}-${ColorSentiment | ColorInteraction}`
+    | `--cui-${ColorUsage}-${ColorVariant}-${ColorSentiment}-${ColorInteraction}`;
   value: Color;
 }
 
