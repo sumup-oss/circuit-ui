90583346b1a1a46e3648a3f42d57ed60bb003a66
Connor Bär <connor-baer@users.noreply.github.com>
2024-03-04 23:06:49 +0100

    Version Packages (#2460)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- packages/remix-template/.eslintrc.cjs
+++ packages/remix-template/.eslintrc.cjs
@@ -1,13 +1,6 @@
 module.exports = require('@sumup/foundry/eslint')({
   extends: ['@remix-run/eslint-config', '@remix-run/eslint-config/node'],
-  plugins: ['@sumup/circuit-ui'],
   rules: {
     'notice/notice': 'off',
-    '@sumup/circuit-ui/component-lifecycle-imports': 'error',
-    '@sumup/circuit-ui/no-invalid-custom-properties': 'error',
-    '@sumup/circuit-ui/no-deprecated-components': 'error',
-    '@sumup/circuit-ui/no-deprecated-props': 'error',
-    '@sumup/circuit-ui/no-renamed-props': 'error',
-    '@sumup/circuit-ui/prefer-custom-properties': 'error',
   },
 });
--- packages/stylelint-plugin-circuit-ui/CHANGELOG.md
+++ packages/stylelint-plugin-circuit-ui/CHANGELOG.md
@@ -1,5 +1,11 @@
 # @sumup/stylelint-plugin-circuit-ui
 
+## 2.0.0
+
+### Major Changes
+
+- [#2406](https://github.com/sumup-oss/circuit-ui/pull/2406) [`455e5ed`](https://github.com/sumup-oss/circuit-ui/commit/455e5edf1ae951bbac04c2e523720a6544deb95e) Thanks [@connor-baer](https://github.com/connor-baer)! - Upgraded to [Stylelint 16](https://stylelint.io/migration-guide/to-16/#removed-support-for-nodejs-less-than-18120) and migrated to ECMAScript modules (ESM). The minimum required Node.js version is 18.12.0.
+
 ## 1.0.0
 
 ### Major Changes
