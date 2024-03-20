12b8b25851a572ed01c4de7a4fc2fc293af2c64d
Connor Bär <github@connorbaer.com>
2024-03-20 11:43:03 +0100

    Add Tooltip to public exports

--- .storybook/components/Icons.tsx
+++ .storybook/components/Icons.tsx
@@ -30,11 +30,11 @@ import {
   SearchInput,
   Select,
   Badge,
-  Tooltip,
   SelectorGroup,
   clsx,
   utilClasses,
 } from '../../packages/circuit-ui/index.js';
+import { Tooltip } from '../../packages/circuit-ui/experimental.js';
 import { slugify } from '../slugify.js';
 import classes from './Icons.module.css';
 
