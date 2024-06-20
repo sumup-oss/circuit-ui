629ed682190f165a83688743fe6f0af2667bf22d
Connor Bär <github@connorbaer.com>
2024-06-20 17:26:11 +0200

    Apply formatting changes

--- packages/eslint-plugin-circuit-ui/no-renamed-props/index.ts
+++ packages/eslint-plugin-circuit-ui/no-renamed-props/index.ts
@@ -428,11 +428,14 @@ export const noRenamedProps = createRule({
       });
     }
 
-    const components = configs.reduce((acc, config) => {
-      acc[config.component] = acc[config.component] || [];
-      acc[config.component].push(config);
-      return acc;
-    }, {} as Record<string, Config[]>);
+    const components = configs.reduce(
+      (acc, config) => {
+        acc[config.component] = acc[config.component] || [];
+        acc[config.component].push(config);
+        return acc;
+      },
+      {} as Record<string, Config[]>,
+    );
 
     const componentVisitors = Object.entries(components).reduce(
       (visitors, [component, configs]) => {
@@ -459,14 +462,17 @@ export const noRenamedProps = createRule({
       {} as TSESLint.RuleListener,
     );
 
-    const hooks = configs.reduce((acc, config) => {
-      if (!config.hook) {
+    const hooks = configs.reduce(
+      (acc, config) => {
+        if (!config.hook) {
+          return acc;
+        }
+        acc[config.hook] = acc[config.hook] || [];
+        acc[config.hook].push(config);
         return acc;
-      }
-      acc[config.hook] = acc[config.hook] || [];
-      acc[config.hook].push(config);
-      return acc;
-    }, {} as Record<string, Config[]>);
+      },
+      {} as Record<string, Config[]>,
+    );
 
     const hookVisitors = Object.entries(hooks).reduce(
       (visitors, [hook, configs]) => {
