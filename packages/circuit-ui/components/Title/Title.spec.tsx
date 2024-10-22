0f64d5ed9c749c483659a55a91f681452f687262
Connor Bär <github@connorbaer.com>
2024-10-22 22:23:02 +0200

    Re-add the Title component

---
+++ packages/circuit-ui/components/Title/Title.spec.tsx
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2019, SumUp Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { describe, expect, it } from 'vitest';
+import { createRef } from 'react';
+
+import { axe, render } from '../../util/test-utils.js';
+
+import { Title } from './Title.js';
+
+describe('Title', () => {
+  it('should merge a custom class name with the default ones', () => {
+    const className = 'foo';
+    const { container } = render(
+      <Title as="h2" className={className}>
+        Title
+      </Title>,
+    );
+    const headline = container.querySelector('h2');
+    expect(headline?.className).toContain(className);
+  });
+
+  it('should forward a ref', () => {
+    const ref = createRef<HTMLHeadingElement>();
+    const { container } = render(
+      <Title as="h2" ref={ref}>
+        Title
+      </Title>,
+    );
+    const headline = container.querySelector('h2');
+    expect(ref.current).toBe(headline);
+  });
+
+  it('should meet accessibility guidelines', async () => {
+    const { container } = render(<Title as="h3">Subheading</Title>);
+    const actual = await axe(container);
+    expect(actual).toHaveNoViolations();
+  });
+});
