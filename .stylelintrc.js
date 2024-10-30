9475ede00c3725cbb9db99a5de178c4b23c32cfd
Connor Bär <connor-baer@users.noreply.github.com>
2024-10-30 09:18:14 +0100

    Upgrade all dependencies (#2744)

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,12 +1,6 @@
 module.exports = require('@sumup-oss/foundry/stylelint')({
   extends: ['stylelint-prettier/recommended', 'stylelint-config-css-modules'],
-  plugins: [
-    // TODO: Remove once Foundry has been updated
-    '@sumup-oss/stylelint-plugin-circuit-ui',
-  ],
   rules: {
-    'circuit-ui/no-invalid-custom-properties': true,
-    'circuit-ui/no-deprecated-custom-properties': true,
     'selector-class-pattern': [
       '^([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
       {
