69b1a23dceb589ae5aa6f3f5165fde088f6cd82f
sirineJ <112706079+sirineJ@users.noreply.github.com>
2024-12-02 13:22:11 +0100

    add tests for dialog context

---
+++ packages/circuit-ui/components/Dialog/ModalDialogContext.spec.tsx
@@ -0,0 +1,162 @@
+/**
+ * Copyright 2021, SumUp Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {
+  afterAll,
+  afterEach,
+  beforeAll,
+  describe,
+  expect,
+  it,
+  vi,
+} from 'vitest';
+import { useContext } from 'react';
+
+import {
+  render,
+  act,
+  userEvent as baseUserEvent,
+  screen,
+} from '../../util/test-utils.js';
+
+import {
+  ModalDialogProvider,
+  ModalDialogContext,
+} from './ModalDialogContext.js';
+import { animationDuration, Dialog, type DialogProps } from './Dialog.js';
+
+const Modal = (props: DialogProps) => <Dialog {...props} />;
+
+describe('ModalDialogContext', () => {
+  beforeAll(() => {
+    vi.useFakeTimers();
+
+    // HACK: Temporary workaround for a bug in @testing-library/react when
+    // using  @testing-library/user-event with fake timers.
+    // https://github.com/testing-library/react-testing-library/issues/1197
+    const originalJest = globalThis.jest;
+
+    globalThis.jest = {
+      ...globalThis.jest,
+      advanceTimersByTime: vi.advanceTimersByTime.bind(vi),
+    };
+
+    return () => {
+      globalThis.jest = originalJest;
+    };
+  });
+  afterAll(() => {
+    vi.useRealTimers();
+    vi.resetModules();
+  });
+  afterEach(() => {
+    vi.clearAllMocks();
+  });
+
+  const userEvent = baseUserEvent.setup({
+    advanceTimers: vi.advanceTimersByTime,
+  });
+
+  describe('ModalDialogProvider', () => {
+    const onClose = vi.fn();
+    const modal = {
+      id: 'initial',
+      component: Modal,
+      onClose,
+      children: () => <p>Modal content</p>,
+      closeButtonLabel: 'Close',
+    };
+    const initialState = [modal];
+
+    // `react-modal` tries to access `document` to hide the app which fails in
+    // the simulated DOM environment of the unit tests. That's why we need to
+    // set `ariaHideApp="false"`. This should not be done in production apps.
+    it('should render the initial modals', () => {
+      render(
+        <ModalDialogProvider initialState={initialState}>
+          <div />
+        </ModalDialogProvider>,
+      );
+
+      expect(screen.getByRole('dialog')).toBeVisible();
+    });
+
+    it('should open and close a modal when the context functions are called', async () => {
+      const Trigger = () => {
+        const { setModal, removeModal } = useContext(ModalDialogContext);
+        return (
+          <>
+            <button onClick={() => setModal(modal)}>Open modal</button>
+            <button onClick={() => removeModal(modal)}>Close modal</button>
+          </>
+        );
+      };
+
+      render(
+        <ModalDialogProvider>
+          <Trigger />
+        </ModalDialogProvider>,
+      );
+
+      await userEvent.click(screen.getByRole('button', { name: 'Open modal' }));
+
+      expect(screen.getByRole('dialog')).toBeVisible();
+
+      await userEvent.click(screen.getByRole('button', { name: 'Close' }));
+
+      act(() => {
+        vi.runAllTimers();
+      });
+
+      expect(screen.queryByRole('dialog')).toBeNull();
+    });
+
+    it('should close the modal when the user navigates back', () => {
+      const { container, unmount } = render(
+        <ModalDialogProvider initialState={initialState}>
+          <div />
+        </ModalDialogProvider>,
+      );
+      const dialog = container.querySelector('dialog') as HTMLDialogElement;
+      vi.spyOn(dialog, 'close');
+
+      unmount();
+      act(() => {
+        vi.runAllTimers();
+        vi.advanceTimersByTime(animationDuration);
+      });
+
+      expect(screen.queryByRole('dialog')).toBeNull();
+      expect(dialog.close).toHaveBeenCalledTimes(1);
+    });
+
+    it('should close the modal when the onClose method is called', async () => {
+      render(
+        <ModalDialogProvider initialState={initialState}>
+          <div />
+        </ModalDialogProvider>,
+      );
+
+      const closeButton = screen.queryByRole('button') as HTMLButtonElement;
+      await userEvent.click(closeButton);
+      act(() => {
+        vi.runAllTimers();
+      });
+
+      expect(screen.queryByRole('dialog')).toBeNull();
+      expect(onClose).toHaveBeenCalledTimes(1);
+    });
+  });
+});
