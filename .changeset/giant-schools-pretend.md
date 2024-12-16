b357c8ea0f75a382881c282d461e63a274ddba09
Connor Bär <connor-baer@users.noreply.github.com>
2024-12-16 11:48:15 +0100

    Upgrade Astro template to v5 (#2830)
    * Fix Next.js template
    
    * Upgrade Astro template to v5
    
    * Add changeset
    
    * Pin Node to v22.11 in CI

---
+++ .changeset/giant-schools-pretend.md
@@ -0,0 +1,5 @@
+---
+"@sumup-oss/astro-template-circuit-ui": major
+---
+
+Upgraded to [Astro v5](https://astro.build/blog/astro-5/).
--- templates/nextjs/template/app/preload-resources.ts
+++ templates/nextjs/template/app/preload-resources.ts
@@ -3,7 +3,8 @@
 import ReactDOM from 'react-dom';
 
 export function PreloadResources() {
-  // @ts-expect-error This API is available in the React canary used by Next.js
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore This API is available in the React canary used by Next.js
   // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   ReactDOM.preload(
     'https://static.sumup.com/fonts/Inter/Inter-normal-latin.woff2',
