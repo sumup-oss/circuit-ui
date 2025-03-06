e9f5611c4d0f09d33c8dd7345d8f3fb6cdc554a2
sirineJ <112706079+sirineJ@users.noreply.github.com>
2025-03-06 18:01:14 +0100

    add table header

---
+++ packages/circuit-ui/components/ComparisonTable/components/TableHeader/TableHeader.spec.tsx
@@ -0,0 +1,48 @@
+/**
+ * Copyright 2025, SumUp Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import { beforeEach, describe, expect, it, type Mock, vi } from 'vitest';
+
+import { render, screen } from '../../../../util/test-utils.js';
+import { useMedia } from '../../../../hooks/useMedia/index.js';
+import { posPlan } from '../../fixtures.js';
+
+import { TableHeader, type TableHeaderProps } from './TableHeader.js';
+
+vi.mock('../../../../hooks/useMedia/index.js');
+
+describe('TableHeader', () => {
+  beforeEach(() => {
+    (useMedia as Mock).mockReturnValue(false);
+  });
+
+  const baseProps: TableHeaderProps = posPlan;
+
+  it('should render all plan information', () => {
+    render(<TableHeader {...baseProps} tier={{ variant: 'plus' }} />);
+    expect(screen.getByText(baseProps.title)).toBeVisible();
+    expect(screen.getByText(baseProps.description)).toBeVisible();
+    expect(
+      screen.getByText(baseProps.callToAction.children as string),
+    ).toBeVisible();
+    expect(screen.getByText('plus')).toBeVisible();
+  });
+
+  it('should hide call to action button on mobile', () => {
+    (useMedia as Mock).mockReturnValue(true);
+    render(<TableHeader {...baseProps} />);
+    expect(screen.queryByText('call to action')).not.toBeInTheDocument();
+  });
+});
---
+++ packages/circuit-ui/components/ComparisonTable/components/TableHeader/TableHeader.tsx
@@ -0,0 +1,82 @@
+/**
+ * Copyright 2025, SumUp Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use client';
+
+import type { ThHTMLAttributes } from 'react';
+
+import {
+  TierIndicator,
+  type TierIndicatorProps,
+} from '../../../TierIndicator/TierIndicator.js';
+import { Button, type ButtonProps } from '../../../Button/index.js';
+import { Body } from '../../../Body/index.js';
+import { useMedia } from '../../../../hooks/useMedia/index.js';
+import { Compact } from '../../../Compact/index.js';
+
+import classes from './TableHeader.module.css';
+
+export interface TableHeaderProps
+  extends ThHTMLAttributes<HTMLTableCellElement> {
+  /**
+   * The title of the plan.
+   */
+  title: string;
+  /**
+   * The id of the plan.
+   */
+  id: string;
+  /**
+   * An optional tier indicator
+   */
+  tier?: TierIndicatorProps;
+  /**
+   *  Additional information about the plan
+   */
+  description: string;
+  /**
+   *  The call-to-action button props
+   */
+  callToAction: ButtonProps;
+}
+
+export const TableHeader = ({
+  title,
+  description,
+  tier,
+  callToAction,
+}: TableHeaderProps) => {
+  const isMobile = useMedia('(max-width: 767px)');
+
+  return (
+    <th scope="col" className={classes.wrapper}>
+      <div className={classes.base}>
+        <div className={classes.title}>
+          {(!isMobile || (isMobile && !tier)) && (
+            <Compact size={isMobile ? 'm' : 'l'} weight="bold">
+              {title}
+            </Compact>
+          )}
+          {tier && <TierIndicator {...tier} size={isMobile ? 's' : 'm'} />}
+        </div>
+
+        <Body size={isMobile ? 's' : 'm'} className={classes.description}>
+          {description}
+        </Body>
+        {!isMobile && <Button {...callToAction} variant="secondary" size="s" />}
+      </div>
+    </th>
+  );
+};
