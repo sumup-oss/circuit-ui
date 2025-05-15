a279063a42ee0d48988ae975b3d0fbb0098803b2
sirineJ <112706079+sirineJ@users.noreply.github.com>
2025-05-15 17:42:24 +0200

    declare suggestions types

--- packages/circuit-ui/index.ts
+++ packages/circuit-ui/index.ts
@@ -69,7 +69,10 @@ export type { PhoneNumberInputProps } from './components/PhoneNumberInput/index.
 export { ColorInput } from './components/ColorInput/index.js';
 export type { ColorInputProps } from './components/ColorInput/index.js';
 export { Autocomplete } from './components/Autocomplete/Autocomplete.js';
-export type { AutocompleteProps } from './components/Autocomplete/Autocomplete.js';
+export type {
+  AutocompleteProps,
+  AutocompleteSuggestions,
+} from './components/Autocomplete/Autocomplete.js';
 
 // Actions
 export { Button } from './components/Button/index.js';
