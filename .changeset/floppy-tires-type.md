a1d2880c778810a9ccd04d1291e45b9234302b82
sirineJ <112706079+sirineJ@users.noreply.github.com>
2025-09-11 14:20:34 +0200

    fix selected tab style issues (#3283)
    * move --selected-tab-pseudo-content var to TabList
    
    * disable pointer events inside tabs
    
    * add changeset

---
+++ .changeset/floppy-tires-type.md
@@ -0,0 +1,5 @@
+---
+"@sumup-oss/circuit-ui": patch
+---
+
+Fixed minor styling issues with the selected tab indicator in the TabList component.
--- packages/circuit-ui/components/Tabs/components/TabList/TabList.tsx
+++ packages/circuit-ui/components/Tabs/components/TabList/TabList.tsx
@@ -58,7 +58,10 @@ export const TabList = forwardRef<HTMLDivElement, TabListProps>(
     const stretchOnMobile = numberOfTabs <= MOBILE_AUTOSTRETCH_ITEMS_MAX;
 
     const updateGliderStyles = useCallback((tab: HTMLElement) => {
-      tab.style.setProperty('--selected-tab-pseudo-content', 'unset');
+      tabListRef.current?.style.setProperty(
+        '--selected-tab-pseudo-content',
+        'unset',
+      );
       const { offsetLeft, offsetWidth } = tab;
       gliderRef.current?.style?.setProperty(
         'transform',
