217e3c4ae73add15ff47a62b8b27f6b447f2c2b4
Connor Bär <github@connorbaer.com>
2025-09-13 13:22:02 +0200

    Add back Storybook plugin

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,5 @@
 import { configs, defineConfig, files } from '@sumup-oss/foundry/eslint';
+import storybook from 'eslint-plugin-storybook';
 import testingLibrary from 'eslint-plugin-testing-library';
 import circuitUI from '@sumup-oss/eslint-plugin-circuit-ui';
 
@@ -36,22 +37,13 @@ export default defineConfig([
     },
   },
   {
+    extends: [storybook.configs['flat/recommended'], configs.stories],
+  },
+  {
     files: [...files.stories, ...files.tests],
     rules: {
       'import-x/no-relative-packages': 'off',
       'react-server-components/use-client': 'off',
-    },
-  },
-  {
-    files: ['packages/circuit-ui/components/legacy/**/*'],
-    rules: {
-      '@sumup-oss/circuit-ui/prefer-custom-properties': 'off',
-    },
-  },
-  {
-    files: ['vite.*', 'vitest.*'],
-    rules: {
-      'import-x/no-extraneous-dependencies': 'off',
     },
   },
   {
@@ -71,35 +63,7 @@ export default defineConfig([
 
 // module.exports = require('@sumup-oss/foundry/eslint')({
 //   extends: [
-//     'plugin:storybook/recommended',
 //     'plugin:react-server-components/recommended',
 //   ],
-//   plugins: ['@sumup-oss/circuit-ui'],
-//   settings: {
-//     'import/parsers': {
-//       '@typescript-eslint/parser': ['.ts', '.tsx'],
-//     },
-//     'import/resolver': {
-//       typescript: {
-//         alwaysTryTypes: true,
-//         project: ['./packages/*/tsconfig.json', './tsconfig.eslint.json'],
-//       },
-//     },
-//   },
-//   rules: {
-//     '@sumup-oss/circuit-ui/no-invalid-custom-properties': 'error',
-//     '@sumup-oss/circuit-ui/no-deprecated-custom-properties': 'error',
-//     '@sumup-oss/circuit-ui/no-deprecated-props': 'error',
-//     '@sumup-oss/circuit-ui/no-deprecated-components': 'error',
-//     '@sumup-oss/circuit-ui/no-renamed-props': 'error',
-//     '@sumup-oss/circuit-ui/prefer-custom-properties': 'warn',
-//     'react/no-unknown-property': ['error', { ignore: ['css'] }],
-//     // These rules are already covered by Biome
-//     'jsx-a11y/click-events-have-key-events': 'off',
-//     'jsx-a11y/no-static-element-interactions': 'off',
-//   },
-//   parserOptions: {
-//     project: ['./packages/*/tsconfig.json', './tsconfig.eslint.json'],
-//     tsconfigRootDir: __dirname,
 //   },
 // });
