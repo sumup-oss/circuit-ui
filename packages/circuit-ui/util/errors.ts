b6fd143e2ed9de8d36f7e23cf0d4d63f3103f55b
Davi Medeiros <davilima6@gmail.com>
2026-01-19 13:47:35 -0300

    fix: guard React internals access in CircuitError
    Backport of this fix applied in CUI v10: https://github.com/sumup-oss/circuit-ui/pull/3138/changes#diff-5c08c5f3cfa692f51cc6bdfcae1decb66c174883a15ab71dd13f880a93ddce80
    
    Addresses this error with React 19:
    
    """
      file:///Users/davimedeiros/Projects/storefront-web/node_modules/@sumup-oss/circuit-ui/dist/util/
      errors.js:11
            (_b = (_a = React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner) ==
      null ? void 0 : _a.return) == null ? void 0 : _b.type
                                                                                 ^
    
      TypeError: Cannot read properties of undefined (reading 'ReactCurrentOwner')
          at new CircuitError (file:///Users/davimedeiros/Projects/storefront-web/node_modules/@sumup-oss/
      circuit-ui/dist/util/errors.js:11:76)
          at file:///Users/davimedeiros/Projects/storefront-web/[eval1]:1:90
          at ModuleJob.run (node:internal/modules/esm/module_job:413:25)
          at async onImport.tracePromise.__proto__ (node:internal/modules/esm/loader:268:26)
          at async ModuleLoader.executeModuleJob (node:internal/modules/esm/loader:265:20)
          at async asyncRunEntryPointWithESMLoader (node:internal/modules/run_main:101:5)
    """

--- packages/circuit-ui/util/errors.ts
+++ packages/circuit-ui/util/errors.ts
@@ -27,8 +27,8 @@ export class CircuitError extends Error {
     /* eslint-disable max-len, no-underscore-dangle, @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call */
     if (
       // @ts-expect-error Since this code only runs in development, it's fine to use this internal React API.
-      React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner
-        ?.return?.type
+      React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED
+        ?.ReactCurrentOwner?.return?.type
     ) {
       this.stack =
         // @ts-expect-error This is fine.
@@ -38,7 +38,7 @@ export class CircuitError extends Error {
     if (
       // @ts-expect-error Since this code only runs in development, it's fine to use this internal React API.
       React.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED
-        .ReactDebugCurrentFrame?.getCurrentStack
+        ?.ReactDebugCurrentFrame?.getCurrentStack
     ) {
       this.stack =
         // @ts-expect-error This is fine.
