c027ceaaa4d83ab25f4772a47e80b29d01902692
Connor Bär <github@connorbaer.com>
2026-01-28 18:22:46 +0100

    Fix lint issues

--- packages/eslint-plugin-circuit-ui/utils/helpers.ts
+++ packages/eslint-plugin-circuit-ui/utils/helpers.ts
@@ -1,11 +1,27 @@
+/**
+ * Copyright 2025, SumUp Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 export function get(obj: unknown, path: string[]) {
   let currentValue = obj;
 
+  // eslint-disable-next-line no-restricted-syntax
   for (const property of path) {
     if (typeof currentValue !== 'object' || currentValue === null) {
       return undefined;
     }
-    // @ts-expect-error
+    // @ts-expect-error It's okay if this returns undefined.
     currentValue = currentValue[property];
   }
 
