c30f8860b1eee9616b1d334efb1c9d1aa4b99d20
sirineJ <112706079+sirineJ@users.noreply.github.com>
2026-06-03 13:27:42 +0200

    Remove legacy tooltip inline elements (#3670)
    * removed legacy tooltip component
    
    * removed legacy inline elements component
    
    * add changesets

--- packages/circuit-ui/legacy.ts
+++ packages/circuit-ui/legacy.ts
@@ -16,12 +16,6 @@
 // Layout
 export { Grid, Row, Col } from './components/legacy/Grid/index.js';
 export type { ColProps } from './components/legacy/Grid/index.js';
-export { InlineElements } from './components/legacy/InlineElements/index.js';
-export type { InlineElementsProps } from './components/legacy/InlineElements/index.js';
-
-// Miscellaneous
-export { Tooltip } from './components/legacy/Tooltip/index.js';
-export type { TooltipProps } from './components/legacy/Tooltip/index.js';
 
 export {
   cx,
