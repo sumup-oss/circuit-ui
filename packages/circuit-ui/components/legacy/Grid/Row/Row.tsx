7f87eb8ed430a3a5103544d539a89208ac89f08e
sirineJ <112706079+sirineJ@users.noreply.github.com>
2026-06-03 15:48:47 +0200

    refactor row

---
+++ packages/circuit-ui/components/legacy/Grid/Row/Row.tsx
@@ -0,0 +1,33 @@
+/**
+ * Copyright 2019, SumUp Ltd.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/** @jsxImportSource @emotion/react */
+
+
+import {forwardRef, type HTMLAttributes} from 'react';
+import styles from "./Row.module.css"
+import { clsx } from '../../../../styles/clsx.js';
+
+
+
+/**
+ * @legacy
+ *
+ * Row wrapping for the Col component.
+ */
+export const Row = forwardRef<HTMLDivElement , HTMLAttributes<HTMLDivElement>>(
+  ({ className, ...props}, ref) =>
+    <div ref={ref} className={clsx(styles.base, className)} {...props}></div>
+  )
