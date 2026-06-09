ac299a16fc343ad1efc696a35d5cd0ac4ec7fbe4
sirineJ <112706079+sirineJ@users.noreply.github.com>
2026-06-09 14:16:16 +0200

    remove BodyLarge component (#3684)

--- packages/circuit-ui/index.ts
+++ packages/circuit-ui/index.ts
@@ -26,8 +26,6 @@ export { Display } from './components/Display/index.js';
 export type { DisplayProps } from './components/Display/index.js';
 export { Body } from './components/Body/index.js';
 export type { BodyProps } from './components/Body/index.js';
-export { BodyLarge } from './components/BodyLarge/index.js';
-export type { BodyLargeProps } from './components/BodyLarge/index.js';
 export { Compact } from './components/Compact/index.js';
 export type { CompactProps } from './components/Compact/index.js';
 export { Numeral } from './components/Numeral/index.js';
