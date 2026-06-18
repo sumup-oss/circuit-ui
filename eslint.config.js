f19afa4cecaaa1d00c8a0ed76e15cd7906d79093
sirineJ <112706079+sirineJ@users.noreply.github.com>
2026-06-18 14:45:18 +0200

    apply rule locally

--- eslint.config.js
+++ eslint.config.js
@@ -37,6 +37,7 @@ export default defineConfig([
       'circuit-ui/no-deprecated-props': 'error',
       'circuit-ui/no-renamed-props': 'error',
       'circuit-ui/prefer-custom-properties': 'warn',
+      'circuit-ui/no-deprecated-icons': 'warn',
     },
   },
   configs.browser,
