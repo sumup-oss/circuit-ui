14073bba59fd02e138fdc245361e3b64a9d4dc14
missating <atingenkay@gmail.com>
2026-07-02 15:55:22 +0200

    change icon sizes to rem

--- stylelint.config.js
+++ stylelint.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from '@sumup-oss/foundry/stylelint';
 
 export default defineConfig({
   extends: ['stylelint-config-css-modules'],
+  plugins: ['@sumup-oss/stylelint-plugin-circuit-ui'],
   rules: {
+    'circuit-ui/no-invalid-custom-properties': true,
+    'circuit-ui/no-deprecated-custom-properties': true,
     'selector-class-pattern': [
       '^([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
       {
