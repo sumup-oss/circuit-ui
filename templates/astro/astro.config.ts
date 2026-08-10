02eb7643cc5fed99d4cc377d9c3676a34b5f7aad
sirineJ <112706079+sirineJ@users.noreply.github.com>
2026-08-10 10:48:49 +0200

    fix templates

--- templates/astro/astro.config.ts
+++ templates/astro/astro.config.ts
@@ -4,4 +4,9 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  vite: {
+    resolve: {
+      noExternal: ['@sumup-oss/circuit-ui', '@sumup-oss/illustrations'],
+    },
+  },
 });
